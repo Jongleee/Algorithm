ff4a03167a21313e3d7f12b9e1cec9cf85a4d803
Jonglee <forin777@gmail.com>
2022-07-15 17:28:48 +0900

    초기설정

---
+++ src/main/java/No4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        // 두 집단의 비교를 위하여 먼저 오름차순으로 각 배열을 정렬
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        // 반복문으로 정렬된 값의 비교에서 일치하지 않는 경우를 찾아줌
+        for (int i=0;i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                answer+=participant[i];
+                return answer;
+            }
+        }
+        // 위의 반복문에서 일치하지 않는 값이 없다면 배열의 마지막 값이 답이 됨
+        answer+=participant[participant.length -1];
+        return answer;
+    }
+}
---
+++ src/main/java/no2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int cnt = 0;
+        int j = 0;
+        //divisor로 나눈 나머지가 0인 값의 갯수를 구함
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                cnt++;
+            }
+        }
+        // 나누어 떨어지지 않는 경우 {-1} 대입
+        if (cnt == 0) {
+            int[] answer = {-1};
+            return answer;
+        }
+        // 나누어 떨어지는 값의 갯수 만큼의 배열을 정의
+        int[] answer = new int[cnt];
+        // 그 배열에 해당하는 경우의 값을 넣어줌
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%divisor==0){
+                answer[j]=arr[i];
+                j++;
+            }
+        }
+
+        Arrays.sort(answer);
+        return answer;
+    }
+}
