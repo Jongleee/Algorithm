eb177c54912641b28e4df98af4ad4a0609f45f9c
Jonglee <forin777@gmail.com>
2022-07-15 19:01:54 +0900

    5번 해결

---
+++ src/main/java/No5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        //단어를 하나씩 쪼개기
+        String[] ss = s.split("");
+
+        int cnt = 0;
+        for (int i = 0; i < ss.length; i++) {
+            //공백을 만나면 카운트 초기화
+            if (ss[i].equals(" ") ) {
+                cnt=0;
+            }
+            //짝수번째 단어 대문자
+            else if (cnt % 2 == 0) {
+                ss[i] = ss[i].toUpperCase();
+                cnt++;
+            }
+            //나머지 경우는 소문자로 덧붙여줌
+            else {
+                ss[i] = ss[i].toLowerCase();
+                cnt++;
+            }
+            answer+=ss[i];
+
+        }
+        return answer;
+    }
+}
