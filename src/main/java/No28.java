9b83fc886266abb8db3fddbbc84f3ae1cc4b3c31
Jonglee <forin777@gmail.com>
2022-07-19 11:24:34 +0900

    오늘의 커밋

---
+++ src/main/java/No28.java
@@ -0,0 +1,16 @@
+public class No28 {
+    class Solution {
+        public int[] solution(int n, int m) {
+            int GCD = 0;
+            int min = (n <= m) ? n : m;
+            for (int i = min; i >0; i--) {
+                if (n % i == 0 && m % i == 0) {
+                    GCD = i;
+                    break;
+                }
+            }
+            int commonMultiply = n * m / GCD;
+            return new int[]{GCD, commonMultiply};
+        }
+    }
+}
