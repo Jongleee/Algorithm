6ad0b0ad364b7fde5847f392ca5bf00c2067bcdc
Jongleee <108917552+Jongleee@users.noreply.github.com>
2022-07-28 15:47:45 +0900

    Update No39.java

--- src/main/java/No39.java
+++ src/main/java/No39.java
@@ -1,23 +1,4 @@
 public class No39 {
-<<<<<<< HEAD
-    public int solution(int n) {
-        int answer = 1;
-        for (int i = 1; i < n; i++) {
-            int sum = i;
-            for (int j = i + 1; j < n; j++) {
-                sum += j;
-                if (sum > n) break;
-                if (sum == n) {
-                    answer += 1;
-                    break;
-                }
-            }
-        }
-        return answer;
-    }
-
-=======
-    class Solution {
         public int solution(int n) {
             int answer = 1;
             for (int i =1; i < n; i++) {
@@ -31,5 +12,4 @@ public class No39 {
             return answer;
         }
     }
->>>>>>> fb4f084ef03003a3f04c923d4d076afc4af57e35
 }
