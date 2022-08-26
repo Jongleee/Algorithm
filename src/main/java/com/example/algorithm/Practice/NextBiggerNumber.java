6d37bcfb05e4b5521f7a952487d52f9ba150d910
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-08-26 21:32:10 +0900

    다음 큰 숫자

---
+++ src/main/java/com/example/algorithm/Practice/NextBiggerNumber.java
@@ -0,0 +1,11 @@
+package com.example.algorithm.Practice;
+
+public class NextBiggerNumber {
+    public int solution(int n) {
+        int num=Integer.bitCount(n);
+        for (int i = n+1; i <= 1000000 ; i++) {
+            if (Integer.bitCount(i)==num) return i;
+        }
+        return num;
+    }
+}
