883c1df8eff66fe2b8b9de12ffb3dffbed685102
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-10-18 21:35:10 +0900

    가장 먼 노드 BFS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution method = new Solution();
-        System.out.println(method.solution("[](){}"));
+        System.out.println(method.solution(6,new int[][]{{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}} ));
     }
 }
