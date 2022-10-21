9ba22e3df717b04ecb3d10cc2eedc6b57f540ddd
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-10-21 21:31:36 +0900

    N으로 나타내기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution method = new Solution();
-        System.out.println(method.solution("[](){}"));
+        System.out.println();
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class Solution {
+
 }
