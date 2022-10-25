4ec0173d49ff934aaebc8ab65be55fd65a327625
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-10-25 22:41:10 +0900

    단어 변환

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution method = new Solution();
-        System.out.println(method.solution(new int[][]{{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}}));
+        System.out.println(method.solution("hit",	"cog",new String[]{"hot", "dot", "dog", "lot", "log"}));
 
     }
 }
