56a94983b00158da2497f3be3cd56db2a4b7ea0b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-11-01 22:45:58 +0900

    다단계 칫솔 판매

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution method = new Solution();
-        System.out.println(method.solution());
+        System.out.println(Arrays.toString(method.solution()));
 
     }
 }
