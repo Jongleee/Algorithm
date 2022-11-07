c3d3866ae0f6ea5f77437926e59120d1cc4a1816
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-11-07 23:55:23 +0900

    베스트 앨범

---
+++ src/main/java/com/example/algorithm/Hash/BestAlbum.java
@@ -0,0 +1,56 @@
+package com.example.algorithm.Hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BestAlbum {
+
+    public int[] solution(String[] genres, int[] plays) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i=0; i<genres.length; i++){
+            map.put(genres[i], map.getOrDefault(genres[i], 0)+plays[i]);
+        }
+
+
+        ArrayList<String> genre = new ArrayList<>();
+        for(String key : map.keySet()) {
+            genre.add(key);
+        }
+        genre.sort((o1, o2) -> map.get(o2) - map.get(o1)); //key값에 해당하는 value를 내림차순으로 정렬한다.
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < genre.size(); i++) {
+            String g = genre.get(i);
+
+            //해당 장르의 음악 중에서 play횟수가 가장 큰 인덱스를 찾는다
+            int max = 0;
+            int firstIdx = -1;
+            for(int j = 0; j < genres.length; j++) {
+                if(g.equals(genres[j]) && max < plays[j]) {
+                    max = plays[j];
+                    firstIdx = j;
+                }
+            }
+
+            //해당 장르의 음악 중에서 play횟수가 두번째로 큰 인덱스를 찾는다.
+            max = 0;
+            int secondIdx = -1;
+            for(int j = 0; j < genres.length; j++) {
+                if(g.equals(genres[j]) && max < plays[j] && j != firstIdx) {
+                    max = plays[j];
+                    secondIdx = j;
+                }
+            }
+
+            list.add(firstIdx);
+            if(secondIdx >= 0) list.add(secondIdx); //secondIdx는 존재 할수도, 안할수도 있다.
+        }
+
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
