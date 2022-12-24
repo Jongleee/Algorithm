12af32076bf46a47d5b25fd3f7e357e6984b84df
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-12-24 22:37:20 +0900

    refactor

--- src/main/java/com/example/algorithm/searchDFS/Network.java
+++ src/main/java/com/example/algorithm/searchDFS/Network.java
@@ -1,8 +1,7 @@
 package com.example.algorithm.searchDFS;
 
 public class Network {
-    public int solution(int n, int[][] computers) {
-        //3, new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}
+    public static int solution(int n, int[][] computers) {
         int answer = 0;
         boolean[] check = new boolean[n];
         for (int i = 0; i < n; i++) {
@@ -14,12 +13,15 @@ public class Network {
         return answer;
     }
 
-    void dfs(int[][] computers, boolean[] check, int num) {
+    static void dfs(int[][] computers, boolean[] check, int num) {
         check[num] = true;
         for (int i = 0; i < computers.length; i++) {
             if (computers[num][i] == 1 && !check[i]) {
                 dfs(computers, check, i);
             }
         }
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(3, new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
     }
 }
