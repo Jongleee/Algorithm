0252ef0fb6f0959338aec7bf7b6c2c6bc3c3a7b3
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-12-28 22:53:58 +0900

    refactor

--- src/main/java/prac/No28.java
+++ src/main/java/prac/No28.java
@@ -3,16 +3,18 @@ package prac;
 public class No28 {
     class Solution {
         public int[] solution(int n, int m) {
-            int GCD = 0;
+            int gcd = 0;
             int min = (n <= m) ? n : m;
-            for (int i = min; i >0; i--) {
+            for (int i = min; i > 0; i--) {
                 if (n % i == 0 && m % i == 0) {
-                    GCD = i;
+                    gcd = i;
                     break;
                 }
             }
-            int commonMultiply = n * m / GCD;
-            return new int[]{GCD, commonMultiply};
+            if (gcd == 0)
+                return new int[] { 0, 0 };
+            int commonMultiply = n * m / gcd;
+            return new int[] { gcd, commonMultiply };
         }
     }
 }
