60e54270ad28daba032e95eda7b313a70683dab4
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-02-21 21:10:47 +0900

    주식가격

--- src/main/java/com/example/algorithm/java/example/BellmanFord.java
+++ src/main/java/com/example/algorithm/java/example/BellmanFord.java
@@ -7,38 +7,40 @@ class Edge {
     int dest;
     int weight;
 
-    Edge() {
-        src = dest = weight = 0;
+    public Edge() {
+        this.src = this.dest = this.weight = 0;
     }
 }
 
 class Graph {
     private static final int INF = Integer.MAX_VALUE;
 
-    private int vertex;
+    private int vertexCount;
     private Edge[] edges;
 
-    Graph(int v, int e) {
-        vertex = v;
-        edges = new Edge[e];
-        for (int i = 0; i < e; ++i)
-            edges[i] = new Edge();
+    public Graph(int vertexCount, int edgeCount) {
+        this.vertexCount = vertexCount;
+        this.edges = new Edge[edgeCount];
+        for (int i = 0; i < edgeCount; i++) {
+            this.edges[i] = new Edge();
+        }
     }
 
-    void bellmanFord(int src) {
+    public void bellmanFord(int sourceVertex) {
         // Step 1: 원점에서 정점으로 가는 모든 거리를 INF로 초기화
-        int[] dist = new int[vertex];
-        Arrays.fill(dist, INF);
-        dist[src] = 0;
+        int[] distances = new int[vertexCount];
+        Arrays.fill(distances, INF);
+        distances[sourceVertex] = 0;
 
         // Step 2: 모든 정점을 방문하여 거리 업데이트
-        for (int i = 1; i < vertex; ++i) {
+        for (int i = 1; i < vertexCount; i++) {
             for (Edge edge : edges) {
                 int u = edge.src;
                 int v = edge.dest;
                 int weight = edge.weight;
-                if (dist[u] != INF && dist[u] + weight < dist[v])
-                    dist[v] = dist[u] + weight;
+                if (distances[u] != INF && distances[u] + weight < distances[v]) {
+                    distances[v] = distances[u] + weight;
+                }
             }
         }
 
@@ -47,25 +49,25 @@ class Graph {
             int u = edge.src;
             int v = edge.dest;
             int weight = edge.weight;
-            if (dist[u] != INF && dist[u] + weight < dist[v]) {
+            if (distances[u] != INF && distances[u] + weight < distances[v]) {
                 System.out.println("음수 사이클 존재");
                 return;
             }
         }
 
-        printDistances(dist);
+        printDistances(distances);
     }
 
-    void printDistances(int[] dist) {
+    void printDistances(int[] distances) {
         System.out.println("Vertex Distance from Source");
-        for (int i = 0; i < vertex; ++i)
-            System.out.println(i + "\t\t" + dist[i]);
+        for (int i = 0; i < vertexCount; i++) {
+            System.out.println(i + "\t\t" + distances[i]);
+        }
     }
 
     public static void main(String[] args) {
         int vertex = 5; // Number of vertices in graph
         int edge = 8; // Number of edges in graph
-
         Graph graph = new Graph(vertex, edge);
 
         // add edge 0-1 (or A-B in above figure)
