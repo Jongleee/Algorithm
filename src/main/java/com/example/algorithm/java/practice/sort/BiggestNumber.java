019594d2b4512d092e92b32a0dbc610a02e18dd6
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-02-24 18:28:15 +0900

    join

--- src/main/java/com/example/algorithm/java/practice/sort/BiggestNumber.java
+++ src/main/java/com/example/algorithm/java/practice/sort/BiggestNumber.java
@@ -6,8 +6,8 @@ public class BiggestNumber {
 
     public static String solution(int[] numbers) {
         String[] arr = Arrays.stream(numbers)
-                             .mapToObj(String::valueOf)
-                             .toArray(String[]::new);
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
 
         Arrays.sort(arr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
 
@@ -19,8 +19,8 @@ public class BiggestNumber {
     }
 
     public static void main(String[] args) {
-        int[] n1 = {6, 10, 2};
-        int[] n2 = {3, 30, 34, 5, 9};
+        int[] n1 = { 6, 10, 2 };
+        int[] n2 = { 3, 30, 34, 5, 9 };
 
         System.out.println(solution(n1)); // 6210
         System.out.println(solution(n2)); // 9534330
