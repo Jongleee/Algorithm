cbb2e21432844b8fd0d0c284ae108ef232196650
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-03-08 23:20:12 +0900

    무지의 먹방 라이브

---
+++ src/main/java/com/example/algorithm/java/queue/LiveMukbang.java
@@ -0,0 +1,79 @@
+package com.example.algorithm.java.queue;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class LiveMukbang {
+    public static class Food {
+        private final int index;
+        private int time;
+
+        public Food(int index, int time) {
+            this.index = index;
+            this.time = time;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public void setTime(int time) {
+            this.time = time;
+        }
+    }
+
+    public static int solution(int[] food_times, long k) {
+        Queue<Food> q = new PriorityQueue<>(Comparator.comparingInt(Food::getTime).thenComparingInt(Food::getIndex));
+
+        for (int i = 0; i < food_times.length; i++) {
+            q.add(new Food(i + 1, food_times[i]));
+        }
+
+        int prevTime = 0;
+        int n = food_times.length;
+
+        while (!q.isEmpty()) {
+            Food food = q.peek();
+            int diff = food.getTime() - prevTime;
+            long totalTime = (long) diff * n;
+
+            if (totalTime <= k) {
+                k -= totalTime;
+                prevTime = food.getTime();
+                q.poll();
+                n--;
+            } else {
+                break;
+            }
+        }
+
+        if (q.isEmpty()) {
+            return -1;
+        }
+
+        List<Food> remainingFoods = new ArrayList<>();
+
+        while (!q.isEmpty()) {
+            remainingFoods.add(q.poll());
+        }
+
+        remainingFoods.sort(Comparator.comparingInt(Food::getIndex));
+
+        int index = (int) (k % n);
+        return remainingFoods.get(index).getIndex();
+    }
+
+    public static void main(String[] args) {
+        int[] f1 = { 3, 1, 2 };
+        int k1 = 5;
+        System.out.println(solution(f1, k1));
+    }
+
+}
