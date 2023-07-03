91bef51292f237b55b4e071cf8c1ea2a67bc1c04
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-07-03 22:47:51 +0900

    예상 대진표

--- src/main/java/com/example/algorithm/java/greedy/Lifeboat.java
+++ src/main/java/com/example/algorithm/java/greedy/Lifeboat.java
@@ -20,14 +20,13 @@ public class Lifeboat {
 
 		return answer;
 	}
-	// @Test
-	// public void test() {
-	// Assert.assertEquals(1, solution(new int[]{50}, 50));
-	// Assert.assertEquals(2, solution(new int[]{20, 50, 50, 80}, 100));
-	// Assert.assertEquals(3, solution(new int[]{70, 50, 80, 50}, 100));
-	// Assert.assertEquals(3, solution(new int[]{50, 30, 20, 70, 10}, 100));
-	// Assert.assertEquals(3, solution(new int[]{70, 80, 50}, 100));
-	// Assert.assertEquals(5, solution(new int[]{10, 20, 30, 40, 50, 60, 70, 80,
-	// 90}, 100));
-	// }
 }
+// @Test
+// 	public void 정답() {
+// 		Assertions.assertEquals(1, solution(new int[] { 50 }, 50));
+// 		Assertions.assertEquals(2, solution(new int[] { 20, 50, 50, 80 }, 100));
+// 		Assertions.assertEquals(3, solution(new int[] { 70, 50, 80, 50 }, 100));
+// 		Assertions.assertEquals(3, solution(new int[] { 50, 30, 20, 70, 10 }, 100));
+// 		Assertions.assertEquals(3, solution(new int[] { 70, 80, 50 }, 100));
+// 		Assertions.assertEquals(5, solution(new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90 }, 100));
+// 	}
