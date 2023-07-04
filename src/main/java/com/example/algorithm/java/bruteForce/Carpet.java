be7d35e091ecf9cff756590c0ef86a8d0ab4cb0c
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-07-04 23:10:53 +0900

    카펫

---
+++ src/main/java/com/example/algorithm/java/bruteForce/Carpet.java
@@ -0,0 +1,32 @@
+package com.example.algorithm.java.bruteForce;
+
+public class Carpet {
+    public int[] solution(int brown, int yellow) {
+		int[] answer = new int[2];
+
+		int area = brown + yellow;
+
+		for (int row = 1; row <= area; row++) {
+			int col = area / row;
+
+			if (row > col) {
+				continue;
+			}
+
+			if ((row - 2) * (col - 2) == yellow) {
+				answer[0] = col;
+				answer[1] = row;
+				return answer;
+			}
+		}
+
+		return answer;
+	}
+
+	// @Test
+	// public void 정답() {
+	// 	Assertions.assertEquals("[4, 3]", Arrays.toString(solution(10,2)));
+	// 	Assertions.assertEquals("[3, 3]", Arrays.toString(solution(8,1)));
+	// 	Assertions.assertEquals("[8, 6]", Arrays.toString(solution(24,24)));
+	// }
+}
--- src/test/java/TestSolution.java
+++ src/test/java/TestSolution.java
@@ -1,19 +1,35 @@
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestSolution {
-	public int solution(int n, int a, int b) {
-		int answer = 0;
-		while (a != b) {
-			a = a / 2 + a % 2;
-			b = b / 2 + b % 2;
-			answer++;
+	public int[] solution(int brown, int yellow) {
+		int[] answer = new int[2];
+
+		int area = brown + yellow;
+
+		for (int row = 1; row <= area; row++) {
+			int col = area / row;
+
+			if (row > col) {
+				continue;
+			}
+
+			if ((row - 2) * (col - 2) == yellow) {
+				answer[0] = col;
+				answer[1] = row;
+				return answer;
+			}
 		}
+
 		return answer;
 	}
 
 	@Test
 	public void 정답() {
-		Assertions.assertEquals(3, solution(8, 4, 7));
+		Assertions.assertEquals("[4, 3]", Arrays.toString(solution(10,2)));
+		Assertions.assertEquals("[3, 3]", Arrays.toString(solution(8,1)));
+		Assertions.assertEquals("[8, 6]", Arrays.toString(solution(24,24)));
 	}
 }
