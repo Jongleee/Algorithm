2ea1a056043b9d4597db55e15080f88dd9a93b02
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-07-07 23:07:12 +0900

    JadenCase 문자열 만들기

---
+++ src/main/java/com/example/algorithm/java/string/CreateJadenCaseString.java
@@ -0,0 +1,32 @@
+package com.example.algorithm.java.string;
+
+public class CreateJadenCaseString {
+    public String solution(String s) {
+        String[] words = s.split(" ");
+
+        StringBuilder answerBuilder = new StringBuilder();
+
+        for (String word : words) {
+            if (word.isEmpty()) {
+                answerBuilder.append(" ");
+            } else {
+                String capitalized = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+                answerBuilder.append(capitalized).append(" ");
+            }
+        }
+
+        String answer = answerBuilder.toString();
+        if (s.substring(s.length() - 1, s.length()).equals(" "))
+            return answer;
+        return answer.trim();
+    }
+
+    // @Test
+    // public void 정답() {
+    // Assertions.assertEquals("3people Unfollowed Me", solution("3people unFollowed
+    // me"));
+    // Assertions.assertEquals("3people Unfollowed Me ", solution("3people
+    // unFollowed me "));
+    // Assertions.assertEquals("For The Last Week", solution("for the last week"));
+    // }
+}
