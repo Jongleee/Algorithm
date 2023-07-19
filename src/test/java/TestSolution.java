1cb85a41a7ad159a05eb813bea6e2ac633abe556
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-07-19 19:02:51 +0900

    최솟값 만들기

--- src/test/java/TestSolution.java
+++ src/test/java/TestSolution.java
@@ -1,22 +1,23 @@
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestSolution {
-    public long solution(int n) {
-        long[] dp = new long[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    public static int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int answer = 0;
+        int length = A.length;
+        for (int i = 0; i < length; i++) {
+            answer += A[i] * B[length - i - 1];
         }
-
-        return dp[n];
+        return answer;
     }
 
     @Test
     public void 정답() {
-        Assertions.assertEquals(2, solution(3));
-        Assertions.assertEquals(5, solution(5));
+        Assertions.assertEquals(29, solution(new int[] { 1, 4, 2 }, new int[] { 5, 4, 4 }));
+        Assertions.assertEquals(10, solution(new int[] { 1, 2 }, new int[] { 3, 4 }));
     }
 }
