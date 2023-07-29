c7a8100215410644c07a53966e95a02884d83ced
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-07-29 23:31:10 +0900

    개인정보 수집 유효기간

--- src/main/java/com/example/algorithm/java/implement/PersonalInformationCollectionValidity.java
+++ src/main/java/com/example/algorithm/java/implement/PersonalInformationCollectionValidity.java
@@ -1,13 +1,11 @@
+package com.example.algorithm.java.implement;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class TestSolution {
-
+public class PersonalInformationCollectionValidity {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
 
@@ -62,11 +60,11 @@ public class TestSolution {
         return answerArr;
     }
 
-    @Test
-    public void 정답() {
-        Assertions.assertArrayEquals(new int[] { 1, 3 }, solution("2022.05.19", new String[] { "A 6", "B 12", "C 3" },
-                new String[] { "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C" }));
-        Assertions.assertArrayEquals(new int[] { 1, 4, 5 }, solution("2020.01.01", new String[] { "Z 3", "D 5" },
-                new String[] { "2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z" }));
-    }
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertArrayEquals(new int[] { 1, 3 }, solution("2022.05.19", new String[] { "A 6", "B 12", "C 3" },
+    //             new String[] { "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C" }));
+    //     Assertions.assertArrayEquals(new int[] { 1, 4, 5 }, solution("2020.01.01", new String[] { "Z 3", "D 5" },
+    //             new String[] { "2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z" }));
+    // }
 }
