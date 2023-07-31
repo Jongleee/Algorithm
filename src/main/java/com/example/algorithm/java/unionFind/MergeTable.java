ca9da8edfa6814bde284b737176e3790ca9d3d32
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-07-31 23:22:02 +0900

    표 병합

--- src/main/java/com/example/algorithm/java/unionFind/MergeTable.java
+++ src/main/java/com/example/algorithm/java/unionFind/MergeTable.java
@@ -1,12 +1,10 @@
+package com.example.algorithm.java.unionFind;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class TestSolution {
-
+public class MergeTable {
     private int[] parent = new int[2501];
     private String[] value = new String[2501];
 
@@ -108,17 +106,16 @@ public class TestSolution {
         }
         return result.toArray(new String[0]);
     }
-
-    @Test
-    public void 정답() {
-        Assertions.assertArrayEquals(new String[] { "EMPTY", "group" },
-                solution(new String[] { "UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap",
-                        "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean",
-                        "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian",
-                        "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik",
-                        "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4" }));
-        Assertions.assertArrayEquals(new String[] { "d", "EMPTY" },
-                solution(new String[] { "UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2",
-                        "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1" }));
-    }
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertArrayEquals(new String[] { "EMPTY", "group" },
+    //             solution(new String[] { "UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap",
+    //                     "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean",
+    //                     "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian",
+    //                     "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik",
+    //                     "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4" }));
+    //     Assertions.assertArrayEquals(new String[] { "d", "EMPTY" },
+    //             solution(new String[] { "UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2",
+    //                     "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1" }));
+    // }
 }
