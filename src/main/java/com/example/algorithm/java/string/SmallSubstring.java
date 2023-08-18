82814589f6cba8b5525a768f4c1cd8886391479a
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-08-18 20:57:17 +0900

    크기가 작은 부분 문자열

--- src/main/java/com/example/algorithm/java/string/SmallSubstring.java
+++ src/main/java/com/example/algorithm/java/string/SmallSubstring.java
@@ -1,7 +1,6 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+package com.example.algorithm.java.string;
 
-public class TestValue {
+public class SmallSubstring {
     public int solution(String t, String p) {
         int len = p.length();
         long num = Long.parseLong(p);
@@ -16,10 +15,10 @@ public class TestValue {
         return result;
     }
 
-    @Test
-    public void 정답() {
-        Assertions.assertEquals(2, solution("3141592", "271"));
-        Assertions.assertEquals(8, solution("500220839878", "7"));
-        Assertions.assertEquals(3, solution("10203", "15"));
-    }
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertEquals(2, solution("3141592", "271"));
+    //     Assertions.assertEquals(8, solution("500220839878", "7"));
+    //     Assertions.assertEquals(3, solution("10203", "15"));
+    // }
 }
