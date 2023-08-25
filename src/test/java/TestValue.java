7711478ae814b1c433c233cabe56c97fc75fc463
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-08-25 23:45:33 +0900

    내적

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,14 +1,18 @@
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestValue {
-    public long solution(int price, int money, int count) {
-        long totalCost = (long) price * count * (count + 1) / 2;
-        return Math.max(totalCost - money, 0);
+    public int solution(int[] a, int[] b) {
+        return IntStream.range(0, a.length)
+                .map(i -> a[i] * b[i])
+                .sum();
     }
 
     @Test
     public void 정답() {
-        Assertions.assertEquals(10, solution(3, 20, 4));
+        Assertions.assertEquals(3, solution(new int[] { 1, 2, 3, 4 }, new int[] { -3, -1, 0, 2 }));
+        Assertions.assertEquals(-2, solution(new int[] { -1,0,1 }, new int[] {1,0, -1}));
     }
 }
