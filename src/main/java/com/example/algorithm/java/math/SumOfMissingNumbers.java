284e86bb3dbd2e9cfc8f89c15e73722fe1b1499f
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-08-28 21:24:48 +0900

    없는 숫자 더하기

---
+++ src/main/java/com/example/algorithm/java/math/SumOfMissingNumbers.java
@@ -0,0 +1,17 @@
+package com.example.algorithm.java.math;
+
+public class SumOfMissingNumbers {
+    public int solution(int[] numbers) {
+        int temp = 0;
+        for (int i : numbers) {
+            temp += i;
+        }
+        return 45 - temp;
+    }
+
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertEquals(14, solution(new int[] { 1, 2, 3, 4, 6, 7, 8, 0 }));
+    //     Assertions.assertEquals(6, solution(new int[] { 5, 8, 4, 0, 6, 7, 9 }));
+    // }
+}
--- src/main/java/com/example/algorithm/java/string/RectangularStar.java
+++ src/main/java/com/example/algorithm/java/string/RectangularStar.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class RectangularStar {
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < a; i++) {
-            sb.append("*");
-        }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < a; i++) {
+                sb.append("*");
+            }
 
-        for (int i = 0; i < b; i++) {
-            System.out.println(sb);
+            for (int i = 0; i < b; i++) {
+                System.out.println(sb);
+            }
         }
     }
 }
