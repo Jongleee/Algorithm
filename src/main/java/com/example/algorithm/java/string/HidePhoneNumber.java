80c72c7af19f9c0e19fada48dbdf5c9530c8da37
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-08-29 21:55:28 +0900

    핸드폰 번호 가리기

---
+++ src/main/java/com/example/algorithm/java/string/HidePhoneNumber.java
@@ -0,0 +1,17 @@
+package com.example.algorithm.java.string;
+
+public class HidePhoneNumber {
+    public String solution(String phone_number) {
+        int len = phone_number.length() - 4;
+        String asterisks = "*".repeat(len);
+        String lastFourDigits = phone_number.substring(len);
+
+        return asterisks + lastFourDigits;
+    }
+
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertEquals("*******4444", solution("01033334444"));
+    //     Assertions.assertEquals("*****8888", solution("027778888"));
+    // }
+}
