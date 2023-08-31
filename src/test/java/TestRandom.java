2f1bb086d6f35a8ba94f7295d4bca96ab2057eea
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-08-31 15:48:39 +0900

    Random

---
+++ src/test/java/TestRandom.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class TestRandom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int num = scanner.nextInt();
+        while (true) {
+            System.out.println("Choose an number:");
+            if (scanner.hasNextInt()) {
+                if (num <= 5)
+                    break;
+                List<Integer> numbers = new ArrayList<>();
+                numbers = generateUniqueSortedNumbers(random, num);
+                if (num < 10)
+                    numbers = generateRandomNumbers(random, num);
+                System.out.println("=======================");
+                System.out.println(numbers);
+                System.out.println("=======================");
+            } else {
+                break;
+            }
+        }
+        scanner.close();
+    }
+
+    private static List<Integer> generateUniqueSortedNumbers(Random random, int max) {
+        Set<Integer> numberSet = new HashSet<>();
+
+        while (numberSet.size() < 6) {
+            numberSet.add(random.nextInt(max + 1));
+        }
+
+        List<Integer> sortedNumbers = new ArrayList<>(numberSet);
+        Collections.sort(sortedNumbers);
+
+        return sortedNumbers;
+    }
+
+    private static List<Integer> generateRandomNumbers(Random random, int max) {
+        List<Integer> numberList = new ArrayList<>();
+
+        while (numberList.size() < 6) {
+            numberList.add(random.nextInt(max + 1));
+        }
+
+        return numberList;
+    }
+}
