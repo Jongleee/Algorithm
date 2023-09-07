52acea8d654488df4ab9bc8d7411bacf2b1c2d34
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-09-07 14:00:52 +0900

    Random.ints() 사용

--- src/test/java/TestRandom.java
+++ src/test/java/TestRandom.java
@@ -1,26 +1,32 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
 
 public class TestRandom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
         while (true) {
             System.out.println("Choose an number:");
             if (scanner.hasNextInt()) {
+                Random random = new Random();
                 int num = scanner.nextInt();
                 if (num <= 5)
                     break;
                 List<Integer> numbers = new ArrayList<>();
-                numbers = generateUniqueSortedNumbers(random, num);
                 if (num < 10)
-                    numbers = generateRandomNumbers(random, num);
+                    numbers = random.ints(6, 1, num + 1)
+                            .boxed()
+                            .toList();
+                else {
+                    numbers = random.ints(1, num + 1)
+                            .distinct()
+                            .limit(6)
+                            .sorted()
+                            .boxed()
+                            .toList();
+                }
                 System.out.println("=======================");
                 System.out.println(numbers);
                 System.out.println("=======================");
@@ -29,28 +35,5 @@ public class TestRandom {
             }
         }
         scanner.close();
-    }
-
-    private static List<Integer> generateUniqueSortedNumbers(Random random, int max) {
-        Set<Integer> numberSet = new HashSet<>();
-
-        while (numberSet.size() < 6) {
-            numberSet.add(random.nextInt(max + 1));
-        }
-
-        List<Integer> sortedNumbers = new ArrayList<>(numberSet);
-        Collections.sort(sortedNumbers);
-
-        return sortedNumbers;
-    }
-
-    private static List<Integer> generateRandomNumbers(Random random, int max) {
-        List<Integer> numberList = new ArrayList<>();
-
-        while (numberList.size() < 6) {
-            numberList.add(random.nextInt(max + 1));
-        }
-
-        return numberList;
     }
 }
