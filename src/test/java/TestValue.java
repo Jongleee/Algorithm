aa7c7946279c276f9bcffca8dae6fe4031177485
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-09-15 22:20:54 +0900

    N-Queen

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,32 +1,43 @@
-import java.util.Stack;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestValue {
-    boolean solution(String s) {
-        return isCorrect(s);
+    private static int countSolutions = 0;
+
+    public static int solution(int n) {
+        int[] board = new int[n];
+        solveNQueens(board, 0);
+        return countSolutions;
     }
 
-    private boolean isCorrect(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            } else if (!stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else {
-                stack.push(c);
+    private static void solveNQueens(int[] board, int row) {
+        int n = board.length;
+        if (row == n) {
+            countSolutions++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            board[row] = col;
+            if (isSafe(board, row, col)) {
+                solveNQueens(board, row + 1);
             }
         }
-        return stack.isEmpty();
     }
+
+    private static boolean isSafe(int[] board, int row, int col) {
+        for (int prevRow = 0; prevRow < row; prevRow++) {
+            int prevCol = board[prevRow];
+            if (prevCol == col || Math.abs(prevRow - row) == Math.abs(prevCol - col)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     @Test
     public void 정답() {
-        Assertions.assertEquals(true, solution("()()"));
-        Assertions.assertEquals(true, solution("(())()"));
-        Assertions.assertEquals(false, solution(")()("));
-        Assertions.assertEquals(false, solution("(()("));
+        Assertions.assertEquals(2, solution(4));
     }
 }
