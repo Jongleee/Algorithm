06535206e7c6e5e1453b64a2e052f7b944e89bc4
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-09-16 19:53:36 +0900

    네트워크

--- src/main/java/com/example/algorithm/java/searchDFS/Network.java
+++ src/main/java/com/example/algorithm/java/searchDFS/Network.java
@@ -1,27 +1,33 @@
 package com.example.algorithm.java.searchDFS;
 
 public class Network {
-    public static int solution(int n, int[][] computers) {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
         int answer = 0;
-        boolean[] check = new boolean[n];
+
         for (int i = 0; i < n; i++) {
-            if (!check[i]) {
-                dfs(computers, check, i);
+            if (!visited[i]) {
+                dfs(computers, visited, i);
                 answer++;
             }
         }
+
         return answer;
     }
 
-    static void dfs(int[][] computers, boolean[] check, int num) {
-        check[num] = true;
+    static void dfs(int[][] computers, boolean[] visited, int start) {
+        visited[start] = true;
         for (int i = 0; i < computers.length; i++) {
-            if (computers[num][i] == 1 && !check[i]) {
-                dfs(computers, check, i);
+            if (computers[start][i] == 1 && !visited[i]) {
+                dfs(computers, visited, i);
             }
         }
     }
-    public static void main(String[] args) {
-        System.out.println(solution(3, new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
-    }
+
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertEquals(2, solution(3, new int[][] { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } }));
+
+    //     Assertions.assertEquals(1, solution(3, new int[][] { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } }));
+    // }
 }
