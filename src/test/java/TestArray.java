df5a6c88ab836c1211330cb5f5757511144d264d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-09-23 19:57:10 +0900

    부대복귀

--- src/test/java/TestArray.java
+++ src/test/java/TestArray.java
@@ -1,53 +1,66 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestArray {
-    public static int[] solution(String[] maps) {
-        boolean[][] visited = new boolean[maps.length][maps[0].length()];
-        List<Integer> territories = new ArrayList<>();
+    private int[] distances;
+    private List<Integer>[] routes;
+    private int limit = 1000000;
 
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[i].length(); j++) {
-                int territorySize = getTerritorySize(i, j, visited, maps);
-                if (territorySize > 0) {
-                    territories.add(territorySize);
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        initializeGraph(n, roads);
+        distances = new int[n + 1];
+        Arrays.fill(distances, limit);
+
+        bfs(destination);
+
+        int[] answer = new int[sources.length];
+        for (int i = 0; i < sources.length; i++) {
+            answer[i] = (distances[sources[i]] < limit) ? distances[sources[i]] : -1;
+        }
+        return answer;
+    }
+
+    private void initializeGraph(int n, int[][] roads) {
+        routes = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            routes[i] = new ArrayList<>();
+        }
+        for (int[] road : roads) {
+            int start = road[0];
+            int end = road[1];
+            routes[start].add(end);
+            routes[end].add(start);
+        }
+    }
+
+    private void bfs(int destination) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(destination);
+        distances[destination] = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int next : routes[current]) {
+                if (distances[next] > distances[current] + 1) {
+                    distances[next] = distances[current] + 1;
+                    queue.add(next);
                 }
             }
         }
-
-        Collections.sort(territories);
-        return territories.isEmpty() ? new int[] { -1 } : territories.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private static int getTerritorySize(int row, int col, boolean[][] visited, String[] maps) {
-        if (row < 0 || col < 0 || row >= visited.length || col >= visited[0].length || visited[row][col]
-                || maps[row].charAt(col) == 'X') {
-            return 0;
-        }
-
-        visited[row][col] = true;
-        int territorySize = maps[row].charAt(col) - '0';
-        int[] dr = { -1, 1, 0, 0 };
-        int[] dc = { 0, 0, -1, 1 };
-
-        for (int i = 0; i < 4; i++) {
-            int newRow = row + dr[i];
-            int newCol = col + dc[i];
-            territorySize += getTerritorySize(newRow, newCol, visited, maps);
-        }
-
-        return territorySize;
     }
 
     @Test
     public void 정답() {
-        Assertions.assertArrayEquals(new int[] { 1, 1, 27 },
-                solution(new String[] { "X591X", "X1X5X", "X231X", "1XXX1" }));
-        Assertions.assertArrayEquals(new int[] { -1 },
-                solution(new String[] { "XXX", "XXX", "XXX" }));
+        Assertions.assertArrayEquals(new int[] { 2, -1, 0 },
+                solution(3, new int[][] { { 1, 2 }, { 2, 3 } }, new int[] { 2, 3 }, 1));
+        Assertions.assertArrayEquals(new int[] { 2, -1, 0 },
+                solution(5, new int[][] { { 1, 2 }, { 1, 4 }, { 2, 4 }, { 2, 5 }, { 4, 5 } }, new int[] { 1, 3, 5 }, 5));
     }
 }
