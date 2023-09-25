f897eb454a8128fae195b3c821a583a52e2122b5
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-09-25 22:10:59 +0900

    아이템 줍기

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,102 +1,92 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestValue {
-    private static class Location {
-        final int x;
-        final int y;
-        final int depth;
+    private static final int MAX = 101;
+    private boolean[][] board = new boolean[MAX][MAX];
+    private int[][] direction = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-        public Location(int x, int y, int depth) {
-            this.x = x;
-            this.y = y;
-            this.depth = depth;
+    public int solution(int[][] rectangles, int characterX, int characterY, int itemX, int itemY) {
+        initializeBoard(rectangles);
+        int startRow = characterY * 2;
+        int startCol = characterX * 2;
+        int endRow = itemY * 2;
+        int endCol = itemX * 2;
+
+        int totalDistance = findDistance(startRow, startCol, startRow, startCol, new boolean[MAX][MAX], 0) + 1;
+        int distance = findDistance(startRow, startCol, endRow, endCol, new boolean[MAX][MAX], 0);
+
+        return Math.min(distance, totalDistance - distance) / 2;
+    }
+
+    private void initializeBoard(int[][] rectangles) {
+        for (int[] rectangle : rectangles) {
+            int firstRow = 2 * rectangle[1];
+            int firstCol = 2 * rectangle[0];
+            int secondRow = 2 * rectangle[3];
+            int secondCol = 2 * rectangle[2];
+
+            markEdge(firstRow, firstCol, secondRow, secondCol);
+        }
+        for (int[] rectangle : rectangles) {
+            int firstRow = 2 * rectangle[1];
+            int firstCol = 2 * rectangle[0];
+            int secondRow = 2 * rectangle[3];
+            int secondCol = 2 * rectangle[2];
+
+            markSpace(firstRow, firstCol, secondRow, secondCol);
         }
     }
 
-    private static final int[] dx = { -1, 1, 0, 0 };
-    private static final int[] dy = { 0, 0, -1, 1 };
-
-    private int n;
-    private int m;
-
-    public int solution(String[] board) {
-        initializeDimensions(board);
-
-        Location robot = findLocation(board, 'R');
-        Location goal = findLocation(board, 'G');
-
-        return bfs(board, robot, goal);
+    private void markEdge(int firstRow, int firstCol, int secondRow, int secondCol) {
+        for (int row = firstRow; row <= secondRow; row++) {
+            board[row][firstCol] = true;
+        }
+        for (int col = firstCol + 1; col <= secondCol; col++) {
+            board[secondRow][col] = true;
+        }
+        for (int row = secondRow - 1; row >= firstRow; row--) {
+            board[row][secondCol] = true;
+        }
+        for (int col = secondCol - 1; col > firstCol; col--) {
+            board[firstRow][col] = true;
+        }
     }
 
-    private void initializeDimensions(String[] board) {
-        n = board.length;
-        m = board[0].length();
+    private void markSpace(int firstRow, int firstCol, int secondRow, int secondCol) {
+        for (int row = firstRow + 1; row < secondRow; row++) {
+            for (int col = firstCol + 1; col < secondCol; col++) {
+                board[row][col] = false;
+            }
+        }
     }
 
-    private Location findLocation(String[] board, char target) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                char ch = board[i].charAt(j);
+    private int findDistance(int row, int col, int dstRow, int dstCol, boolean[][] visited, int count) {
+        if (count > 0 && row == dstRow && col == dstCol) {
+            return count;
+        }
 
-                if (ch == target) {
-                    return new Location(i, j, 0);
+        visited[row][col] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = row + direction[i][0];
+            int newCol = col + direction[i][1];
+
+            if (newRow >= 0 && newRow < MAX && newCol >= 0 && newCol < MAX) {
+                if (board[newRow][newCol] && !visited[newRow][newCol]) {
+                    return findDistance(newRow, newCol, dstRow, dstCol, visited, count + 1);
                 }
             }
         }
-        return null;
-    }
 
-    private int bfs(String[] board, Location robot, Location goal) {
-        Queue<Location> queue = new LinkedList<>();
-        queue.add(robot);
-
-        boolean[][] visited = new boolean[n][m];
-        visited[robot.x][robot.y] = true;
-
-        while (!queue.isEmpty()) {
-            Location current = queue.poll();
-
-            if (current.x == goal.x && current.y == goal.y) {
-                return current.depth;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x;
-                int ny = current.y;
-
-                while (inBounds(nx, ny) && board[nx].charAt(ny) != 'D') {
-                    nx += dx[i];
-                    ny += dy[i];
-                }
-
-                nx -= dx[i];
-                ny -= dy[i];
-
-                if (visited[nx][ny]) {
-                    continue;
-                }
-
-                visited[nx][ny] = true;
-                queue.add(new Location(nx, ny, current.depth + 1));
-            }
-        }
-
-        return -1;
-    }
-
-    private boolean inBounds(int x, int y) {
-        return x >= 0 && y >= 0 && x < n && y < m;
+        return count;
     }
 
     @Test
     public void 정답() {
-        String[] b1 = { "...D..R", ".D.G...", "....D.D", "D....D.", "..D...." };
-        String[] b2 = { ".D.R", "....", ".G..", "...D" };
-        Assertions.assertEquals(7, solution(b1));
-        Assertions.assertEquals(-1, solution(b2));
+        Assertions.assertEquals(17,
+                solution(new int[][] { { 1, 1, 7, 4 }, { 3, 2, 5, 5 }, { 4, 3, 6, 9 }, { 2, 6, 8, 8 } }, 1, 3, 7, 8));
+        Assertions.assertEquals(11,
+                solution(new int[][] { { 1, 1, 8, 4 }, { 2, 2, 4, 9 }, { 3, 6, 9, 8 }, { 6, 3, 7, 7 } }, 9, 7, 6, 1));
     }
 }
