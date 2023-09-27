f0f5d5bebbff3ca419cdfb7af34c61db54eb68f2
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-09-27 22:57:21 +0900

    동굴 탐험

--- src/main/java/com/example/algorithm/java/searchBroadFirstSearch/FarthestNode.java
+++ src/main/java/com/example/algorithm/java/searchBroadFirstSearch/FarthestNode.java
@@ -53,9 +53,9 @@ public class FarthestNode {
         return depths;
     }
 
-    @Test
-    public void 정답() {
-        Assertions.assertEquals(3,
-                solution(6, new int[][] { { 3, 6 }, { 4, 3 }, { 3, 2 }, { 1, 3 }, { 1, 2 }, { 2, 4 }, { 5, 2 } }));
-    }
+    // @Test
+    // public void 정답() {
+    //     Assertions.assertEquals(3,
+    //             solution(6, new int[][] { { 3, 6 }, { 4, 3 }, { 3, 2 }, { 1, 3 }, { 1, 2 }, { 2, 4 }, { 5, 2 } }));
+    // }
 }
