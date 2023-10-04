545ea9e2774ca0ca96e2be948a45f9590244fe6a
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-10-04 22:10:23 +0900

    하노이의 탑

--- src/test/java/TestArray.java
+++ src/test/java/TestArray.java
@@ -1,74 +1,39 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestArray {
-    private static final int[] dx = { 0, 1, 0, -1 };
-    private static final int[] dy = { -1, 0, 1, 0 };
+    public int[][] solution(int n) {
+        List<int[]> moveList = generateMoves(n, 1, 3, 2);
+        int[][] answer = new int[moveList.size()][];
 
-    public int[] solution(String[][] places) {
-        int[] result = new int[places.length];
-        for (int i = 0; i < places.length; i++) {
-            result[i] = isCorrect(places[i]);
+        for (int i = 0; i < moveList.size(); i++) {
+            answer[i] = moveList.get(i);
         }
-        return result;
+
+        return answer;
     }
 
-    private int isCorrect(String[] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length(); j++) {
-                if (board[i].charAt(j) == 'P') {
-                    if (!isSafe(board, i, j))
-                        return 0;
-                }
-            }
+    private List<int[]> generateMoves(int n, int start, int to, int mid) {
+        List<int[]> moveList = new ArrayList<>();
+
+        if (n == 1) {
+            moveList.add(new int[] { start, to });
+        } else {
+            moveList.addAll(generateMoves(n - 1, start, mid, to));
+            moveList.add(new int[] { start, to });
+            moveList.addAll(generateMoves(n - 1, mid, to, start));
         }
-        return 1;
-    }
 
-    private boolean isSafe(String[] p, int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { x, y });
-        boolean[][] visited = new boolean[p.length][p[0].length()];
-
-        while (!queue.isEmpty()) {
-            int[] temp = queue.poll();
-            int currX = temp[0];
-            int currY = temp[1];
-
-            visited[currX][currY] = true;
-
-            for (int i = 0; i < 4; i++) {
-                int nx = currX + dx[i];
-                int ny = currY + dy[i];
-
-                if (!isValid(p, nx, ny) || visited[nx][ny]) {
-                    continue;
-                }
-
-                int manhattanDistance = Math.abs(x - nx) + Math.abs(y - ny);
-
-                if (p[nx].charAt(ny) == 'P' && manhattanDistance <= 2) {
-                    return false;
-                } else if (p[nx].charAt(ny) == 'O' && manhattanDistance < 2) {
-                    queue.add(new int[] { nx, ny });
-                }
-            }
-        }
-        return true;
-    }
-
-    private boolean isValid(String[] p, int x, int y) {
-        return x >= 0 && y >= 0 && x < p.length && y < p[0].length();
+        return moveList;
     }
 
     @Test
     public void 정답() {
-        String[][] p1 = { { "POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP" },
-                { "POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP" }, { "PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX" },
-                { "OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO" }, { "PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP" } };
-        Assertions.assertArrayEquals(new int[] { 1, 0, 1, 1, 1 }, solution(p1));
+        Assertions.assertArrayEquals(new int[][] { { 1, 2 }, { 1, 3 }, { 2, 3 } }, solution(2));
+        Assertions.assertArrayEquals(
+                new int[][] { { 1, 3 }, { 1, 2 }, { 3, 2 }, { 1, 3 }, { 2, 1 }, { 2, 3 }, { 1, 3 } }, solution(3));
     }
 }
