1d875ec95ed02c78b803d6413ad4941e13ac08af
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-10-07 15:36:59 +0900

    쿼드압축 후 개수 세기

--- src/test/java/TestArray.java
+++ src/test/java/TestArray.java
@@ -1,82 +1,48 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestArray {
-    private List<String> answerList;
-    private Map<String, Integer> hashMap;
+    private int[] answer;
 
-    public String[] solution(String[] orders, int[] course) {
-        answerList = new ArrayList<>();
-        hashMap = new HashMap<>();
-        preprocessOrders(orders);
-        generateCombinations(orders, course);
-        return getAnswer();
+    public int[] solution(int[][] arr) {
+        answer = new int[2];
+        countQuads(arr, 0, 0, arr.length);
+        return answer;
     }
 
-    private void preprocessOrders(String[] orders) {
-        for (int i = 0; i < orders.length; i++) {
-            char[] arr = orders[i].toCharArray();
-            Arrays.sort(arr);
-            orders[i] = String.valueOf(arr);
-        }
-    }
+    private void countQuads(int[][] arr, int x, int y, int size) {
+        int val = arr[x][y];
 
-    private void generateCombinations(String[] orders, int[] course) {
-        for (int courseLength : course) {
-            for (String order : orders) {
-                generateCombination("", order, courseLength);
-            }
-            checkAndAddValidCombinations();
-            hashMap.clear();
-        }
-    }
-
-    private void generateCombination(String current, String remaining, int count) {
-        if (count == 0) {
-            hashMap.put(current, hashMap.getOrDefault(current, 0) + 1);
+        if (isUniform(arr, x, y, size, val)) {
+            answer[val]++;
             return;
         }
 
-        for (int i = 0; i < remaining.length(); i++) {
-            generateCombination(current + remaining.charAt(i), remaining.substring(i + 1), count - 1);
-        }
+        int halfSize = size / 2;
+        countQuads(arr, x, y, halfSize);
+        countQuads(arr, x, y + halfSize, halfSize);
+        countQuads(arr, x + halfSize, y, halfSize);
+        countQuads(arr, x + halfSize, y + halfSize, halfSize);
     }
 
-    private void checkAndAddValidCombinations() {
-        if (!hashMap.isEmpty()) {
-            int maxFrequency = Collections.max(hashMap.values());
-
-            if (maxFrequency > 1) {
-                for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
-                    if (entry.getValue() == maxFrequency) {
-                        answerList.add(entry.getKey());
-                    }
+    private boolean isUniform(int[][] arr, int x, int y, int size, int val) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (arr[i][j] != val) {
+                    return false;
                 }
             }
         }
-    }
-
-    private String[] getAnswer() {
-        Collections.sort(answerList);
-        return answerList.toArray(new String[0]);
+        return true;
     }
 
     @Test
     public void 정답() {
-        String[] o1 = { "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH" };
-        int[] c1 = { 2, 3, 4 };
-        Assertions.assertArrayEquals(new String[] { "AC", "ACDE", "BCFG", "CDE" }, solution(o1, c1));
-        String[] o2 = { "ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD" };
-        int[] c2 = { 2, 3, 5 };
-        Assertions.assertArrayEquals(new String[] { "ACD", "AD", "ADE", "CD", "XYZ" }, solution(o2, c2));
-        String[] o3 = { "XYZ", "XWY", "WXA" };
-        Assertions.assertArrayEquals(new String[] { "WX", "XY" }, solution(o3, c1));
+        int[][] a1 = { { 1, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 1 }, { 1, 1, 1, 1 } };
+        Assertions.assertArrayEquals(new int[] { 4, 9 }, solution(a1));
+        int[][] a2 = { { 1, 1, 1, 1, 1, 1, 1, 1 }, { 0, 1, 1, 1, 1, 1, 1, 1 }, { 0, 0, 0, 0, 1, 1, 1, 1 },
+                { 0, 1, 0, 0, 1, 1, 1, 1 }, { 0, 0, 0, 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 1, 0, 0, 1 }, { 0, 0, 0, 0, 1, 1, 1, 1 } };
+        Assertions.assertArrayEquals(new int[] { 10, 15 }, solution(a2));
     }
 }
