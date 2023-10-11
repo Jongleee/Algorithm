91e70d133734e2d128b2c24b1566db5a604f436f
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-10-11 23:22:34 +0900

    숫자 카드 나누기

--- src/main/java/com/example/algorithm/java/recursive/DivideNumberCard.java
+++ src/main/java/com/example/algorithm/java/recursive/DivideNumberCard.java
@@ -1,29 +1,12 @@
 package com.example.algorithm.java.recursive;
 
 public class DivideNumberCard {
-    public static int solution(int[] arrayA, int[] arrayB) {
-        int length = arrayA.length;
-        int gcdA = arrayA[0];
-        int gcdB = arrayB[0];
+    public int solution(int[] arrayA, int[] arrayB) {
+        int gcdA = calculateGCD(arrayA);
+        int gcdB = calculateGCD(arrayB);
 
-        for (int i = 1; i < length; i++) {
-            gcdA = gcd(gcdA, arrayA[i]);
-            gcdB = gcd(gcdB, arrayB[i]);
-        }
-
-        for (int i = length- 1; i >= 0; i--) {
-            if (arrayA[i] % gcdB == 0) {
-                gcdB = 1;
-                break;
-            }
-        }
-
-        for (int i = length- 1; i >= 0; i--) {
-            if (arrayB[i] % gcdA == 0) {
-                gcdA = 1;
-                break;
-            }
-        }
+        gcdA = updateGCD(gcdA, arrayB);
+        gcdB = updateGCD(gcdB, arrayA);
 
         if (gcdA == gcdB) {
             return 0;
@@ -32,14 +15,41 @@ public class DivideNumberCard {
         }
     }
 
-    public static int gcd(int a, int b) {
+    public int calculateGCD(int[] arr) {
+        int gcd = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            gcd = calculateGCD(gcd, arr[i]);
+        }
+        return gcd;
+    }
+
+    public int calculateGCD(int a, int b) {
         if (a == 0) {
             return b;
         }
-        return gcd(b % a, a);
+        return calculateGCD(b % a, a);
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(new int [] {14, 35, 119}, new int[]	{18, 30, 102}));//7
+    public int updateGCD(int gcd, int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] % gcd == 0) {
+                gcd = 1;
+                break;
+            }
+        }
+        return gcd;
     }
+
+    // @Test
+    // void 정답() {
+    //     int[] a1 = { 10, 17 };
+    //     int[] b1 = { 5, 20 };
+    //     int[] a2 = { 10, 20 };
+    //     int[] b2 = { 5, 17 };
+    //     int[] a3 = { 14, 35, 119 };
+    //     int[] b3 = { 18, 30, 102 };
+    //     Assertions.assertEquals(0, solution(a1, b1));
+    //     Assertions.assertEquals(10, solution(a2, b2));
+    //     Assertions.assertEquals(7, solution(a3, b3));
+    // }
 }
