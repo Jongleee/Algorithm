d093e43370cc81941309a78e77dc9ad80eeb75ec
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-10-12 15:22:14 +0900

    괄호 변환

--- src/main/java/com/example/algorithm/java/recursive/ConvertParentheses.java
+++ src/main/java/com/example/algorithm/java/recursive/ConvertParentheses.java
@@ -6,67 +6,76 @@ public class ConvertParentheses {
     public String solution(String p) {
         return dfs(p);
     }
-    
-    public static String dfs(String w) {
+
+    private String dfs(String w) {
         if (w.isEmpty()) {
             return "";
         }
-        
-        String u = "";
-        String v = "";
+
+        int splitIndex = findSplitIndex(w);
+        String u = w.substring(0, splitIndex + 1);
+        String v = w.substring(splitIndex + 1);
+
+        if (isCorrect(u)) {
+            return u + dfs(v);
+        } else {
+            StringBuilder result = new StringBuilder("(");
+            result.append(dfs(v));
+            result.append(")");
+
+            u = u.substring(1, u.length() - 1);
+
+            for (char ch : u.toCharArray()) {
+                if (ch == '(') {
+                    result.append(')');
+                } else {
+                    result.append('(');
+                }
+            }
+
+            return result.toString();
+        }
+    }
+
+    private int findSplitIndex(String w) {
         int lcnt = 0;
         int rcnt = 0;
-        
+
         for (int i = 0; i < w.length(); i++) {
             if (w.charAt(i) == '(') {
                 lcnt++;
             } else {
                 rcnt++;
             }
-            
-            u += w.charAt(i);
-            
+
             if (lcnt == rcnt) {
-                v = w.substring(i + 1);
-                break;
+                return i;
             }
         }
-        
-        if (isCorrect(u)) {
-            return u + dfs(v);
-        } else {
-            StringBuilder temp = new StringBuilder("(");
-            temp.append(dfs(v));
-            temp.append(")");
-            u = u.substring(1, u.length() - 1);
-            
-            for (int i = 0; i < u.length(); i++) {
-                if (u.charAt(i) == '(') {
-                    temp.append(')');
-                } else {
-                    temp.append('(');
-                }
-            }
-            
-            return temp.toString();
-        }
+
+        return -1;
     }
-    
-    public static boolean isCorrect(String str) {
-        Stack<Character> st = new Stack<>();
-        
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '(') {
-                st.push('(');
+
+    private boolean isCorrect(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
             } else {
-                if (st.isEmpty() || st.peek() == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
                     return false;
-                } else {
-                    st.pop();
                 }
             }
         }
-        
+
         return true;
     }
+
+    // @Test
+    // void 정답() {
+    //     Assertions.assertEquals("(()())()", solution("(()())()"));
+    //     Assertions.assertEquals("()", solution(")("));
+    //     Assertions.assertEquals("()(())()", solution("()))((()"));
+    // }
 }
