20f7d0e16489d8cc51b7f5d3d68331d412149fac
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-10-23 22:59:12 +0900

    기능개발

--- src/main/java/com/example/algorithm/java/queue/DevelopeFeature.java
+++ src/main/java/com/example/algorithm/java/queue/DevelopeFeature.java
@@ -1,32 +1,54 @@
 package com.example.algorithm.java.queue;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class DevelopeFeature {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> daysToComplete = new LinkedList<>();
+
         for (int i = 0; i < speeds.length; i++) {
             int remainingDays = (100 - progresses[i]) / speeds[i];
             if ((100 - progresses[i]) % speeds[i] != 0) {
                 remainingDays++;
             }
-            queue.offer(remainingDays);
+            daysToComplete.offer(remainingDays);
         }
 
-        int[] result = new int[queue.size()];
-        int idx = 0;
-        int prev = 0;
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            if (prev >= curr) {
-                result[idx - 1]++;
+        List<Integer> resultList = new ArrayList<>();
+        int currentTask = daysToComplete.poll();
+        int count = 1;
+
+        while (!daysToComplete.isEmpty()) {
+            int nextTask = daysToComplete.poll();
+            if (currentTask >= nextTask) {
+                count++;
             } else {
-                result[idx] = 1;
-                prev = curr;
-                idx++;
+                resultList.add(count);
+                count = 1;
+                currentTask = nextTask;
             }
         }
 
-        return Arrays.copyOf(result, idx);
+        resultList.add(count);
+
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+
+        return result;
     }
+
+    // @Test
+    // void 정답() {
+    //     int[] p1 = { 93, 30, 55 };
+    //     int[] s1 = { 1, 30, 5 };
+    //     Assertions.assertArrayEquals(new int[] { 2, 1 }, solution(p1, s1));
+    //     int[] p2 = { 95, 90, 99, 99, 80, 99 };
+    //     int[] s2 = { 1, 1, 1, 1, 1, 1 };
+    //     Assertions.assertArrayEquals(new int[] { 1, 3, 2 }, solution(p2, s2));
+    // }
 }
