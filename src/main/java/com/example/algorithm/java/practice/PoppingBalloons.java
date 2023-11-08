4b25083f4de41ad4f5a9c6ae4f492434410e0204
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-08 22:40:12 +0900

    풍선 터트리기

--- src/main/java/com/example/algorithm/java/practice/PoppingBalloons.java
+++ src/main/java/com/example/algorithm/java/practice/PoppingBalloons.java
@@ -1,31 +1,38 @@
 package com.example.algorithm.java.practice;
 
 public class PoppingBalloons {
-    public static void main(String[] args) {
-        int[] a = {-16, 27, 65, -2, 58, -92, -71, -68, -61, -33};
-        System.out.println(solution(a));
-    }
-
-    public static int solution(int[] a) {
+    public int solution(int[] a) {
         int answer = 0;
         int leftMin = 1000000001;
         int rightMin = 1000000001;
-        for (int i = 0; i < a.length; i++) {
+        int n = a.length;
+
+        for (int i = 0; i < n; i++) {
             if (a[i] < leftMin) {
                 leftMin = a[i];
                 answer++;
             }
-            if (a[a.length - i - 1] < rightMin) {
-                rightMin = a[a.length - i - 1];
+            if (a[n - i - 1] < rightMin) {
+                rightMin = a[n - i - 1];
                 answer++;
             }
             if (leftMin == rightMin) {
                 break;
             }
         }
+
         if (leftMin == rightMin) {
             answer--;
         }
+
         return answer;
     }
+
+    // @Test
+    // void 정답() {
+    //     int[] a1 = { 9, -1, -5 };
+    //     int[] a2 = { -16, 27, 65, -2, 58, -92, -71, -68, -61, -33 };
+    //     Assertions.assertEquals(3, solution(a1));
+    //     Assertions.assertEquals(6, solution(a2));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,87 +1,39 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    class Edge implements Comparable<Edge> {
-        int index;
-        int cost;
+    public int solution(int[] a) {
+        int answer = 0;
+        int leftMin = 1000000001;
+        int rightMin = 1000000001;
+        int n = a.length;
 
-        Edge(int index, int cost) {
-            this.index = index;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge e) {
-            return this.cost - e.cost;
-        }
-    }
-
-    private int MAX = 20000001;
-    ArrayList<ArrayList<Edge>> graph;
-
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = MAX;
-
-        graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int[] i : fares) {
-            graph.get(i[0]).add(new Edge(i[1], i[2]));
-            graph.get(i[1]).add(new Edge(i[0], i[2]));
-        }
-
-        int[] startA = dijkstra(a, n);
-        int[] startB = dijkstra(b, n);
-        int[] start = dijkstra(s, n);
-
-        for (int i = 1; i <= n; i++)
-            answer = Math.min(answer, startA[i] + startB[i] + start[i]);
-        return answer;
-    }
-
-    int[] dijkstra(int start, int n) {
-        int[] costs = new int[n + 1];
-        Arrays.fill(costs, MAX);
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start, 0));
-        costs[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Edge now = pq.poll();
-
-            if (now.cost > costs[now.index])
-                continue;
-
-            ArrayList<Edge> edges = graph.get(now.index);
-            for (Edge edge : edges) {
-                int cost = costs[now.index] + edge.cost;
-
-                if (cost < costs[edge.index]) {
-                    costs[edge.index] = cost;
-                    pq.offer(new Edge(edge.index, cost));
-                }
+        for (int i = 0; i < n; i++) {
+            if (a[i] < leftMin) {
+                leftMin = a[i];
+                answer++;
+            }
+            if (a[n - i - 1] < rightMin) {
+                rightMin = a[n - i - 1];
+                answer++;
+            }
+            if (leftMin == rightMin) {
+                break;
             }
         }
-        return costs;
+
+        if (leftMin == rightMin) {
+            answer--;
+        }
+
+        return answer;
     }
 
     @Test
     void 정답() {
-        int[][] f1 = { { 4, 1, 10 }, { 3, 5, 24 }, { 5, 6, 2 }, { 3, 1, 41 },
-                { 5, 1, 24 }, { 4, 6, 50 }, { 2, 4, 66 }, { 2, 3, 22 }, { 1, 6, 25 } };
-        int[][] f2 = { { 5, 7, 9 }, { 4, 6, 4 }, { 3, 6, 1 }, { 3, 2, 3 }, { 2, 1, 6 } };
-        int[][] f3 = { { 2, 6, 6 }, { 6, 3, 7 }, { 4, 6, 7 }, { 6, 5, 11 }, { 2, 5, 12 }, { 5, 3, 20 }, { 2, 4, 8 },
-                { 4, 3, 9 } };
-        Assertions.assertEquals(82, solution(6, 4, 6, 2, f1));
-        Assertions.assertEquals(14, solution(7, 3, 4, 1, f2));
-        Assertions.assertEquals(18, solution(6, 4, 5, 6, f3));
+        int[] a1 = { 9, -1, -5 };
+        int[] a2 = { -16, 27, 65, -2, 58, -92, -71, -68, -61, -33 };
+        Assertions.assertEquals(3, solution(a1));
+        Assertions.assertEquals(6, solution(a2));
     }
 }
