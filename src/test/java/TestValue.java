229d1bb7383f49962f2ff5df08356d1575ab6612
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-10 20:42:31 +0900

    보행자 천국

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,35 +1,36 @@
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int[][] scores) {
-        int[] standard = scores[0];
-        Arrays.sort(scores, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+    public int solution(int m, int n, int[][] cityMap) {
+        int[][][] dp = new int[m + 1][n + 1][2];
+        final int mod = 20170805;
+        dp[0][0][0] = 1;
 
-        int answer = 1;
-        int temp = 0;
-        int standardScore = standard[0] + standard[1];
-
-        for (int[] score : scores) {
-            if (score[1] < temp) {
-                if (Arrays.equals(score, standard)) {
-                    return -1;
-                }
-            } else {
-                temp = Math.max(temp, score[1]);
-                if (score[0] + score[1] > standardScore) {
-                    answer++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                switch (cityMap[i][j]) {
+                    case 0:
+                        dp[i + 1][j][0] += (dp[i][j][0] + dp[i][j][1]) % mod;
+                        dp[i][j + 1][1] += (dp[i][j][0] + dp[i][j][1]) % mod;
+                        break;
+                    case 2:
+                        dp[i + 1][j][0] += dp[i][j][0] % mod;
+                        dp[i][j + 1][1] += dp[i][j][1] % mod;
+                        break;
+                    default:
+                        break;
                 }
             }
         }
-
-        return answer;
+        return (dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % mod;
     }
 
     @Test
     void 정답() {
-        Assertions.assertEquals(4, solution(new int[][] { { 2, 2 }, { 1, 4 }, { 3, 2 }, { 3, 2 }, { 2, 1 } }));
+        int[][] c1 = { { 0, 2, 0, 0, 0, 2 }, { 0, 0, 2, 0, 1, 0 }, { 1, 0, 0, 2, 2, 0 } };
+        Assertions.assertEquals(2, solution(3, 6, c1));
+        int[][] c2 = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        Assertions.assertEquals(6, solution(3, 3, c2));
     }
 }
