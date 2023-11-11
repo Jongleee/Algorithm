1ab78f383a4f51de1abaad9ee7a8387d4ab42b8f
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-11 16:50:56 +0900

    괄호 회전하기

--- src/main/java/com/example/algorithm/java/practice/ParenthesisRotation.java
+++ src/main/java/com/example/algorithm/java/practice/ParenthesisRotation.java
@@ -5,52 +5,56 @@ import java.util.Stack;
 public class ParenthesisRotation {
     public int solution(String s) {
         int answer = 0;
-        for (int i = 0; i <s.length() ; i++) {
+        for (int i = 0; i < s.length(); i++) {
             StringBuilder sb = new StringBuilder(s);
-            String substring=s.substring(0,i);
-            sb.delete(0,i);
+            String substring = s.substring(0, i);
+            sb.delete(0, i);
             sb.append(substring);
-            if(isCorrect(sb))answer+=1;
+            if (isCorrect(sb.toString())) {
+                answer += 1;
+            }
         }
 
         return answer;
     }
 
-    private boolean isCorrect(StringBuilder s) {
+    private boolean isCorrect(String s) {
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if(stack.isEmpty())stack.push(s.charAt(i));
-            else {
-                if (s.charAt(i) == ']') {
-                    popConditionBig(s, stack, i);
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                switch (c) {
+                    case ']':
+                        popCondition(stack, '[');
+                        break;
+                    case '}':
+                        popCondition(stack, '{');
+                        break;
+                    case ')':
+                        popCondition(stack, '(');
+                        break;
+                    default:
+                        stack.push(c);
                 }
-
-                else if(s.charAt(i)=='}'){
-                    popConditionMiddle(s, stack, i);
-                }
-                else if(s.charAt(i)==')'){
-                    popConditionSmall(s, stack, i);
-                }
-                else {stack.push(s.charAt(i));}
             }
         }
         return stack.isEmpty();
     }
 
-    private void popConditionSmall(StringBuilder s, Stack<Character> stack, int i) {
-        if(stack.peek() == '(') stack.pop();
-        else {stack.push(s.charAt(i));}
-    }
-
-    private void popConditionMiddle(StringBuilder s, Stack<Character> stack, int i) {
-        if(stack.peek() == '{') stack.pop();
-        else {stack.push(s.charAt(i));}
-    }
-
-    private void popConditionBig(StringBuilder s, Stack<Character> stack, int i) {
-        if (stack.peek() == '[') stack.pop();
-        else {
-            stack.push(s.charAt(i));
+    private void popCondition(Stack<Character> stack, char openingBracket) {
+        if (!stack.isEmpty() && stack.peek() == openingBracket) {
+            stack.pop();
+        } else {
+            stack.push(openingBracket == '(' ? ')' : (openingBracket == '{' ? '}' : ']'));
         }
     }
+
+    // @Test
+    // void 정답() {
+    //     Assertions.assertEquals(3, solution("[](){}"));
+    //     Assertions.assertEquals(2, solution("}]()[{"));
+    //     Assertions.assertEquals(0, solution("[)(]"));
+    //     Assertions.assertEquals(0, solution("}}}"));
+    // }
 }
