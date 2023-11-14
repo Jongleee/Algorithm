824f66f118d27176fabb90ac633036e1b37c5617
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-14 22:31:42 +0900

    야간 전술 보행

--- src/main/java/com/example/algorithm/java/practice/NightTacticalWork.java
+++ src/main/java/com/example/algorithm/java/practice/NightTacticalWork.java
@@ -6,30 +6,43 @@ import java.util.Collections;
 import java.util.List;
 
 public class NightTacticalWork {
-    public static int solution(int distance, int[][] scope, int[][] times) {
-        List<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < scope.length; i++) {
+    public int solution(int distance, int[][] scope, int[][] times) {
+        List<Integer> validTimes = findValidTimes(scope, times);
+        Collections.sort(validTimes);
 
+        if (!validTimes.isEmpty()) {
+            return validTimes.get(0);
+        }
+        return distance;
+    }
+
+    private List<Integer> findValidTimes(int[][] scope, int[][] times) {
+        List<Integer> validTimes = new ArrayList<>();
+
+        for (int i = 0; i < scope.length; i++) {
             Arrays.sort(scope[i]);
+
             int start = scope[i][0];
             int end = scope[i][1];
             int workTime = times[i][0];
             int restTime = times[i][1];
 
-            while (start<=end){
-                int timeCheck = start%(workTime+restTime);
-                if(timeCheck>0&&timeCheck<=workTime){
-                    temp.add(start);
+            while (start <= end) {
+                int timeCheck = start % (workTime + restTime);
+                if (timeCheck > 0 && timeCheck <= workTime) {
+                    validTimes.add(start);
                     break;
                 }
                 start++;
             }
         }
-        Collections.sort(temp);
-        if(!temp.isEmpty()) return temp.get(0);
-        return distance;
+
+        return validTimes;
     }
-    public static void main(String[] args) {
-        System.out.println(solution(10, new int[][]{{3, 4}, {5, 8}}, new int[][]{{2, 5}, {4, 3}}));
-    }
+
+    // @Test
+    // void 정답() {
+    //     Assertions.assertEquals(8,
+    //             solution(10, new int[][] { { 3, 4 }, { 5, 8 } }, new int[][] { { 2, 5 }, { 4, 3 } }));
+    // }
 }
