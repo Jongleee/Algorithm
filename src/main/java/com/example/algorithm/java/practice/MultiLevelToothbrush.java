d4e78cc48b9fb97e6205e6355a3e1c32c469c9a2
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-17 21:34:21 +0900

    다단계 칫솔 판매

--- src/main/java/com/example/algorithm/java/practice/MultiLevelToothbrush.java
+++ src/main/java/com/example/algorithm/java/practice/MultiLevelToothbrush.java
@@ -4,44 +4,54 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MultiLevelToothbrush {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        Map<String, String> parent = new HashMap<>();
+        Map<String, Integer> memberIndex = new HashMap<>();
 
-    class Solution {
-        public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        initializeMaps(enroll, referral, parent, memberIndex);
 
-            int[] answer = new int[enroll.length];
-            Map<String, String> parent = new HashMap<>();
-            Map<String, Integer> memberIndex = new HashMap<>();
+        calculateProfitsForSellers(seller, amount, answer, parent, memberIndex);
 
-            for (int i = 0; i < enroll.length; i++) {
-                parent.put(enroll[i], referral[i]);
-                memberIndex.put(enroll[i], i);
-            }
-            for (int i = 0; i < seller.length; i++) {
+        return answer;
+    }
 
-                String me = seller[i];
-                int profit = 100 * amount[i];
-
-                while (!me.equals("-")) {
-                    int parentProfit = profit / 10;
-                    int myProfit = profit - parentProfit;
-                    answer[memberIndex.get(me)] += myProfit;
-                    me = parent.get(me);
-                    profit /= 10;
-                    if (profit < 1) {
-                        break;
-                    }
-                }
-            }
-
-            return answer;
-        }
-
-        public static void main(String[] args) {
-            System.out.println(
-                    solution(new String[] { "john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young" },
-                            new String[] { "-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward" },
-                            new String[] { "sam", "emily", "jaimie", "edward" },
-                            new int[] { 2, 3, 5, 4 }));
+    private void initializeMaps(String[] enroll, String[] referral, Map<String, String> parent,
+            Map<String, Integer> memberIndex) {
+        for (int i = 0; i < enroll.length; i++) {
+            parent.put(enroll[i], referral[i]);
+            memberIndex.put(enroll[i], i);
         }
     }
+
+    private void calculateProfitsForSellers(String[] seller, int[] amount, int[] answer, Map<String, String> parent,
+            Map<String, Integer> memberIndex) {
+        for (int i = 0; i < seller.length; i++) {
+            String currentSeller = seller[i];
+            int profit = 100 * amount[i];
+
+            while (!currentSeller.equals("-")) {
+                int parentProfit = profit / 10;
+                int myProfit = profit - parentProfit;
+                answer[memberIndex.get(currentSeller)] += myProfit;
+                currentSeller = parent.get(currentSeller);
+                profit /= 10;
+                if (profit < 1) {
+                    break;
+                }
+            }
+        }
+    }
+
+    // @Test
+    // void 정답() {
+    //     String[] e1 = { "john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young" };
+    //     String[] r1 = { "-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward" };
+    //     String[] s1 = { "sam", "emily", "jaimie", "edward" };
+    //     int[] a1 = { 2, 3, 5, 4 };
+    //     String[] s2 = { "young", "john", "tod", "emily", "mary" };
+    //     int[] a2 = { 12, 4, 2, 5, 10 };
+    //     Assertions.assertArrayEquals(new int[] { 0, 110, 378, 180, 270, 450, 0, 0 }, solution(e1, r1, s1, a1));
+    //     Assertions.assertArrayEquals(new int[] { 360, 958, 108, 0, 450, 18, 180, 1080 }, solution(e1, r1, s2, a2));
+    // }
 }
