44ac24f0507365750ebf8d89bd391edcf3178586
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-20 22:34:51 +0900

    미로 탈출 명령어

--- src/main/java/com/example/algorithm/java/practice/MazeEscapeCommand.java
+++ src/main/java/com/example/algorithm/java/practice/MazeEscapeCommand.java
@@ -1,47 +1,55 @@
 package com.example.algorithm.java.practice;
 
 public class MazeEscapeCommand {
-    public static String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
-        int down = 0;
-        int left = 0;
-        int right = 0;
-        int up = 0;
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
         int distance = Math.abs(x - r) + Math.abs(c - y);
         k -= distance;
-        if (k < 0 || k % 2 != 0||m<y)
-            return "impossible";
-        if (x - r > 0)
-            up = x - r;
-        else
-            down = r - x;
-        if (y - c > 0)
-            left = y - c;
-        else
-            right = c - y;
 
-        answer += "d".repeat(down);
+        if (k < 0 || k % 2 != 0 || m < y) {
+            return "impossible";
+        }
+
+        return calculateMovements(n, m, x, r, y, c, k);
+    }
+
+    private String calculateMovements(int n, int m, int x, int r, int y, int c, int k) {
+        int[] movements = new int[4];
+        StringBuilder moves = new StringBuilder();
+        if (x - r > 0) {
+            movements[1] = x - r;
+        } else {
+            movements[0] = r - x;
+        }
+
+        if (y - c > 0) {
+            movements[2] = y - c;
+        } else {
+            movements[3] = c - y;
+        }
+        int down = movements[0];
+        moves.append("d".repeat(down));
         down = Math.min(k / 2, n - (x + down));
-        answer += "d".repeat(down);
-        up += down;
+        moves.append("d".repeat(down));
+        int up = movements[1] + down;
         k -= 2 * down;
 
-        answer += "l".repeat(left);
-        left = Math.min(k / 2, y - left -1);
-        answer += "l".repeat(left);
-        right += left;
+        int left = movements[2];
+        moves.append("l".repeat(left));
+        left = Math.min(k / 2, y - left - 1);
+        moves.append("l".repeat(left));
+        int right = movements[3] + left;
         k -= 2 * left;
 
-        answer += "rl".repeat(k / 2);
-        answer += "r".repeat(right);
-        answer += "u".repeat(up);
+        moves.append("rl".repeat(k / 2));
+        moves.append("r".repeat(right));
+        moves.append("u".repeat(up));
 
-        return answer;
+        return moves.toString();
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(3, 4, 2, 3, 3, 1, 5));
-    }
-
-    // "dllrl"
+    // @Test
+    // void 정답() {
+    //     Assertions.assertEquals("dllrl", solution(3, 4, 2, 3, 3, 1, 5));
+    //     Assertions.assertEquals("dr", solution(2, 2, 1, 1, 2, 2, 2));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -2,21 +2,55 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int n) {
-        int targetBitCount = Integer.bitCount(n);
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        int distance = Math.abs(x - r) + Math.abs(c - y);
+        k -= distance;
 
-        for (int i = n + 1; i <= 1000000; i++) {
-            if (Integer.bitCount(i) == targetBitCount) {
-                return i;
-            }
+        if (k < 0 || k % 2 != 0 || m < y) {
+            return "impossible";
         }
 
-        return targetBitCount;
+        return calculateMovements(n, m, x, r, y, c, k);
+    }
+
+    private String calculateMovements(int n, int m, int x, int r, int y, int c, int k) {
+        int[] movements = new int[4];
+        StringBuilder moves = new StringBuilder();
+        if (x - r > 0) {
+            movements[1] = x - r;
+        } else {
+            movements[0] = r - x;
+        }
+
+        if (y - c > 0) {
+            movements[2] = y - c;
+        } else {
+            movements[3] = c - y;
+        }
+        int down = movements[0];
+        moves.append("d".repeat(down));
+        down = Math.min(k / 2, n - (x + down));
+        moves.append("d".repeat(down));
+        int up = movements[1] + down;
+        k -= 2 * down;
+
+        int left = movements[2];
+        moves.append("l".repeat(left));
+        left = Math.min(k / 2, y - left - 1);
+        moves.append("l".repeat(left));
+        int right = movements[3] + left;
+        k -= 2 * left;
+
+        moves.append("rl".repeat(k / 2));
+        moves.append("r".repeat(right));
+        moves.append("u".repeat(up));
+
+        return moves.toString();
     }
 
     @Test
     void 정답() {
-        Assertions.assertEquals(83, solution(78));
-        Assertions.assertEquals(23, solution(15));
+        Assertions.assertEquals("dllrl", solution(3, 4, 2, 3, 3, 1, 5));
+        Assertions.assertEquals("dr", solution(2, 2, 1, 1, 2, 2, 2));
     }
 }
