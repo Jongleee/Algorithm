d8f9809eb98fee0762957422b8b1c5d0671cbf4d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-11-23 16:38:33 +0900

    가장 긴 팰린드롬

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,67 +1,35 @@
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    private int[] visit;
-    private long[] temp;
-    private ArrayList<Integer>[] adj;
-    private long answer;
+    public int solution(String s) {
+        char[] chars = s.toCharArray();
 
-    public long solution(int[] a, int[][] edges) {
-        int sum = 0;
-        int n = a.length;
-        visit = new int[n];
-        adj = new ArrayList[n];
-        temp = new long[n];
-        answer = 0;
+        for (int length = s.length(); length > 1; length--) {
+            for (int start = 0; start + length <= s.length(); start++) {
+                boolean isPalindrome = isPalindrome(chars, start, length);
 
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
-            sum += a[i];
-            temp[i] = a[i];
-        }
-
-        if (sum != 0) {
-            return -1;
-        }
-
-        buildGraph(edges);
-
-        dfs(0);
-
-        return answer;
-    }
-
-    private void buildGraph(int[][] edges) {
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].add(v);
-            adj[v].add(u);
-        }
-    }
-
-    private long dfs(int i) {
-        visit[i] = 1;
-        for (int j = 0; j < adj[i].size(); j++) {
-            int next = adj[i].get(j);
-            if (visit[next] == 0) {
-                temp[i] += dfs(next);
+                if (isPalindrome) {
+                    return length;
+                }
             }
         }
-        answer += Math.abs(temp[i]);
-        return temp[i];
+
+        return 1;
+    }
+
+    private boolean isPalindrome(char[] chars, int start, int length) {
+        for (int i = 0; i < length / 2; i++) {
+            if (chars[start + i] != chars[start + length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Test
     void 정답() {
-        int[] a1 = { -5, 0, 2, 1, 2 };
-        int[][] e1 = { { 0, 1 }, { 3, 4 }, { 2, 3 }, { 0, 3 } };
-        int[] a2 = { 0, 1, 0 };
-        int[][] e2 = { { 0, 1 }, { 1, 2 } };
-        Assertions.assertEquals(9, solution(a1, e1));
-        Assertions.assertEquals(-1, solution(a2, e2));
+        Assertions.assertEquals(7, solution("abcdcba"));
+        Assertions.assertEquals(3, solution("abacde"));
     }
 }
