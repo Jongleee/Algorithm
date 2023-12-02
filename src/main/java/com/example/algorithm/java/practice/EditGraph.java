b723fc38cdefa44f6ed04930fc83fcc0730d5489
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-02 16:04:39 +0900

    표 편집

--- src/main/java/com/example/algorithm/java/practice/EditGraph.java
+++ src/main/java/com/example/algorithm/java/practice/EditGraph.java
@@ -3,67 +3,94 @@ package com.example.algorithm.java.practice;
 import java.util.Stack;
 
 public class EditGraph {
-    public static class Node{
+    static class Node {
         int pre;
         int cur;
         int nxt;
-        
+
         public Node(int pre, int cur, int nxt) {
             this.pre = pre;
             this.cur = cur;
             this.nxt = nxt;
         }
     }
-    
-    public static String solution(int n, int k, String[] cmd) {
+
+    public String solution(int n, int k, String[] cmd) {
         int[] pre = new int[n];
         int[] next = new int[n];
-        for(int i = 0; i < n; i++) {
-            pre[i] = i - 1;
-            next[i] = i + 1;
-        }
-        next[n - 1] = -1;
-        
+        initializeLinkedList(pre, next, n);
+
         Stack<Node> stack = new Stack<>();
         StringBuilder sb = new StringBuilder("O".repeat(n));
-        for(int i = 0; i < cmd.length; i++) {
-            char c = cmd[i].charAt(0);
-            if(c == 'U') {
-                int num = Integer.parseInt(cmd[i].substring(2));
-                while(num-- > 0) {
-                    k=pre[k];
-                }
-            } else if(c == 'D') {
-                int num = Integer.parseInt(cmd[i].substring(2));
-                while(num-- > 0) {
-                    k=next[k];
-                }
-            } else if(c == 'C') {
-                k = findC(k, pre, next, stack, sb);
+
+        for (String command : cmd) {
+            char c = command.charAt(0);
+            if (c == 'U') {
+                k = moveUp(k, Integer.parseInt(command.substring(2)), pre);
+            } else if (c == 'D') {
+                k = moveDown(k, Integer.parseInt(command.substring(2)), next);
+            } else if (c == 'C') {
+                k = deleteNode(k, pre, next, stack, sb);
             } else {
-                findO(pre, next, stack, sb);
+                restoreNode(pre, next, stack, sb);
             }
         }
         return sb.toString();
     }
-    private static void findO(int[] pre, int[] next, Stack<Node> stack, StringBuilder sb) {
-        Node node = stack.pop();
-        if(node.pre != -1) next[node.pre] = node.cur;
-        if(node.nxt != -1) pre[node.nxt] = node.cur;
-        sb.setCharAt(node.cur, 'O');
+
+    private void initializeLinkedList(int[] pre, int[] next, int n) {
+        for (int i = 0; i < n; i++) {
+            pre[i] = i - 1;
+            next[i] = i + 1;
+        }
+        next[n - 1] = -1;
     }
-    private static int findC(int k, int[] pre, int[] next, Stack<Node> stack, StringBuilder sb) {
-        stack.push(new Node(pre[k], k, next[k]));
-        if(pre[k] != -1) next[pre[k]] = next[k]; 
-        if(next[k] != -1) pre[next[k]] = pre[k];
-        sb.setCharAt(k, 'X');
-        
-        if(next[k] != -1) k = next[k];
-        else k = pre[k];
+
+    private int moveUp(int k, int num, int[] pre) {
+        while (num-- > 0) {
+            k = pre[k];
+        }
         return k;
     }
-    
-    public static void main(String[] args) {
-        System.out.println(solution(8, 2, new String[]{"D 2","C","U 3","C","D 4","C","U 2","Z","Z"}));
+
+    private int moveDown(int k, int num, int[] next) {
+        while (num-- > 0) {
+            k = next[k];
+        }
+        return k;
     }
+
+    private int deleteNode(int k, int[] pre, int[] next, Stack<Node> stack, StringBuilder sb) {
+        stack.push(new Node(pre[k], k, next[k]));
+
+        if (pre[k] != -1) {
+            next[pre[k]] = next[k];
+        }
+        if (next[k] != -1) {
+            pre[next[k]] = pre[k];
+        }
+
+        sb.setCharAt(k, 'X');
+
+        return (next[k] != -1) ? next[k] : pre[k];
+    }
+
+    private void restoreNode(int[] pre, int[] next, Stack<Node> stack, StringBuilder sb) {
+        Node node = stack.pop();
+        if (node.pre != -1) {
+            next[node.pre] = node.cur;
+        }
+        if (node.nxt != -1) {
+            pre[node.nxt] = node.cur;
+        }
+        sb.setCharAt(node.cur, 'O');
+    }
+
+    // @Test
+    // void 정답() {
+    //     String[] c1 = { "D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z" };
+    //     String[] c2 = { "D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C" };
+    //     Assertions.assertEquals("OOOOXOOO", solution(8, 2, c1));
+    //     Assertions.assertEquals("OOXOXOOO", solution(8, 2, c2));
+    // }
 }
