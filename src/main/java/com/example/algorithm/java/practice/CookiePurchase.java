b16b1939dc4d27fb7e54313abdf0773d3ac7623a
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-05 20:53:26 +0900

    쿠키 구입

--- src/main/java/com/example/algorithm/java/practice/CookiePurchase.java
+++ src/main/java/com/example/algorithm/java/practice/CookiePurchase.java
@@ -1,32 +1,36 @@
 package com.example.algorithm.java.practice;
 
 public class CookiePurchase {
-    public static int solution(int[] cookie) {
+    public int solution(int[] cookie) {
         int answer = 0;
         for (int i = 0; i < cookie.length - 1; i++) {
             int firstIndex = i;
             int secondIndex = i + 1;
             int firstSum = cookie[firstIndex];
             int secondSum = cookie[secondIndex];
-            while (true) {
-                if (firstSum == secondSum && answer < secondSum)
-                    answer = Math.max(answer, secondSum);
-                else if (firstSum <= secondSum && firstIndex > 0)
-                    firstSum += cookie[--firstIndex];
-                else if (firstSum > secondSum && secondIndex < cookie.length - 1)
-                    secondSum += cookie[++secondIndex];
-                else
-                    break;
 
+            while (true) {
+                if (firstSum == secondSum) {
+                    answer = Math.max(answer, secondSum);
+                }
+
+                if (firstSum <= secondSum && firstIndex > 0) {
+                    firstSum += cookie[--firstIndex];
+                } else if (firstSum > secondSum && secondIndex < cookie.length - 1) {
+                    secondSum += cookie[++secondIndex];
+                } else {
+                    break;
+                }
             }
         }
         return answer;
     }
 
-    public static void main(String[] args) {
-        int[] c1 = { 1, 1, 2, 3 };
-        int[] c2 = { 1, 2, 4, 5 };
-        System.out.println(solution(c1));
-        System.out.println(solution(c2));
-    }
+    // @Test
+    // void 정답() {
+    //     int[] c1 = { 1,1,2,3};
+    //     int[] c2 = { 1,2, 4, 5 };
+    //     Assertions.assertEquals(3, solution(c1));
+    //     Assertions.assertEquals(0, solution(c2));
+    // }
 }
