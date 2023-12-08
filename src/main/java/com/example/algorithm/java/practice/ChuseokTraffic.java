e2b280447820dc1607fd91f1dffb3d9f5e5199f7
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-08 22:02:40 +0900

    추석 트래픽

--- src/main/java/com/example/algorithm/java/practice/ChuseokTraffic.java
+++ src/main/java/com/example/algorithm/java/practice/ChuseokTraffic.java
@@ -1,48 +1,78 @@
 package com.example.algorithm.java.practice;
 
 public class ChuseokTraffic {
-    public static int solution(String[] lines) {
-        String[] time = new String[lines.length];
-        convertTime(lines, time);
+    public int solution(String[] lines) {
+        TimeInfo[] timeInfo = convertTime(lines);
         int answer = 0;
-        for (int i = 0; i < time.length; i++) {
-            String[] split = time[i].split(" ");
-            Double area = Double.valueOf(split[0]) + 1;
+
+        for (int i = 0; i < timeInfo.length; i++) {
+            double area = timeInfo[i].endTime + 1;
             int cnt = 1;
-            for (int j = i + 1; j < time.length; j++) {
-                String[] temp = time[j].split(" ");
-                Double start = Double.valueOf(temp[0]) - Double.valueOf(temp[1]) + 0.001;
-                if (start < area)
+
+            for (int j = i + 1; j < timeInfo.length; j++) {
+                if (timeInfo[j].startTime < area) {
                     cnt++;
-                else
-                    continue;
+                }
             }
+
             answer = Math.max(answer, cnt);
         }
+
         return answer;
     }
 
-    private static void convertTime(String[] lines, String[] time) {
+    private TimeInfo[] convertTime(String[] lines) {
+        TimeInfo[] timeInfo = new TimeInfo[lines.length];
+
         for (int i = 0; i < lines.length; i++) {
-            lines[i] = lines[i].substring(11).replace(":", "").replace("s", "");
-            int t = Integer.valueOf(lines[i].substring(0, 2)) * 3600
-                    + Integer.valueOf(lines[i].substring(2, 4)) * 60
-                    + Integer.valueOf(lines[i].substring(4, 6));
-            time[i] = "" + t + lines[i].substring(6);
+            String[] split = lines[i].split(" ");
+            String[] timeParts = split[1].split(":");
+            int hour = Integer.parseInt(timeParts[0]);
+            int minute = Integer.parseInt(timeParts[1]);
+            double second = Double.parseDouble(timeParts[2]);
+            double processingTime = Double.parseDouble(split[2].substring(0, split[2].length() - 1));
+
+            double totalSeconds = hour * 3600 + minute * 60 + second;
+            double startTime = totalSeconds - processingTime + 0.001;
+            double endTime = totalSeconds;
+
+            timeInfo[i] = new TimeInfo(startTime, endTime);
+        }
+
+        return timeInfo;
+    }
+
+    private static class TimeInfo {
+        double startTime;
+        double endTime;
+
+        public TimeInfo(double startTime, double endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(new String[] {
-                "2016-09-15 20:59:57.421 0.351s",
-                "2016-09-15 20:59:58.233 1.181s",
-                "2016-09-15 20:59:58.299 0.8s",
-                "2016-09-15 20:59:58.688 1.041s",
-                "2016-09-15 20:59:59.591 1.412s",
-                "2016-09-15 21:00:00.464 1.466s",
-                "2016-09-15 21:00:00.741 1.581s",
-                "2016-09-15 21:00:00.748 2.31s",
-                "2016-09-15 21:00:00.966 0.381s",
-                "2016-09-15 21:00:02.066 2.62s" }));
-    }
+    // @Test
+    // void 정답() {
+    //     String[] t1 = {
+    //             "2016-09-15 20:59:57.421 0.351s",
+    //             "2016-09-15 20:59:58.233 1.181s",
+    //             "2016-09-15 20:59:58.299 0.8s",
+    //             "2016-09-15 20:59:58.688 1.041s",
+    //             "2016-09-15 20:59:59.591 1.412s",
+    //             "2016-09-15 21:00:00.464 1.466s",
+    //             "2016-09-15 21:00:00.741 1.581s",
+    //             "2016-09-15 21:00:00.748 2.31s",
+    //             "2016-09-15 21:00:00.966 0.381s",
+    //             "2016-09-15 21:00:02.066 2.62s" };
+    //     String[] t2 = {
+    //             "2016-09-15 01:00:04.002 2.0s",
+    //             "2016-09-15 01:00:07.000 2s" };
+    //     String[] t3 = {
+    //             "2016-09-15 01:00:04.001 2.0s",
+    //             "2016-09-15 01:00:07.000 2s" };
+    //     Assertions.assertEquals(7, solution(t1));
+    //     Assertions.assertEquals(2, solution(t2));
+    //     Assertions.assertEquals(1, solution(t3));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -2,65 +2,78 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    private boolean[] visit;
-    private int answer;
+    public int solution(String[] lines) {
+        TimeInfo[] timeInfo = convertTime(lines);
+        int answer = 0;
 
-    public int solution(String begin, String target, String[] words) {
-        visit = new boolean[words.length];
-        answer = 0;
+        for (int i = 0; i < timeInfo.length; i++) {
+            double area = timeInfo[i].endTime + 1;
+            int cnt = 1;
 
-        if (!containsTarget(target, words)) {
-            return 0;
+            for (int j = i + 1; j < timeInfo.length; j++) {
+                if (timeInfo[j].startTime < area) {
+                    cnt++;
+                }
+            }
+
+            answer = Math.max(answer, cnt);
         }
 
-        dfs(begin, target, words, 0);
         return answer;
     }
 
-    private boolean containsTarget(String target, String[] words) {
-        for (String word : words) {
-            if (word.equals(target)) {
-                return true;
-            }
+    private TimeInfo[] convertTime(String[] lines) {
+        TimeInfo[] timeInfo = new TimeInfo[lines.length];
+
+        for (int i = 0; i < lines.length; i++) {
+            String[] split = lines[i].split(" ");
+            String[] timeParts = split[1].split(":");
+            int hour = Integer.parseInt(timeParts[0]);
+            int minute = Integer.parseInt(timeParts[1]);
+            double second = Double.parseDouble(timeParts[2]);
+            double processingTime = Double.parseDouble(split[2].substring(0, split[2].length() - 1));
+
+            double totalSeconds = hour * 3600 + minute * 60 + second;
+            double startTime = totalSeconds - processingTime + 0.001;
+            double endTime = totalSeconds;
+
+            timeInfo[i] = new TimeInfo(startTime, endTime);
         }
-        return false;
+
+        return timeInfo;
     }
 
-    private void dfs(String begin, String target, String[] words, int depth) {
-        if (begin.equals(target)) {
-            answer = depth;
-            return;
-        }
+    private static class TimeInfo {
+        double startTime;
+        double endTime;
 
-        for (int i = 0; i < words.length; i++) {
-            if (visit[i]) {
-                continue;
-            }
-
-            int matchingChars = countMatchingChars(begin, words[i]);
-            if (matchingChars == begin.length() - 1) {
-                visit[i] = true;
-                dfs(words[i], target, words, depth + 1);
-                visit[i] = false;
-            }
+        public TimeInfo(double startTime, double endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
         }
-    }
-
-    private int countMatchingChars(String str1, String str2) {
-        int count = 0;
-        for (int j = 0; j < str1.length(); j++) {
-            if (str1.charAt(j) == str2.charAt(j)) {
-                count++;
-            }
-        }
-        return count;
     }
 
     @Test
     void 정답() {
-        String[] w1 = { "hot", "dot", "dog", "lot", "log", "cog" };
-        String[] w2 = { "hot", "dot", "dog", "lot", "log" };
-        Assertions.assertEquals(4, solution("hit", "cog", w1));
-        Assertions.assertEquals(0, solution("hit", "cog", w2));
+        String[] t1 = {
+                "2016-09-15 20:59:57.421 0.351s",
+                "2016-09-15 20:59:58.233 1.181s",
+                "2016-09-15 20:59:58.299 0.8s",
+                "2016-09-15 20:59:58.688 1.041s",
+                "2016-09-15 20:59:59.591 1.412s",
+                "2016-09-15 21:00:00.464 1.466s",
+                "2016-09-15 21:00:00.741 1.581s",
+                "2016-09-15 21:00:00.748 2.31s",
+                "2016-09-15 21:00:00.966 0.381s",
+                "2016-09-15 21:00:02.066 2.62s" };
+        String[] t2 = {
+                "2016-09-15 01:00:04.002 2.0s",
+                "2016-09-15 01:00:07.000 2s" };
+        String[] t3 = {
+                "2016-09-15 01:00:04.001 2.0s",
+                "2016-09-15 01:00:07.000 2s" };
+        Assertions.assertEquals(7, solution(t1));
+        Assertions.assertEquals(2, solution(t2));
+        Assertions.assertEquals(1, solution(t3));
     }
 }
