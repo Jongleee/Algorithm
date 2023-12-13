61f64a8eab38509723a88549d83858ba4f90ff30
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-13 23:16:16 +0900

    기지국 설치

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,101 +1,43 @@
-import java.util.PriorityQueue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    private class Car implements Comparable<Car> {
-        int x;
-        int y;
-        int dir;
-        int sum;
-
-        public Car(int x, int y, int dir, int sum) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
-            this.sum = sum;
-        }
-
-        @Override
-        public int compareTo(Car o) {
-            return this.sum - o.sum;
-        }
-    }
-
-    private int n;
-    private int[] dx = { 0, 1, 0, -1 };
-    private int[] dy = { 1, 0, -1, 0 };
-
-    public int solution(int[][] board) {
-        n = board.length;
-        return findMinimumSum(board);
-    }
-
-    private int findMinimumSum(int[][] board) {
+    public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int[][][] minSum = new int[n][n][4];
+        int start = 1;
 
-        PriorityQueue<Car> queue = new PriorityQueue<>();
-        addInitialCars(board, minSum, queue);
-
-        while (!queue.isEmpty()) {
-            Car currentCar = queue.poll();
-            int x = currentCar.x;
-            int y = currentCar.y;
-            int dir = currentCar.dir;
-            int sum = currentCar.sum;
-
-            if (x == n - 1 && y == n - 1) {
-                answer = sum;
-                break;
+        for (int station : stations) {
+            if (start < station - w) {
+                answer = calculateStations(start, station - w, w, answer);
             }
-            moveCar(board, minSum, queue, x, y, dir, sum);
+            start = station + w + 1;
+        }
+
+        if (start <= n) {
+            return calculateStations(start, n + 1, w, answer);
         }
 
         return answer;
     }
 
-    private void moveCar(int[][] board, int[][][] minSum, PriorityQueue<Car> queue, int x, int y, int dir, int sum) {
-        for (int d = 0; d < 4; d++) {
-            int row = x + dx[d];
-            int col = y + dy[d];
-            int tempSum = sum + (d == dir ? 100 : 600);
-            if (!isValidMove(row, col, board))
-                continue;
-            if (!isNewPathShorter(minSum, row, col, d, tempSum))
-                continue;
-            queue.add(new Car(row, col, d, tempSum));
-            minSum[row][col][d] = tempSum;
-        }
-    }
+    private int calculateStations(int start, int end, int w, int answer) {
+        int length = end - start;
+        int coverage = w * 2 + 1;
+        int count = length / coverage;
 
-    private boolean isValidMove(int row, int col, int[][] board) {
-        return row >= 0 && row < n && col >= 0 && col < n && board[row][col] == 0;
-    }
-
-    private boolean isNewPathShorter(int[][][] minSum, int row, int col, int direction, int tempSum) {
-        return minSum[row][col][direction] == 0 || minSum[row][col][direction] > tempSum;
-    }
-
-    private void addInitialCars(int[][] board, int[][][] minSum, PriorityQueue<Car> queue) {
-        if (board[0][1] == 0) {
-            queue.add(new Car(0, 1, 0, 100));
-            minSum[0][1][0] = 100;
+        if (length % coverage != 0) {
+            count++;
         }
-        if (board[1][0] == 0) {
-            queue.add(new Car(1, 0, 1, 100));
-            minSum[1][0][1] = 100;
-        }
+
+        answer += count;
+        return answer;
     }
 
     @Test
     void 정답() {
-        int[][] b1 = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-        int[][] b2 = { { 0, 0, 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0 },
-                { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 1 }, { 0, 0, 1, 0, 0, 0, 1, 0 },
-                { 0, 1, 0, 0, 0, 1, 0, 0 }, { 1, 0, 0, 0, 0, 0, 0, 0 } };
-        Assertions.assertEquals(900, solution(b1));
-        Assertions.assertEquals(3800, solution(b2));
+        int[] s1 = { 4, 11 };
+        int[] s2 = { 9 };
+        Assertions.assertEquals(3, solution(11, s1, 1));
+        Assertions.assertEquals(3, solution(16, s2, 2));
     }
 }
