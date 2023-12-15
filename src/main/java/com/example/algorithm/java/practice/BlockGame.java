faefacb1c844570ccd98bfbbf05eece218740fee
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-15 21:31:05 +0900

    블록 게임

--- src/main/java/com/example/algorithm/java/practice/BlockGame.java
+++ src/main/java/com/example/algorithm/java/practice/BlockGame.java
@@ -18,7 +18,6 @@ public class BlockGame {
                         removeBlock(i, j, 3, 2, board);
                         removeCount++;
                     }
-
                 }
             }
             answer += removeCount;
@@ -27,7 +26,7 @@ public class BlockGame {
         return answer;
     }
 
-    public boolean canRemoveBlock(int row, int col, int height, int width, int n, int[][] board) {
+    private boolean canRemoveBlock(int row, int col, int height, int width, int n, int[][] board) {
         if (row + height > n || col + width > n) {
             return false;
         }
@@ -38,19 +37,13 @@ public class BlockGame {
         for (int i = row; i < row + height; i++) {
             for (int j = col; j < col + width; j++) {
                 if (board[i][j] == 0) {
-                    if (!isEmptyAbove(i, j, board)) {
+                    if (!isEmptyAbove(i, j, board) || ++emptyCount > 2) {
                         return false;
                     }
-                    emptyCount++;
-                    if (emptyCount > 2) {
-                        return false;
-                    }
-                } else {
-                    if (value == -1) {
-                        value = board[i][j];
-                    } else if (value != board[i][j]) {
-                        return false;
-                    }
+                } else if (value == -1) {
+                    value = board[i][j];
+                } else if (value != board[i][j]) {
+                    return false;
                 }
             }
         }
@@ -58,7 +51,7 @@ public class BlockGame {
         return true;
     }
 
-    public boolean isEmptyAbove(int row, int col, int[][] board) {
+    private boolean isEmptyAbove(int row, int col, int[][] board) {
         for (int i = 0; i < row; i++) {
             if (board[i][col] != 0) {
                 return false;
@@ -68,11 +61,21 @@ public class BlockGame {
         return true;
     }
 
-    public void removeBlock(int row, int col, int height, int width, int[][] board) {
+    private void removeBlock(int row, int col, int height, int width, int[][] board) {
         for (int i = row; i < row + height; i++) {
             for (int j = col; j < col + width; j++) {
                 board[i][j] = 0;
             }
         }
     }
+
+    // @Test
+    // void 정답() {
+    //     int[][] board = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    //             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    //             { 0, 0, 0, 0, 0, 0, 4, 0, 0, 0 }, { 0, 0, 0, 0, 0, 4, 4, 0, 0, 0 },
+    //             { 0, 0, 0, 0, 3, 0, 4, 0, 0, 0 }, { 0, 0, 0, 2, 3, 0, 0, 0, 5, 5 },
+    //             { 1, 2, 2, 2, 3, 3, 0, 0, 0, 5 }, { 1, 1, 1, 0, 0, 0, 0, 0, 0, 5 } };
+    //     Assertions.assertEquals(2, solution(board));
+    // }
 }
