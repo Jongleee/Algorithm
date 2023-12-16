4c241681b82db041121ea37efb0324abe0f94df3
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-16 14:35:31 +0900

    억억단을 외우자

--- src/main/java/com/example/algorithm/java/practice/BillionBillion.java
+++ src/main/java/com/example/algorithm/java/practice/BillionBillion.java
@@ -3,7 +3,7 @@ package com.example.algorithm.java.practice;
 import java.util.Arrays;
 
 public class BillionBillion {
-    public static int[] solution(int e, int[] starts) {
+    public int[] solution(int e, int[] starts) {
         int[] numbers = new int[e + 1];
         int[] answer = new int[starts.length];
 
@@ -14,25 +14,23 @@ public class BillionBillion {
         }
 
         int[] maxNumber = new int[e + 1];
-        for (int i = 0; i < maxNumber.length; i++) {
-            maxNumber[i] = e;
-        }
+        Arrays.fill(maxNumber, e);
 
         for (int i = e - 1; i > 0; i--) {
-            if (numbers[i] >= numbers[maxNumber[i + 1]]) {
-                maxNumber[i] = i;
-            } else {
-                maxNumber[i] = maxNumber[i + 1];
-            }
+            maxNumber[i] = (numbers[i] >= numbers[maxNumber[i + 1]]) ? i : maxNumber[i + 1];
         }
+
         for (int i = 0; i < starts.length; i++) {
             answer[i] = maxNumber[starts[i]];
         }
+
         return answer;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(8, new int[] { 1, 3, 7 })));
-    }
-    // 답:6,6,8
+    // @Test
+    // void 정답() {
+    //     int[] start = { 1, 3, 7 };
+    //     int[] result = { 6, 6, 8 };
+    //     Assertions.assertArrayEquals(result, solution(8, start));
+    // }
 }
