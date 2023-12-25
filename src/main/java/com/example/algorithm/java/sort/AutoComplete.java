5eb79a6104a2f943390bb53544f67c18b6b77d7d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-25 21:23:46 +0900

    자동완성

--- src/main/java/com/example/algorithm/java/sort/AutoComplete.java
+++ src/main/java/com/example/algorithm/java/sort/AutoComplete.java
@@ -5,32 +5,34 @@ import java.util.Collections;
 import java.util.List;
 
 public class AutoComplete {
-
-    public static int solution(String[] words) {
+    public int solution(String[] words) {
         List<String> sortedWords = Arrays.asList(words);
         Collections.sort(sortedWords);
 
-        int answer = len(sortedWords.get(0), sortedWords.get(1));
-        answer = answer < sortedWords.get(0).length() ? answer + 1 : answer;
+        int answer = commonPrefixLength(sortedWords.get(0), sortedWords.get(1));
+        answer += (answer < sortedWords.get(0).length()) ? 1 : 0;
 
         for (int i = 1; i < sortedWords.size() - 1; i++) {
-            int prevLen = len(sortedWords.get(i), sortedWords.get(i - 1));
-            int nextLen = len(sortedWords.get(i), sortedWords.get(i + 1));
+            int prevLen = commonPrefixLength(sortedWords.get(i), sortedWords.get(i - 1));
+            int nextLen = commonPrefixLength(sortedWords.get(i), sortedWords.get(i + 1));
             int maxLength = Math.max(prevLen, nextLen);
 
-            answer += maxLength < sortedWords.get(i).length() ? maxLength + 1 : maxLength;
+            answer += (maxLength < sortedWords.get(i).length()) ? 1 : 0;
+            answer += maxLength;
         }
 
-        int lastLen = len(sortedWords.get(sortedWords.size() - 2), sortedWords.get(sortedWords.size() - 1));
-        answer += lastLen < sortedWords.get(sortedWords.size() - 1).length() ? lastLen + 1 : lastLen;
+        int lastLen = commonPrefixLength(sortedWords.get(sortedWords.size() - 2),
+                sortedWords.get(sortedWords.size() - 1));
+        answer += (lastLen < sortedWords.get(sortedWords.size() - 1).length()) ? 1 : 0;
 
-        return answer;
+        return answer + lastLen;
     }
 
-    public static int len(String s1, String s2) {
+    private int commonPrefixLength(String s1, String s2) {
         int length = 0;
+        int minLength = Math.min(s1.length(), s2.length());
 
-        for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+        for (int i = 0; i < minLength; i++) {
             if (s1.charAt(i) == s2.charAt(i)) {
                 length++;
             } else {
@@ -41,13 +43,14 @@ public class AutoComplete {
         return length;
     }
 
-    public static void main(String[] args) {
-        String[] w1 = { "go", "gone", "guild" };
-        String[] w2 = { "abc", "def", "ghi", "jklm" };
-        String[] w3 = { "word", "war", "warrior", "world" };
+    // @Test
+    // void 정답() {
+    //     String[] w1 = { "go", "gone", "guild" };
+    //     String[] w2 = { "abc", "def", "ghi", "jklm" };
+    //     String[] w3 = { "word", "war", "warrior", "world" };
 
-        System.out.println(solution(w1));// 7
-        System.out.println(solution(w2));// 4
-        System.out.println(solution(w3));// 15
-    }
+    //     Assertions.assertEquals(7, solution(w1));
+    //     Assertions.assertEquals(4, solution(w2));
+    //     Assertions.assertEquals(15, solution(w3));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,29 +1,57 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int[] citations) {
-        Arrays.sort(citations);
-        int n = citations.length;
-        
-        for (int i = 0; i < n; i++) {
-            int h = n - i;
-            int minCitations = Math.min(citations[i], h);
-            
-            if (minCitations >= h) {
-                return h;
+    public int solution(String[] words) {
+        List<String> sortedWords = Arrays.asList(words);
+        Collections.sort(sortedWords);
+
+        int answer = commonPrefixLength(sortedWords.get(0), sortedWords.get(1));
+        answer += (answer < sortedWords.get(0).length()) ? 1 : 0;
+
+        for (int i = 1; i < sortedWords.size() - 1; i++) {
+            int prevLen = commonPrefixLength(sortedWords.get(i), sortedWords.get(i - 1));
+            int nextLen = commonPrefixLength(sortedWords.get(i), sortedWords.get(i + 1));
+            int maxLength = Math.max(prevLen, nextLen);
+
+            answer += (maxLength < sortedWords.get(i).length()) ? 1 : 0;
+            answer += maxLength;
+        }
+
+        int lastLen = commonPrefixLength(sortedWords.get(sortedWords.size() - 2),
+                sortedWords.get(sortedWords.size() - 1));
+        answer += (lastLen < sortedWords.get(sortedWords.size() - 1).length()) ? 1 : 0;
+
+        return answer + lastLen;
+    }
+
+    private int commonPrefixLength(String s1, String s2) {
+        int length = 0;
+        int minLength = Math.min(s1.length(), s2.length());
+
+        for (int i = 0; i < minLength; i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                length++;
+            } else {
+                break;
             }
         }
 
-        return 0;
+        return length;
     }
-
 
     @Test
     void 정답() {
-        int[] citiations = { 3, 0, 6, 1, 5 };
-        Assertions.assertEquals(3, solution(citiations));
+        String[] w1 = { "go", "gone", "guild" };
+        String[] w2 = { "abc", "def", "ghi", "jklm" };
+        String[] w3 = { "word", "war", "warrior", "world" };
+
+        Assertions.assertEquals(7, solution(w1));
+        Assertions.assertEquals(4, solution(w2));
+        Assertions.assertEquals(15, solution(w3));
     }
 }
