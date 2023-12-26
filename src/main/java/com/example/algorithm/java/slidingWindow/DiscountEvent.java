e660a363ee8c6fda8e753695ed2439e458058fbf
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2023-12-26 20:48:24 +0900

    할인 행사

--- src/main/java/com/example/algorithm/java/slidingWindow/DiscountEvent.java
+++ src/main/java/com/example/algorithm/java/slidingWindow/DiscountEvent.java
@@ -67,4 +67,21 @@ public class DiscountEvent {
         }
         return true;
     }
+
+    // @Test
+    // void 정답() {
+    //     String[] w1 = { "banana", "apple", "rice", "pork", "pot" };
+    //     int[] n1 = { 3, 2, 2, 2, 1 };
+    //     String[] d1 = { "chicken", "apple", "apple", "banana", "rice",
+    //             "apple", "pork", "banana", "pork", "rice", "pot",
+    //             "banana", "apple", "banana" };
+
+    //     String[] w2 = { "apple" };
+    //     int[] n2 = { 10 };
+    //     String[] d2 = { "banana", "banana", "banana", "banana", "banana",
+    //             "banana", "banana", "banana", "banana", "banana" };
+
+    //     Assertions.assertEquals(3, solution(w1, n1, d1));
+    //     Assertions.assertEquals(0, solution(w2, n2, d2));
+    // }
 }
