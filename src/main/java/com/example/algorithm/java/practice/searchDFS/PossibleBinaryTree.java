bd2a2ec4fe86e79dfea56035c3b067fdd939d205
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-01-04 16:06:50 +0900

    표현 가능한 이진트리

--- src/main/java/com/example/algorithm/java/practice/searchDFS/PossibleBinaryTree.java
+++ src/main/java/com/example/algorithm/java/practice/searchDFS/PossibleBinaryTree.java
@@ -1,59 +1,59 @@
 package com.example.algorithm.java.practice.searchDFS;
 
-import java.util.Arrays;
-
 public class PossibleBinaryTree {
-    
-    private static boolean[] node; 
-    private static int result;
+    private int[] answer;
 
-
-    public static int[] solution(long[] numbers) {
-        int[] answer = new int[numbers.length];
+    public int[] solution(long[] numbers) {
+        answer = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            result = 1;
+            answer[i] = 1;
             long number = numbers[i];
-            int numberLength = (int)Math.floor(Math.log(number) / Math.log(2)) + 1;
+            int numberLength = (int) Math.floor(Math.log(number) / Math.log(2)) + 1;
             int exp = 1;
             int treeLength = 0;
-            while(treeLength < numberLength) {
-                treeLength = (int)Math.pow(2, exp++) - 1;
+            while (treeLength < numberLength) {
+                treeLength = (int) Math.pow(2, exp++) - 1;
             }
 
-            node = new boolean[treeLength];
+            boolean[] node = new boolean[treeLength];
             int index = treeLength - 1;
-            while(true) {
+
+            while (number > 0) {
                 long div = number / 2;
                 long mod = number % 2;
                 number = div;
                 node[index--] = (mod == 1);
-                if (div == 1) {
-                    node[index] = true;
-                    break;
-                } else if (div == 0)
+
+                if (div == 0)
                     break;
             }
-            solve(0, treeLength - 1, false);
-            answer[i] = result;
+
+            solve(node, 0, treeLength - 1, false, i);
         }
         return answer;
     }
 
-
-    public static void solve(int s, int e, boolean isEnd) {
+    private void solve(boolean[] node, int s, int e, boolean isEnd, int i) {
         int mid = (s + e) / 2;
         boolean currentNode = node[mid];
+
         if (isEnd && currentNode) {
-            result = 0;
+            answer[i] = 0;
             return;
         }
+
         if (s != e) {
-            solve(s, mid-1, !currentNode);
-            solve(mid+1, e, !currentNode);
+            solve(node, s, mid - 1, !currentNode, i);
+            solve(node, mid + 1, e, !currentNode, i);
         }
     }
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new long[] {63, 111, 95})));
-    }
-    //1,1,0
+
+    // @Test
+    // void 정답() {
+    //     long[] numbers1 = { 7, 42, 5 };
+    //     long[] numbers2 = { 63, 111, 95 };
+    //     int[] result = { 1, 1, 0 };
+    //     Assertions.assertArrayEquals(result, solution(numbers1));
+    //     Assertions.assertArrayEquals(result, solution(numbers2));
+    // }
 }
