95b735e141eb299d86913bcb32ab3e98031b7829
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-01-10 22:58:35 +0900

    게임 맵 최단거리

--- src/main/java/com/example/algorithm/java/practice/searchBFS/GameMapMinDistance.java
+++ src/main/java/com/example/algorithm/java/practice/searchBFS/GameMapMinDistance.java
@@ -4,20 +4,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class GameMapMinDistance {
+    int[] dx = { 1, 0, -1, 0 };
+    int[] dy = { 0, 1, 0, -1 };
+    Queue<int[]> queue = new LinkedList<>();
 
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
-    static Queue<int[]> queue = new LinkedList<>();
-
-    public static int solution(int[][] maps) {
+    public int solution(int[][] maps) {
         int answer = 0;
-
         int[][] visited = new int[maps.length][maps[0].length];
 
-        int x = 0;
-        int y = 0;
-        visited[x][y] = 1;
-        queue.add(new int[] { x, y });
+        visited[0][0] = 1;
+        queue.add(new int[] { 0, 0 });
         bfs(maps, visited);
         answer = visited[maps.length - 1][maps[0].length - 1];
 
@@ -28,8 +24,7 @@ public class GameMapMinDistance {
         return answer;
     }
 
-    public static void bfs(int[][] maps, int[][] visited) {
-
+    public void bfs(int[][] maps, int[][] visited) {
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int currentX = current[0];
@@ -47,16 +42,15 @@ public class GameMapMinDistance {
                     queue.add(new int[] { nX, nY });
                 }
             }
-
         }
-
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(new int[][] { { 1, 0, 1, 1, 1 }, { 1, 0, 1, 0, 1 }, { 1, 0, 1, 1, 1 },
-                { 1, 1, 1, 0, 1 }, { 0, 0, 0, 0, 1 } }));
-        System.out.println(solution(new int[][] { { 1, 0, 1, 1, 1 }, { 1, 0, 1, 0, 1 }, { 1, 0, 1, 1, 1 },
-                { 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1 } }));
-    }
-    // 11 , -1
+    // @Test
+    // void 정답() {
+    //     int[][] maps1 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+    //     int[][] maps2 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
+
+    //     Assertions.assertEquals(11, solution(maps1));
+    //     Assertions.assertEquals(-1, solution(maps2));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -5,118 +5,53 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    class Robot {
-        int x1;
-        int x2;
-        int y1;
-        int y2;
-        int time;
-        int vertical;
+    int[] dx = { 1, 0, -1, 0 };
+    int[] dy = { 0, 1, 0, -1 };
+    Queue<int[]> queue = new LinkedList<>();
 
-        Robot(int x1, int y1, int x2, int y2, int time, int v) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-            this.time = time;
-            this.vertical = v;
-        }
-    }
-
-    boolean[][][] visited;
-
-    public int solution(int[][] board) {
+    public int solution(int[][] maps) {
         int answer = 0;
-        Queue<Robot> q = new LinkedList<>();
-        int[][] op = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-        visited = new boolean[board.length][board.length][2];
+        int[][] visited = new int[maps.length][maps[0].length];
 
-        q.offer(new Robot(0, 0, 0, 1, 0, 0));
+        visited[0][0] = 1;
+        queue.add(new int[] { 0, 0 });
+        bfs(maps, visited);
+        answer = visited[maps.length - 1][maps[0].length - 1];
 
-        while (!q.isEmpty()) {
-            Robot robot = q.poll();
-
-            if (isValid(board, robot) || hasObstacle(board, robot) || visited(robot))
-                continue;
-
-            if (isAtDestination(board, robot)) {
-                answer = robot.time;
-                break;
-            }
-
-            visited[robot.x1][robot.y1][robot.vertical] = true;
-            visited[robot.x2][robot.y2][robot.vertical] = true;
-
-            updateCoordinate(q, op, robot);
-
-            moving(board, q, robot);
+        if (answer == 0) {
+            answer = -1;
         }
 
         return answer;
     }
 
-    private boolean visited(Robot robot) {
-        return visited[robot.x1][robot.y1][robot.vertical] &&
-                visited[robot.x2][robot.y2][robot.vertical];
-    }
+    public void bfs(int[][] maps, int[][] visited) {
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currentX = current[0];
+            int currentY = current[1];
 
-    private boolean isAtDestination(int[][] board, Robot robot) {
-        return (robot.x1 == board.length - 1 && robot.y1 == board.length - 1) ||
-                (robot.x2 == board.length - 1 && robot.y2 == board.length - 1);
-    }
+            for (int i = 0; i < 4; i++) {
+                int nX = currentX + dx[i];
+                int nY = currentY + dy[i];
 
-    private boolean hasObstacle(int[][] board, Robot robot) {
-        return board[robot.x1][robot.y1] == 1 || board[robot.x2][robot.y2] == 1;
-    }
+                if (nX < 0 || nX > maps.length - 1 || nY < 0 || nY > maps[0].length - 1)
+                    continue;
 
-    private boolean isValid(int[][] board, Robot robot) {
-        return robot.x1 < 0 || robot.x1 >= board.length || robot.y1 < 0 || robot.y1 >= board.length ||
-                robot.x2 < 0 || robot.x2 >= board.length || robot.y2 < 0 || robot.y2 >= board.length;
-    }
-
-    private void updateCoordinate(Queue<Robot> q, int[][] op, Robot item) {
-        for (int i = 0; i < op.length; i++) {
-            int nextX1 = item.x1 + op[i][0];
-            int nextY1 = item.y1 + op[i][1];
-            int nextX2 = item.x2 + op[i][0];
-            int nextY2 = item.y2 + op[i][1];
-
-            q.offer(new Robot(nextX1, nextY1, nextX2, nextY2, item.time + 1, item.vertical));
-        }
-    }
-
-    private void moving(int[][] board, Queue<Robot> q, Robot item) {
-        if (item.vertical == 1) {
-            if (item.y1 - 1 >= 0 && board[item.x1][item.y1 - 1] == 0 && board[item.x2][item.y2 - 1] == 0) {
-                q.offer(new Robot(item.x1, item.y1, item.x1, item.y1 - 1, item.time + 1, 0));
-                q.offer(new Robot(item.x2, item.y2, item.x2, item.y2 - 1, item.time + 1, 0));
-            }
-
-            if (item.y1 + 1 <= (board.length - 1) &&
-                    board[item.x1][item.y1 + 1] == 0 && board[item.x2][item.y2 + 1] == 0) {
-                q.offer(new Robot(item.x1, item.y1, item.x1, item.y1 + 1, item.time + 1, 0));
-                q.offer(new Robot(item.x2, item.y2, item.x2, item.y2 + 1, item.time + 1, 0));
-            }
-        } else {
-            if (item.x1 - 1 >= 0 && board[item.x1 - 1][item.y1] == 0 &&
-                    board[item.x2 - 1][item.y2] == 0) {
-                q.offer(new Robot(item.x1, item.y1, item.x1 - 1, item.y1, item.time + 1, 1));
-                q.offer(new Robot(item.x2, item.y2, item.x2 - 1, item.y2, item.time + 1, 1));
-            }
-
-            if (item.x1 + 1 <= (board.length - 1) && board[item.x1 + 1][item.y1] == 0 &&
-                    board[item.x2 + 1][item.y2] == 0) {
-                q.offer(new Robot(item.x1, item.y1, item.x1 + 1, item.y1, item.time + 1, 1));
-                q.offer(new Robot(item.x2, item.y2, item.x2 + 1, item.y2, item.time + 1, 1));
+                if (visited[nX][nY] == 0 && maps[nX][nY] == 1) {
+                    visited[nX][nY] = visited[currentX][currentY] + 1;
+                    queue.add(new int[] { nX, nY });
+                }
             }
         }
     }
 
     @Test
     void 정답() {
-        int[][] board = { { 0, 0, 0, 1, 1 }, { 0, 0, 0, 1, 0 }, { 0, 1, 0, 1, 1 }, { 1, 1, 0, 0, 1 },
-                { 0, 0, 0, 0, 0 } };
+        int[][] maps1 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+        int[][] maps2 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
 
-        Assertions.assertEquals(7, solution(board));
+        Assertions.assertEquals(11, solution(maps1));
+        Assertions.assertEquals(-1, solution(maps2));
     }
 }
