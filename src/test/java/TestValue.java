1c3a21ec3dd4669a8d0854edbc574be9f28a91c4
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-01-12 23:23:58 +0900

    공 이동 시뮬레이션

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,148 +1,60 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    private static final int[] dr = { 1, -1, 0, 0 };
-    private static final int[] dc = { 0, 0, 1, -1 };
-
-    public int solution(int[][] board, int r, int c) {
-        int answer = Integer.MAX_VALUE;
-        int n = getCardCount(board);
-        int[] temp = initializeCards(n);
-        List<int[]> orders = generateCardPermutations(temp);
-
-        for (int[] order : orders) {
-            int total = 0;
-            int[] point = new int[] { r, c };
-            int[][] cBoard = new int[4][4];
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    cBoard[i][j] = board[i][j];
-                }
-            }
-            for (int card : order) {
-                int cnt = 0;
-                cnt += bfs(cBoard, card, point) + 1;
-                cBoard[point[0]][point[1]] = 0;
-                cnt += bfs(cBoard, card, point) + 1;
-                cBoard[point[0]][point[1]] = 0;
-
-                total += cnt;
-            }
-            answer = Math.min(total, answer);
+    public long solution(int n, int m, int x, int y, int[][] queries) {
+        long[] coordinates = { x, y, x, y };
+        for (int i = queries.length - 1; i >= 0; i--) {
+            int direction = queries[i][0];
+            int move = queries[i][1];
+            updateCoordinates(coordinates, direction, move, n, m);
+            if (coordinates[1] >= m || coordinates[3] < 0 || coordinates[0] >= n || coordinates[2] < 0)
+                return 0;
         }
-
-        return answer;
+        return (coordinates[3] - coordinates[1] + 1) * (coordinates[2] - coordinates[0] + 1);
     }
 
-    private int getCardCount(int[][] board) {
-        int count = 0;
-        for (int[] row : board) {
-            for (int card : row) {
-                if (card != 0)
-                    count++;
-            }
-        }
-        return count / 2;
-    }
-
-    private int[] initializeCards(int n) {
-        int[] temp = new int[n];
-        for (int i = 0; i < n; i++) {
-            temp[i] = i + 1;
-        }
-        return temp;
-    }
-
-    private List<int[]> generateCardPermutations(int[] temp) {
-        List<int[]> orders = new ArrayList<>();
-        permutation(temp.length, temp.length, new int[temp.length], temp, 0, 0, orders);
-        return orders;
-    }
-
-    private int bfs(int[][] board, int target, int[] point) {
-        int r = point[0];
-        int c = point[1];
-        Queue<int[]> que = new LinkedList<>();
-        boolean[][] visited = new boolean[4][4];
-
-        que.offer(new int[] { r, c, 0 });
-        visited[r][c] = true;
-
-        while (!que.isEmpty()) {
-            int[] p = que.poll();
-            if (board[p[0]][p[1]] == target) {
-                point[0] = p[0];
-                point[1] = p[1];
-                return p[2];
-            }
-            standardMove(que, visited, p);
-
-            controlMove(board, que, visited, p);
-        }
-        return 0;
-    }
-
-    private void controlMove(int[][] board, Queue<int[]> que, boolean[][] visited, int[] p) {
-        for (int d = 0; d < 4; d++) {
-            int[] result = findCard(board, p[0], p[1], d);
-            if ((result[0] != p[0] || result[1] != p[1]) && !visited[result[0]][result[1]]) {
-                visited[result[0]][result[1]] = true;
-                que.offer(new int[] { result[0], result[1], p[2] + 1 });
-            }
+    private void updateCoordinates(long[] coordinates, int direction, int move, int n, int m) {
+        switch (direction) {
+            case 0:
+                coordinates[1] = findMin(coordinates[1], move);
+                coordinates[3] = Math.min((long) m - 1, coordinates[3] + move);
+                break;
+            case 1:
+                coordinates[3] = findMax(m, coordinates[3], move);
+                coordinates[1] = Math.max(0, coordinates[1] - move);
+                break;
+            case 2:
+                coordinates[0] = findMin(coordinates[0], move);
+                coordinates[2] = Math.min((long) n - 1, coordinates[2] + move);
+                break;
+            case 3:
+                coordinates[2] = findMax(n, coordinates[2], move);
+                coordinates[0] = Math.max(0, coordinates[0] - move);
+                break;
+            default:
+                break;
         }
     }
 
-    private void standardMove(Queue<int[]> que, boolean[][] visited, int[] p) {
-        for (int d = 0; d < 4; d++) {
-            int nr = p[0] + dr[d];
-            int nc = p[1] + dc[d];
-            if (nr >= 0 && nr < 4 && nc >= 0 && nc < 4 && !visited[nr][nc]) {
-                visited[nr][nc] = true;
-                que.offer(new int[] { nr, nc, p[2] + 1 });
-            }
-        }
+    private long findMin(long min, int move) {
+        if (min > 0)
+            min += move;
+        return min;
     }
 
-    private int[] findCard(int[][] board, int r, int c, int d) {
-        r += dr[d];
-        c += dc[d];
-        while (r >= 0 && r < 4 && c >= 0 && c < 4) {
-            if (board[r][c] != 0) {
-                return new int[] { r, c };
-            }
-            r += dr[d];
-            c += dc[d];
-        }
-        return new int[] { r - dr[d], c - dc[d] };
-    }
-
-    private void permutation(int n, int r, int[] perms, int[] input, int depth, int flag, List<int[]> orders) {
-        if (depth == r) {
-            int[] temp = Arrays.copyOf(perms, n);
-            orders.add(temp);
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if ((flag & (1 << i)) == 0) {
-                perms[depth] = input[i];
-                permutation(n, r, perms, input, depth + 1, flag | (1 << i), orders);
-            }
-        }
+    private long findMax(int m, long max, int move) {
+        if (max < m - 1)
+            max -= move;
+        return max;
     }
 
     @Test
     void 정답() {
-        int[][] board1 = { { 1, 0, 0, 3 }, { 2, 0, 0, 0 }, { 0, 0, 0, 2 }, { 3, 0, 1, 0 } };
-        int[][] board2 = { { 3, 0, 0, 2 }, { 0, 0, 1, 0 }, { 0, 1, 0, 0 }, { 2, 0, 0, 3 } };
+        int[][] queries1 = { { 2, 1 }, { 0, 1 }, { 1, 1 }, { 0, 1 }, { 2, 1 } };
+        int[][] queries2 = { { 3, 1 }, { 2, 2 }, { 1, 1 }, { 2, 3 }, { 0, 1 }, { 2, 1 } };
 
-        Assertions.assertEquals(14, solution(board1, 1, 0));
-        Assertions.assertEquals(16, solution(board2, 0, 1));
+        Assertions.assertEquals(4, solution(2, 2, 0, 0, queries1));
+        Assertions.assertEquals(2, solution(2, 5, 0, 1, queries2));
     }
 }
