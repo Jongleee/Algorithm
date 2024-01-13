ee2d3d749bd413f5ecc52e8d7c4bbb1a07f19a3d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-01-13 16:28:22 +0900

    도둑질

--- src/main/java/com/example/algorithm/java/practice/dynamicProgramming/Thievery.java
+++ src/main/java/com/example/algorithm/java/practice/dynamicProgramming/Thievery.java
@@ -1,25 +1,27 @@
 package com.example.algorithm.java.practice.dynamicProgramming;
 
 public class Thievery {
-
-    public static int solution(int[] money) {
+    public int solution(int[] money) {
         int n = money.length;
         int[] dp1 = new int[n];
         int[] dp2 = new int[n];
 
         dp1[0] = money[0];
+        dp1[1] = dp1[0];
         dp2[0] = 0;
-
-        for (int i = 1; i < n; i++) {
-            dp1[i] = Math.max(i >= 2 ? dp1[i - 2] + money[i] : money[i], dp1[i - 1]);
-            dp2[i] = Math.max(i >= 2 ? dp2[i - 2] + money[i] : money[i], dp2[i - 1]);
+        dp2[1] = money[1];
+        for (int i = 2; i < n; i++) {
+            dp1[i] = Math.max(dp1[i - 2] + money[i], dp1[i - 1]);
+            dp2[i] = Math.max(dp2[i - 2] + money[i], dp2[i - 1]);
         }
 
         return Math.max(dp1[n - 2], dp2[n - 1]);
     }
 
-    public static void main(String[] args) {
-        int[] m1 = { 1, 2, 3, 1 };
-        System.out.println(solution(m1));
-    }
+    // @Test
+    // void 정답() {
+    //     int[] money = { 1, 2, 3, 1 };
+
+    //     Assertions.assertEquals(4, solution(money));
+    // }
 }
