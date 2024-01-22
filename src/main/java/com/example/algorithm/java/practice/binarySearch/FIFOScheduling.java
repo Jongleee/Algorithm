b8de791701b1f29adf15f4c2cdaa1b915a371eb7
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-01-22 22:23:18 +0900

    선입 선출 스케줄링

--- src/main/java/com/example/algorithm/java/practice/binarySearch/FIFOScheduling.java
+++ src/main/java/com/example/algorithm/java/practice/binarySearch/FIFOScheduling.java
@@ -1,16 +1,17 @@
 package com.example.algorithm.java.practice.binarySearch;
 
 public class FIFOScheduling {
-
-    public static int solution(int n, int[] cores) {
+    public int solution(int n, int[] cores) {
         int left = -1;
         int right = 100000;
         left = findLeft(n, cores, left, right);
         if (left == -1)
             return n;
+
         int cnt = cores.length;
         for (int i = 0; i < cores.length; i++)
             cnt += left / cores[i];
+
         for (int i = 0; i < cores.length; i++) {
             if ((left + 1) % cores[i] == 0)
                 cnt++;
@@ -18,10 +19,9 @@ public class FIFOScheduling {
                 return i + 1;
         }
         return 0;
-
     }
 
-    private static int findLeft(int n, int[] cores, int left, int right) {
+    private int findLeft(int n, int[] cores, int left, int right) {
         while (left + 1 < right) {
             int mid = (left + right) / 2;
             int cnt = cores.length;
@@ -36,11 +36,10 @@ public class FIFOScheduling {
                 right = mid;
         }
         return left;
-
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(6, new int[] { 1, 2, 3 }));
-    }
-    //답 2
+    // @Test
+    // void 정답() {
+    //     Assertions.assertEquals(2, solution(6, new int[] { 1, 2, 3 }));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -2,77 +2,45 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int alp, int cop, int[][] problems) {
-        int goalAlp = getMaxValue(problems, 0);
-        int goalCop = getMaxValue(problems, 1);
+    public int solution(int n, int[] cores) {
+        int left = -1;
+        int right = 100000;
+        left = findLeft(n, cores, left, right);
+        if (left == -1)
+            return n;
 
-        if (goalAlp <= alp && goalCop <= cop) {
-            return 0;
+        int cnt = cores.length;
+        for (int i = 0; i < cores.length; i++)
+            cnt += left / cores[i];
+
+        for (int i = 0; i < cores.length; i++) {
+            if ((left + 1) % cores[i] == 0)
+                cnt++;
+            if (cnt == n)
+                return i + 1;
         }
-
-        alp = Math.min(alp, goalAlp);
-        cop = Math.min(cop, goalCop);
-
-        int[][] dp = initializeDP(alp, cop, goalAlp, goalCop);
-
-        for (int i = alp; i <= goalAlp; i++) {
-            for (int j = cop; j <= goalCop; j++) {
-                dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + 1);
-                dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
-                updateDP(dp, problems, i, j, goalAlp, goalCop);
-            }
-        }
-
-        return dp[goalAlp][goalCop];
+        return 0;
     }
 
-    private int getMaxValue(int[][] problems, int index) {
-        int maxValue = 0;
-        for (int[] problem : problems) {
-            maxValue = Math.max(maxValue, problem[index]);
-        }
-        return maxValue;
-    }
-
-    private int[][] initializeDP(int alp, int cop, int goalAlp, int goalCop) {
-        int[][] dp = new int[goalAlp + 2][goalCop + 2];
-        for (int i = alp; i <= goalAlp; i++) {
-            for (int j = cop; j <= goalCop; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        dp[alp][cop] = 0;
-        return dp;
-    }
-
-    private void updateDP(int[][] dp, int[][] problems, int i, int j, int goalAlp, int goalCop) {
-        for (int[] problem : problems) {
-            int p0 = problem[0];
-            int p1 = problem[1];
-            int p2 = problem[2];
-            int p3 = problem[3];
-            int p4 = problem[4];
-
-            if (i >= p0 && j >= p1) {
-                if (i + p2 > goalAlp && j + p3 > goalCop) {
-                    dp[goalAlp][goalCop] = Math.min(dp[goalAlp][goalCop], dp[i][j] + p4);
-                } else if (i + p2 > goalAlp) {
-                    dp[goalAlp][j + p3] = Math.min(dp[goalAlp][j + p3], dp[i][j] + p4);
-                } else if (j + p3 > goalCop) {
-                    dp[i + p2][goalCop] = Math.min(dp[i + p2][goalCop], dp[i][j] + p4);
-                } else if (i + p2 <= goalAlp && j + p3 <= goalCop) {
-                    dp[i + p2][j + p3] = Math.min(dp[i + p2][j + p3], dp[i][j] + p4);
+    private int findLeft(int n, int[] cores, int left, int right) {
+        while (left + 1 < right) {
+            int mid = (left + right) / 2;
+            int cnt = cores.length;
+            if (mid > 0) {
+                for (int i = 0; i < cores.length; i++) {
+                    cnt += mid / cores[i];
                 }
             }
+            if (cnt < n)
+                left = mid;
+            else
+                right = mid;
         }
+        return left;
     }
 
     @Test
     void 정답() {
-        int[][] p1 = { { 10, 15, 2, 1, 2 }, { 20, 20, 3, 3, 4 } };
-        int[][] p2 = { { 0, 0, 2, 1, 2 }, { 4, 5, 3, 1, 2 }, { 4, 11, 4, 0, 2 }, { 10, 4, 0, 4, 2 } };
-
-        Assertions.assertEquals(15, solution(10, 10, p1));
-        Assertions.assertEquals(13, solution(0, 0, p2));
+        Assertions.assertEquals(2, solution(6, new int[] { 1, 2, 3 }));
     }
 }
