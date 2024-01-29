1f935e180c070ac1b701400b1ae2ad0efeda7b8a
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-01-29 23:26:47 +0900

    두 원 사이의 정수 쌍

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,73 +1,28 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        int num = picks[0] + picks[1] + picks[2];
-        int[][] section = new int[minerals.length / 5 + 1][3];
-
-        for (int i = 0; i < minerals.length && num > 0; i++) {
-            int index = i / 5;
-            updateSection(section[index], minerals[i]);
-            if (i % 5 == 4) {
-                num--;
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        for (long i = 1; i < r2; i++) {
+            int num2 = (int) Math.sqrt((double) r2 * r2 - i * i);
+            if (i >= r1) {
+                answer += 4 * (num2 + 1);
+            } else {
+                int num1 = (int) Math.sqrt((double) r1 * r1 - i * i);
+                answer += 4 * (num2 - num1);
+                if (Math.sqrt((double) r1 * r1 - i * i) % 1 == 0) {
+                    answer += 4;
+                }
             }
         }
-
-        Arrays.sort(section, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o2[2] - o1[2];
-            }
-        });
-
-        int pick = 0;
-        for (int[] arr : section) {
-            while (pick < 3 && picks[pick] == 0) {
-                pick++;
-            }
-            if (pick == 3) {
-                break;
-            }
-            picks[pick]--;
-            answer += arr[pick];
-        }
-
+        answer += 4;
         return answer;
-    }
-
-    private void updateSection(int[] section, String mineral) {
-        switch (mineral.charAt(0)) {
-            case 'd':
-                section[0]++;
-                section[1] += 5;
-                section[2] += 25;
-                break;
-            case 'i':
-                section[0]++;
-                section[1]++;
-                section[2] += 5;
-                break;
-            default:
-                section[0]++;
-                section[1]++;
-                section[2]++;
-        }
     }
 
     @Test
     void 정답() {
-        int[] picks1 = { 1, 3, 2 };
-        String[] minerals1 = { "diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone" };
-        int[] picks2 = { 0, 1, 1 };
-        String[] minerals2 = { "diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron",
-                "iron", "diamond" };
-
-        Assertions.assertEquals(12, solution(picks1, minerals1));
-        Assertions.assertEquals(50, solution(picks2, minerals2));
+        Assertions.assertEquals(20, solution(2, 3));
+        Assertions.assertEquals(6281440,solution(999999, 1000000));
     }
 }
