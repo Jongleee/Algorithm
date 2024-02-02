a88cb7dfdc5e1a9396b3a707909125f0b3c121eb
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-02 23:59:24 +0900

    가사 검색

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,57 +1,83 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int distance, int[] rocks, int n) {
-        Arrays.sort(rocks);
-        return binarySearch(distance, rocks, n);
-    }
+    public List<Integer> solution(String[] words, String[] queries) {
+        Map<Integer, List<String>> frontMap = new HashMap<>();
+        Map<Integer, List<String>> backMap = new HashMap<>();
 
-    private int binarySearch(int distance, int[] rocks, int n) {
-        int left = 1;
-        int right = distance;
-        int answer = 0;
+        for (String word : words) {
+            int len = word.length();
+            frontMap.computeIfAbsent(len, ArrayList::new).add(word);
+            backMap.computeIfAbsent(len, ArrayList::new).add(reverse(word));
+        }
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int removedRockCnt = countRemovedRocks(rocks, mid, distance);
+        frontMap.values().forEach(Collections::sort);
+        backMap.values().forEach(Collections::sort);
 
-            if (removedRockCnt <= n) {
-                answer = mid;
-                left = mid + 1;
+        List<Integer> answer = new ArrayList<>();
+        for (String query : queries) {
+            List<String> wordList;
+
+            if (query.charAt(0) == '?') {
+                wordList = backMap.get(query.length());
+                query = reverse(query);
             } else {
-                right = mid - 1;
+                wordList = frontMap.get(query.length());
+            }
+
+            if (wordList == null) {
+                answer.add(0);
+            } else {
+                String upperBoundQuery = query.replace('?', Character.MAX_VALUE);
+                String lowerBoundQuery = query.replace("?", "");
+                int upperBound = findBound(wordList, upperBoundQuery);
+                int lowerBound = findBound(wordList, lowerBoundQuery);
+
+                answer.add(upperBound - lowerBound);
             }
         }
+
         return answer;
     }
 
-    private int countRemovedRocks(int[] rocks, int mid, int distance) {
-        int before = 0;
-        int end = distance;
-        int removeCnt = 0;
+    private int findBound(List<String> wordList, String query) {
+        int start = 0;
+        int end = wordList.size();
 
-        for (int rock : rocks) {
-            if (rock - before < mid) {
-                removeCnt++;
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (wordList.get(mid).compareTo(query) >= 0) {
+                end = mid;
             } else {
-                before = rock;
+                start = mid + 1;
             }
         }
 
-        if (end - before < mid) {
-            removeCnt++;
-        }
+        return start;
+    }
 
-        return removeCnt;
+    private String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
     }
 
     @Test
     void 정답() {
-        int[] rocks = { 2, 14, 11, 21, 17 };
-
-        Assertions.assertEquals(4, solution(26, rocks, 2));
+        String[] words = { "frodo", "front", "frost", "frozen", "frame", "kakao" };
+        String[] queries = { "fro??", "????o", "fr???", "fro???", "pro?" };
+        List<Integer> result= new ArrayList<Integer>();
+        result.add(3);
+        result.add(2);
+        result.add(4);
+        result.add(1);
+        result.add(0);
+        Assertions.assertEquals(result , solution(words, queries));
     }
 }
