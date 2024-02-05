8a43a6621a06a73218b73503fcc7783a93cb28b0
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-05 23:41:33 +0900

    입국심사

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,80 +1,40 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public List<Integer> solution(String[] words, String[] queries) {
-        Map<Integer, List<String>> frontMap = new HashMap<>();
-        Map<Integer, List<String>> backMap = new HashMap<>();
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
 
-        for (String word : words) {
-            int len = word.length();
-            frontMap.computeIfAbsent(len, list -> new ArrayList<>()).add(word);
-            backMap.computeIfAbsent(len, list -> new ArrayList<>()).add(reverse(word));
-        }
+        long start = 1;
+        long end = (long) times[times.length - 1] * n;
+        long answer = 0;
 
-        frontMap.values().forEach(Collections::sort);
-        backMap.values().forEach(Collections::sort);
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long sum = 0;
 
-        List<Integer> answer = new ArrayList<>();
-        for (String query : queries) {
-            List<String> wordList;
-
-            if (query.charAt(0) == '?') {
-                wordList = backMap.get(query.length());
-                query = reverse(query);
-            } else {
-                wordList = frontMap.get(query.length());
+            for (int time : times) {
+                sum += mid / time;
             }
 
-            if (wordList == null) {
-                answer.add(0);
+            if (sum >= n) {
+                end = mid - 1;
+                answer = mid;
             } else {
-                String upperBoundQuery = query.replace('?', Character.MAX_VALUE);
-                String lowerBoundQuery = query.replace("?", "");
-                int upperBound = findBound(wordList, upperBoundQuery);
-                int lowerBound = findBound(wordList, lowerBoundQuery);
-
-                answer.add(upperBound - lowerBound);
+                start = mid + 1;
             }
         }
 
         return answer;
     }
 
-    private int findBound(List<String> wordList, String query) {
-        int start = 0;
-        int end = wordList.size();
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (wordList.get(mid).compareTo(query) >= 0) {
-                end = mid;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return start;
-    }
-
-    private String reverse(String s) {
-        return new StringBuilder(s).reverse().toString();
-    }
-
     @Test
     void 정답() {
-        String[] words = { "frodo", "front", "frost", "frozen", "frame", "kakao" };
-        String[] queries = { "fro??", "????o", "fr???", "fro???", "pro?" };
-        List<Integer> result = new ArrayList<>(Arrays.asList(3, 2, 4, 1, 0));
+        int n = 6;
+        int[] times = { 7, 10 };
 
-        Assertions.assertEquals(result, solution(words, queries));
+        Assertions.assertEquals(28, solution(n, times));
     }
 }
