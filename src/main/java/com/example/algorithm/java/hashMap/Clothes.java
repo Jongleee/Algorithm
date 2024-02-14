f612903650bfaaa244bb3200cc7cdfa07087a266
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-14 22:31:18 +0900

    의상

--- src/main/java/com/example/algorithm/java/hashMap/Clothes.java
+++ src/main/java/com/example/algorithm/java/hashMap/Clothes.java
@@ -4,18 +4,28 @@ import java.util.HashMap;
 
 public class Clothes {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> clothesMap = new HashMap<>();
 
         for (String[] clothe : clothes) {
             String type = clothe[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+            clothesMap.put(type, clothesMap.getOrDefault(type, 0) + 1);
         }
 
-        int answer = 1;
-        for (int count : map.values()) {
-            answer *= (count + 1);
+        int totalCombinations = 1;
+        for (int count : clothesMap.values()) {
+            totalCombinations *= (count + 1);
         }
 
-        return answer - 1;
+        return totalCombinations - 1;
     }
+
+    // @Test
+    // void 정답() {
+    //     String[][] clothes1 = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
+    //             { "green_turban", "headgear" } };
+    //     String[][] clothes2 = { { "crow_mask", "face" }, { "blue_sunglasses", "face" }, { "smoky_makeup", "face" } };
+
+    //     Assertions.assertEquals(5, solution(clothes1));
+    //     Assertions.assertEquals(3, solution(clothes2));
+    // }
 }
