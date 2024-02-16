0cd03b83f16f1695c226caf99022014125d122ee
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-16 23:52:08 +0900

    귤 고르기

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,32 +1,39 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(String[][] clothes) {
-        HashMap<String, Integer> clothesMap = new HashMap<>();
-
-        for (String[] clothe : clothes) {
-            String type = clothe[1];
-            clothesMap.put(type, clothesMap.getOrDefault(type, 0) + 1);
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> sizeMap = new HashMap<>();
+        for (int size : tangerine) {
+            sizeMap.put(size, sizeMap.getOrDefault(size, 0) + 1);
         }
 
-        int totalCombinations = 1;
-        for (int count : clothesMap.values()) {
-            totalCombinations *= (count + 1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> sizeMap.get(b) - sizeMap.get(a));
+        pq.addAll(sizeMap.keySet());
+
+        int answer = 0;
+        while (!pq.isEmpty() && k > 0) {
+            int size = pq.poll();
+            int count = Math.min(k, sizeMap.get(size));
+            answer++;
+            k -= count;
         }
 
-        return totalCombinations - 1;
+        return answer;
     }
 
     @Test
     void 정답() {
-        String[][] clothes1 = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
-                { "green_turban", "headgear" } };
-        String[][] clothes2 = { { "crow_mask", "face" }, { "blue_sunglasses", "face" }, { "smoky_makeup", "face" } };
+        int[] tangerine1 = { 1, 3, 2, 5, 4, 5, 2, 3 };
+        int[] tangerine2 = { 1, 3, 2, 5, 4, 5, 2, 3 };
+        int[] tangerine3 = { 1, 1, 1, 1, 2, 2, 2, 3 };
 
-        Assertions.assertEquals(5, solution(clothes1));
-        Assertions.assertEquals(3, solution(clothes2));
+        Assertions.assertEquals(3, solution(6, tangerine1));
+        Assertions.assertEquals(2, solution(4, tangerine2));
+        Assertions.assertEquals(1, solution(2, tangerine3));
     }
 }
