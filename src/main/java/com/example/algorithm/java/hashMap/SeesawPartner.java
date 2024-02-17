8d0ae2bd69e71c92ededa675db0208a19edc0344
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-17 16:04:06 +0900

    시소 짝꿍

--- src/main/java/com/example/algorithm/java/hashMap/SeesawPartner.java
+++ src/main/java/com/example/algorithm/java/hashMap/SeesawPartner.java
@@ -4,7 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SeesawPartner {
-    public boolean isPartner(int f1, int f2) {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Map<Integer, Integer> weightMap = new HashMap<>();
+
+        for (int weight : weights) {
+            weightMap.put(weight, weightMap.getOrDefault(weight, 0) + 1);
+        }
+
+        Integer[] uniqueWeights = weightMap.keySet().toArray(new Integer[0]);
+        Integer[] weightCounts = weightMap.values().toArray(new Integer[0]);
+
+        for (int i = 0; i < uniqueWeights.length - 1; i++) {
+            int count = weightCounts[i];
+            if (count > 1) {
+                answer += (long) count * (count - 1) / 2;
+            }
+            for (int j = i + 1; j < uniqueWeights.length; j++) {
+                int weight1 = uniqueWeights[i];
+                int weight2 = uniqueWeights[j];
+
+                if (isPartner(weight1, weight2)) {
+                    answer += (long) count * weightCounts[j];
+                }
+            }
+        }
+        int lastCount = weightCounts[uniqueWeights.length - 1];
+        if (lastCount > 1) {
+            answer += (long) lastCount * (lastCount - 1) / 2;
+        }
+
+        return answer;
+    }
+
+    private boolean isPartner(int f1, int f2) {
         int[] factors = { 2, 3, 4 };
         for (int factor1 : factors) {
             for (int factor2 : factors) {
@@ -16,29 +49,10 @@ public class SeesawPartner {
         return false;
     }
 
-    public long solution(int[] weights) {
-        long answer = 0;
-        Map<Integer, Integer> weightMap = new HashMap<>();
-        for (int weight : weights) {
-            weightMap.put(weight, weightMap.getOrDefault(weight, 0) + 1);
-        }
-        Integer[] uniqueWeights = weightMap.keySet().toArray(new Integer[0]);
-        Integer[] weightCounts = weightMap.values().toArray(new Integer[0]);
-        for (int i = 0; i < uniqueWeights.length - 1; i++) {
-            int count = weightCounts[i];
-            if (count > 1) {
-                answer += (long) count * (count - 1) / 2;
-            }
-            for (int j = i + 1; j < uniqueWeights.length; j++) {
-                if (isPartner(uniqueWeights[i], uniqueWeights[j])) {
-                    answer += (long) count * weightCounts[j];
-                }
-            }
-        }
-        int count = weightCounts[uniqueWeights.length - 1];
-        if (count > 1) {
-            answer += (long) count * (count - 1) / 2;
-        }
-        return answer;
-    }
+    // @Test
+    // void 정답() {
+    //     int[] weights = { 100, 180, 360, 100, 270 };
+
+    //     Assertions.assertEquals(4, solution(weights));
+    // }
 }
--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,39 +1,59 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int k, int[] tangerine) {
-        Map<Integer, Integer> sizeMap = new HashMap<>();
-        for (int size : tangerine) {
-            sizeMap.put(size, sizeMap.getOrDefault(size, 0) + 1);
+    public long solution(int[] weights) {
+        long answer = 0;
+        Map<Integer, Integer> weightMap = new HashMap<>();
+
+        for (int weight : weights) {
+            weightMap.put(weight, weightMap.getOrDefault(weight, 0) + 1);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> sizeMap.get(b) - sizeMap.get(a));
-        pq.addAll(sizeMap.keySet());
+        Integer[] uniqueWeights = weightMap.keySet().toArray(new Integer[0]);
+        Integer[] weightCounts = weightMap.values().toArray(new Integer[0]);
 
-        int answer = 0;
-        while (!pq.isEmpty() && k > 0) {
-            int size = pq.poll();
-            int count = Math.min(k, sizeMap.get(size));
-            answer++;
-            k -= count;
+        for (int i = 0; i < uniqueWeights.length - 1; i++) {
+            int count = weightCounts[i];
+            if (count > 1) {
+                answer += (long) count * (count - 1) / 2;
+            }
+            for (int j = i + 1; j < uniqueWeights.length; j++) {
+                int weight1 = uniqueWeights[i];
+                int weight2 = uniqueWeights[j];
+
+                if (isPartner(weight1, weight2)) {
+                    answer += (long) count * weightCounts[j];
+                }
+            }
+        }
+        int lastCount = weightCounts[uniqueWeights.length - 1];
+        if (lastCount > 1) {
+            answer += (long) lastCount * (lastCount - 1) / 2;
         }
 
         return answer;
     }
 
+    private boolean isPartner(int f1, int f2) {
+        int[] factors = { 2, 3, 4 };
+        for (int factor1 : factors) {
+            for (int factor2 : factors) {
+                if (f1 * factor1 == f2 * factor2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @Test
     void 정답() {
-        int[] tangerine1 = { 1, 3, 2, 5, 4, 5, 2, 3 };
-        int[] tangerine2 = { 1, 3, 2, 5, 4, 5, 2, 3 };
-        int[] tangerine3 = { 1, 1, 1, 1, 2, 2, 2, 3 };
+        int[] weights = { 100, 180, 360, 100, 270 };
 
-        Assertions.assertEquals(3, solution(6, tangerine1));
-        Assertions.assertEquals(2, solution(4, tangerine2));
-        Assertions.assertEquals(1, solution(2, tangerine3));
+        Assertions.assertEquals(4, solution(weights));
     }
 }
