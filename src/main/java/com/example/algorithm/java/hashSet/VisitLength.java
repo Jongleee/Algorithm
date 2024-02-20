85d64c28d771bd3dfe77a2da7c1ba98eb85547f0
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-20 18:01:22 +0900

    방문 길이

--- src/main/java/com/example/algorithm/java/hashSet/VisitLength.java
+++ src/main/java/com/example/algorithm/java/hashSet/VisitLength.java
@@ -1,66 +1,67 @@
 package com.example.algorithm.java.hashSet;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class VisitLength {
     public int solution(String dirs) {
-        HashSet<String> set = new HashSet<>();
-        int[][] pos = new int[2][2];
+        Set<String> set = new HashSet<>();
+        int[][] pos = new int[1][2];
+        pos[0][0] = 0;
+        pos[0][1] = 0;
 
         for (char direction : dirs.toCharArray()) {
-            pos[1][0] = pos[0][0];
-            pos[1][1] = pos[0][1];
+            int[][] tempPos = new int[2][2];
+            tempPos[0][0] = pos[0][0];
+            tempPos[0][1] = pos[0][1];
 
-            boolean flag = move(pos, direction);
+            move(tempPos, direction);
 
-            if (flag)
-                continue;
+            if (isValidMove(tempPos[0])) {
+                String path1 = posToString(pos[0]) + posToString(tempPos[0]);
+                String path2 = posToString(tempPos[0]) + posToString(pos[0]);
 
-            set.add(posToString(pos[0]) + posToString(pos[1]));
-            set.add(posToString(pos[1]) + posToString(pos[0]));
+                set.add(path1);
+                set.add(path2);
+
+                pos[0][0] = tempPos[0][0];
+                pos[0][1] = tempPos[0][1];
+            }
         }
 
         return set.size() / 2;
     }
 
-    private boolean move(int[][] pos, char direction) {
-        boolean temp = false;
+    private void move(int[][] pos, char direction) {
         switch (direction) {
             case 'U':
                 pos[0][1]++;
-                if (pos[0][1] > 5) {
-                    temp = true;
-                    pos[0][1] = 5;
-                }
                 break;
             case 'D':
                 pos[0][1]--;
-                if (pos[0][1] < -5) {
-                    temp = true;
-                    pos[0][1] = -5;
-                }
                 break;
             case 'R':
                 pos[0][0]++;
-                if (pos[0][0] > 5) {
-                    temp = true;
-                    pos[0][0] = 5;
-                }
                 break;
             case 'L':
                 pos[0][0]--;
-                if (pos[0][0] < -5) {
-                    temp = true;
-                    pos[0][0] = -5;
-                }
                 break;
             default:
                 break;
         }
-        return temp;
+    }
+
+    private boolean isValidMove(int[] pos) {
+        return pos[0] >= -5 && pos[0] <= 5 && pos[1] >= -5 && pos[1] <= 5;
     }
 
     private String posToString(int[] pos) {
         return pos[0] + "," + pos[1];
     }
+
+    // @Test
+    // void 정답() {
+    //     Assertions.assertEquals(7, solution("ULURRDLLU"));
+    //     Assertions.assertEquals(7, solution("LULLLLLLU"));
+    // }
 }
