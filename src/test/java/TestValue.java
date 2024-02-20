6df8411a78a760123ae3932745d35c381ca1fc55
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-20 21:09:15 +0900

    야근 지수

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,42 +1,41 @@
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+    public long solution(int n, int[] works) {
+        long answer = 0;
 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
 
-        int time = 0;
-        int totalResponseTime = 0;
-        int jobsIndex = 0;
-        int count = 0;
-
-        while (count < jobs.length) {
-            while (jobsIndex < jobs.length && jobs[jobsIndex][0] <= time) {
-                minHeap.add(jobs[jobsIndex++]);
-            }
-
-            if (minHeap.isEmpty()) {
-                time = jobs[jobsIndex][0];
-            } else {
-                int[] currentJob = minHeap.poll();
-                totalResponseTime += (time + currentJob[1] - currentJob[0]);
-                time += currentJob[1];
-                count++;
-            }
+        for (int work : works) {
+            maxHeap.offer(work);
         }
 
-        return totalResponseTime / jobs.length;
+        while (n > 0) {
+            int maxWork = maxHeap.poll();
+            if (maxWork == 0)
+                break;
+            maxHeap.offer(maxWork - 1);
+            n--;
+        }
+
+        for (int work : maxHeap) {
+            answer += (long) work * work;
+        }
+
+        return answer;
     }
 
     @Test
     void 정답() {
-        int[][] jobs = { { 0, 3 }, { 1, 9 }, { 2, 6 } };
+        int[] works1 = { 4, 3, 3 };
+        int[] works2 = { 2, 1, 2 };
+        int[] works3 = { 1, 1 };
 
-        Assertions.assertEquals(9, solution(jobs));
+        Assertions.assertEquals(12, solution(4, works1));
+        Assertions.assertEquals(6, solution(1, works2));
+        Assertions.assertEquals(0, solution(3, works3));
     }
 }
