a5022955ddff390a882ee22f2c8b645a5e4a51f9
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-28 22:57:08 +0900

    연속 부분 수열 합의 개수

--- src/main/java/com/example/algorithm/java/implement/ConsecutivePartialSequenceSums.java
+++ src/main/java/com/example/algorithm/java/implement/ConsecutivePartialSequenceSums.java
@@ -7,16 +7,21 @@ public class ConsecutivePartialSequenceSums {
     public int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
 
-        for (int start = 1; start <= elements.length; start++) {
-            for (int i = 0; i < elements.length; i++) {
-                int value = 0;
-                for (int j = i; j < i + start; j++) {
-                    value += elements[j % elements.length];
-                }
-                set.add(value);
+        for (int start = 0; start < elements.length; start++) {
+            int sum = 0;
+            for (int end = start; end < start + elements.length; end++) {
+                sum += elements[end % elements.length];
+                set.add(sum);
             }
         }
 
         return set.size();
     }
+
+    // @Test
+    // void 정답() {
+    //     int[] elements = { 7,9,1,1,4 };
+
+    //     Assertions.assertEquals(18, solution(elements));
+    // }
 }
