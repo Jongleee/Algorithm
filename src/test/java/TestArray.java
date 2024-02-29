9057f2a132d4a354e9f5d8ab65114816bb8c79c0
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-02-29 17:47:07 +0900

    연속된 부분 수열의 합

--- src/test/java/TestArray.java
+++ src/test/java/TestArray.java
@@ -1,37 +1,29 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestArray {
-    public int[] solution(String[] operations) {
-        int[] answer = { 0, 0 };
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    public int[] solution(int[] sequence, int k) {
+        int left = 0;
+        int right = 0;
+        int minLength = Integer.MAX_VALUE;
+        int[] answer = new int[2];
+        int currentSum = 0;
 
-        for (String op : operations) {
-            String[] splitOp = op.split(" ");
-            String command = splitOp[0];
-            int value = Integer.parseInt(splitOp[1]);
-
-            if (command.equals("I")) {
-                maxHeap.add(value);
-                minHeap.add(value);
-            } else if (command.equals("D") && !maxHeap.isEmpty()) {
-                if (value == 1) {
-                    int max = maxHeap.poll();
-                    minHeap.remove(max);
-                } else {
-                    int min = minHeap.poll();
-                    maxHeap.remove(min);
+        while (right < sequence.length) {
+            currentSum += sequence[right];
+            while (currentSum > k) {
+                currentSum -= sequence[left];
+                left++;
+            }
+            if (currentSum == k) {
+                int currentLength = right - left + 1;
+                if (currentLength < minLength) {
+                    minLength = currentLength;
+                    answer[0] = left;
+                    answer[1] = right;
                 }
             }
-        }
-
-        if (!maxHeap.isEmpty()) {
-            answer[0] = maxHeap.peek();
-            answer[1] = minHeap.peek();
+            right++;
         }
 
         return answer;
@@ -39,13 +31,16 @@ class TestArray {
 
     @Test
     void 정답() {
-        String[] operations1 = { "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" };
-        String[] operations2 = { "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" };
+        int[] sequence1 = { 1, 2, 3, 4, 5 };
+        int[] sequence2 = { 1, 1, 1, 2, 3, 4, 5 };
+        int[] sequence3 = { 2, 2, 2, 2, 2 };
 
-        int[] result1 = { 0, 0 };
-        int[] result2 = { 333, -45 };
+        int[] result1 = { 2, 3 };
+        int[] result2 = { 6, 6 };
+        int[] result3 = { 0, 2 };
 
-        Assertions.assertArrayEquals(result1, solution(operations1));
-        Assertions.assertArrayEquals(result2, solution(operations2));
+        Assertions.assertArrayEquals(result1, solution(sequence1, 7));
+        Assertions.assertArrayEquals(result2, solution(sequence2, 5));
+        Assertions.assertArrayEquals(result3, solution(sequence3, 6));
     }
 }
