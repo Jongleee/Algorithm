1f2da298cbdb4192e0dbf8e8af429e2ef9013163
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-03-02 16:40:14 +0900

    수식 최대화

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,78 +1,95 @@
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int m, int n, String[] board) {
-        int answer = 0;
-        char[][] copy = new char[m][n];
+    private long max;
+    private List<Long> operandList;
+    private List<String> operatorList;
 
-        for (int i = 0; i < m; i++) {
-            copy[i] = board[i].toCharArray();
-        }
-
-        boolean flag = true;
-
-        while (flag) {
-            flag = false;
-            boolean[][] v = new boolean[m][n];
-
-            for (int i = 0; i < m - 1; i++) {
-                for (int j = 0; j < n - 1; j++) {
-                    if (copy[i][j] != '-' && check(i, j, copy)) {
-                        v[i][j] = true;
-                        v[i][j + 1] = true;
-                        v[i + 1][j] = true;
-                        v[i + 1][j + 1] = true;
-                        flag = true;
-                    }
-                }
-            }
-
-            answer += erase(m, n, copy, v);
-        }
-
-        return answer;
+    public long solution(String expression) {
+        max = 0;
+        permutation(expression, 0, 3, new String[] { "+", "-", "*" }, new boolean[3], "");
+        return max;
     }
 
-    public boolean check(int x, int y, char[][] board) {
-        char ch = board[x][y];
-        return ch == board[x][y + 1] && ch == board[x + 1][y] && ch == board[x + 1][y + 1];
-    }
-
-    public int erase(int m, int n, char[][] board, boolean[][] v) {
-        int cnt = 0;
-
-        for (int j = 0; j < n; j++) {
-            Queue<Character> q = new LinkedList<>();
-            for (int i = m - 1; i >= 0; i--) {
-                if (v[i][j]) {
-                    cnt++;
-                } else if (board[i][j] != '-') {
-                    q.add(board[i][j]);
-                }
-            }
-
-            for (int i = m - 1; i >= 0; i--) {
-                if (!q.isEmpty()) {
-                    board[i][j] = q.poll();
-                } else {
-                    board[i][j] = '-';
+    private void permutation(String expression, int startDepth, int targetDepth, String[] arr, boolean[] check,
+            String result) {
+        if (startDepth == targetDepth) {
+            calculate(expression, result);
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    permutation(expression, startDepth + 1, targetDepth, arr, check, result + arr[i]);
+                    check[i] = false;
                 }
             }
         }
+    }
 
-        return cnt;
+    private void calculate(String expression, String operators) {
+        operandList = new LinkedList<>();
+        operatorList = new LinkedList<>();
+
+        StringBuilder nextValue = init(expression);
+        operandList.add(Long.parseLong(nextValue.toString()));
+
+        for (int i = 0; i < 3; i++) {
+            String nowOperator = String.valueOf(operators.charAt(i));
+            calculateOperators(nowOperator);
+        }
+
+        max = Math.max(max, Math.abs(operandList.get(0)));
+    }
+
+    private void calculateOperators(String nowOperator) {
+        int size = operatorList.size();
+        for (int i = 0; i < size; i++) {
+            String op = operatorList.get(i);
+            if (op.equals(nowOperator)) {
+                long operand1 = operandList.remove(i);
+                long operand2 = operandList.remove(i);
+                long result = 0;
+                switch (nowOperator) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                }
+                operandList.add(i, result);
+                operatorList.remove(i);
+                i--;
+                size--;
+            }
+        }
+    }
+
+    private StringBuilder init(String expression) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '+' || ch == '-' || ch == '*') {
+                operandList.add(Long.parseLong(result.toString()));
+                result = new StringBuilder();
+                operatorList.add(String.valueOf(ch));
+            } else {
+                result.append(ch);
+            }
+        }
+        return result;
     }
 
     @Test
     void 정답() {
-        String[] board1 = { "CCBDE", "AAADE", "AAABF", "CCBBF" };
-        String[] board2 = { "TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ" };
-
-        Assertions.assertEquals(14, solution(4, 5, board1));
-        Assertions.assertEquals(15, solution(6, 6, board2));
+        Assertions.assertEquals(60420, solution("100-200*300-500+20"));
+        Assertions.assertEquals(300, solution("50*6-3*2"));
     }
 }
