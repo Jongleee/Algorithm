d6510ee522af1b68d9377c4ecd9b51e301542661
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-03-07 16:15:46 +0900

    행렬 테두리 회전하기

--- src/main/java/com/example/algorithm/java/implement/RotateSquareEdge.java
+++ src/main/java/com/example/algorithm/java/implement/RotateSquareEdge.java
@@ -2,22 +2,14 @@ package com.example.algorithm.java.implement;
 
 public class RotateSquareEdge {
     public int[] solution(int rows, int columns, int[][] queries) {
-        int[][] square = initSquare(rows, columns);
-        return rotateNums(square, queries);
-    }
-
-    public int[][] initSquare(int rows, int columns) {
         int[][] square = new int[rows][columns];
         int value = 1;
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                square[i][j] = value;
-                value++;
+                square[i][j] = value++;
             }
         }
-        
-        return square;
+        return rotateNums(square, queries);
     }
 
     public int[] rotateNums(int[][] square, int[][] queries) {
@@ -29,34 +21,51 @@ public class RotateSquareEdge {
             int y1 = query[1] - 1;
             int x2 = query[2] - 1;
             int y2 = query[3] - 1;
-            int firstNum = square[x1][y2];
-            int min = firstNum;
+            int min = Integer.MAX_VALUE;
 
-            for (int i = y2 - 1; i >= y1; i--) {
-                min = Math.min(min, square[x1][i]);
-                square[x1][i + 1] = square[x1][i];
-            }
+            int prev = square[x1][y1];
+            min = Math.min(min, prev);
 
-            for (int i = x1 + 1; i <= x2; i++) {
+            for (int i = x1; i < x2; i++) {
+                square[i][y1] = square[i + 1][y1];
                 min = Math.min(min, square[i][y1]);
-                square[i - 1][y1] = square[i][y1];
             }
 
-            for (int i = y1 + 1; i <= y2; i++) {
+            for (int i = y1; i < y2; i++) {
+                square[x2][i] = square[x2][i + 1];
                 min = Math.min(min, square[x2][i]);
-                square[x2][i - 1] = square[x2][i];
             }
 
-            for (int i = x2 - 1; i >= x1; i--) {
+            for (int i = x2; i > x1; i--) {
+                square[i][y2] = square[i - 1][y2];
                 min = Math.min(min, square[i][y2]);
-                square[i + 1][y2] = square[i][y2];
             }
 
-            square[x1 + 1][y2] = firstNum;
-            answer[index] = min;
-            index++;
+            for (int i = y2; i > y1 + 1; i--) {
+                square[x1][i] = square[x1][i - 1];
+                min = Math.min(min, square[x1][i]);
+            }
+
+            square[x1][y1 + 1] = prev;
+            answer[index++] = min;
         }
 
         return answer;
     }
+
+    // @Test
+    // void 정답() {
+    //     int[] rows = { 6, 3, 100 };
+    //     int[] columns = { 6, 3, 97 };
+
+    //     int[][][] queries = { { { 2, 2, 5, 4 }, { 3, 3, 6, 6 }, { 5, 1, 6, 3 } },
+    //             { { 1, 1, 2, 2 }, { 1, 2, 2, 3 }, { 2, 1, 3, 2 }, { 2, 2, 3, 3 } },
+    //             { { 1, 1, 100, 97 } } };
+
+    //     int[][] result = { { 8, 10, 25 }, { 1, 1, 5, 3 }, { 1 } };
+
+    //     for (int i = 0; i < result.length; i++) {
+    //         Assertions.assertArrayEquals(result[i], solution(rows[i], columns[i], queries[i]));
+    //     }
+    // }
 }
--- src/test/java/TestArray.java
+++ src/test/java/TestArray.java
@@ -1,41 +1,54 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestArray {
-    public String[] solution(String[] record) {
-        List<String> chatLog = new ArrayList<>();
-        Map<String, String> nickMap = new HashMap<>();
-
-        for (String log : record) {
-            StringTokenizer st = new StringTokenizer(log);
-            String command = st.nextToken();
-            String userId = st.nextToken();
-
-            if (command.equals("Enter") || command.equals("Change")) {
-                String nickname = st.nextToken();
-                nickMap.put(userId, nickname);
-            }
-
-            if (command.equals("Enter")) {
-                chatLog.add(userId + "님이 들어왔습니다.");
-            } else if (command.equals("Leave")) {
-                chatLog.add(userId + "님이 나갔습니다.");
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[][] square = new int[rows][columns];
+        int value = 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                square[i][j] = value++;
             }
         }
+        return rotateNums(square, queries);
+    }
 
-        String[] answer = new String[chatLog.size()];
-        int logIdx = 0;
+    public int[] rotateNums(int[][] square, int[][] queries) {
+        int[] answer = new int[queries.length];
+        int index = 0;
 
-        for (String str : chatLog) {
-            int endOfId = str.indexOf("님");
-            String userId = str.substring(0, endOfId);
-            answer[logIdx++] = str.replace(userId, nickMap.get(userId));
+        for (int[] query : queries) {
+            int x1 = query[0] - 1;
+            int y1 = query[1] - 1;
+            int x2 = query[2] - 1;
+            int y2 = query[3] - 1;
+            int min = Integer.MAX_VALUE;
+
+            int prev = square[x1][y1];
+            min = Math.min(min, prev);
+
+            for (int i = x1; i < x2; i++) {
+                square[i][y1] = square[i + 1][y1];
+                min = Math.min(min, square[i][y1]);
+            }
+
+            for (int i = y1; i < y2; i++) {
+                square[x2][i] = square[x2][i + 1];
+                min = Math.min(min, square[x2][i]);
+            }
+
+            for (int i = x2; i > x1; i--) {
+                square[i][y2] = square[i - 1][y2];
+                min = Math.min(min, square[i][y2]);
+            }
+
+            for (int i = y2; i > y1 + 1; i--) {
+                square[x1][i] = square[x1][i - 1];
+                min = Math.min(min, square[x1][i]);
+            }
+
+            square[x1][y1 + 1] = prev;
+            answer[index++] = min;
         }
 
         return answer;
@@ -43,10 +56,17 @@ class TestArray {
 
     @Test
     void 정답() {
-        String[] record = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo",
-                "Change uid4567 Ryan" };
-        String[] result = { "Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다." };
+        int[] rows = { 6, 3, 100 };
+        int[] columns = { 6, 3, 97 };
 
-        Assertions.assertArrayEquals(result, solution(record));
+        int[][][] queries = { { { 2, 2, 5, 4 }, { 3, 3, 6, 6 }, { 5, 1, 6, 3 } },
+                { { 1, 1, 2, 2 }, { 1, 2, 2, 3 }, { 2, 1, 3, 2 }, { 2, 2, 3, 3 } },
+                { { 1, 1, 100, 97 } } };
+
+        int[][] result = { { 8, 10, 25 }, { 1, 1, 5, 3 }, { 1 } };
+
+        for (int i = 0; i < result.length; i++) {
+            Assertions.assertArrayEquals(result[i], solution(rows[i], columns[i], queries[i]));
+        }
     }
 }
