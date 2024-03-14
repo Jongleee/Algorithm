fb5ba0460ba70047b249309cef8b49afa1315687
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-03-14 16:42:56 +0900

    점 찍기

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -2,71 +2,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int n, int k) {
-        int answer = 0;
-        int[] digits = convertToBaseKArray(n, k);
+    public long solution(int k, int d) {
+        long answer = 0;
 
-        int j;
-        for (int i = 0; i < digits.length; i = j) {
-            j = i + 1;
-            while (j < digits.length && digits[j] != 0)
-                j++;
+        long max_x = d / k;
 
-            if (isPrime(convertFromBaseK(digits, i, j)))
-                answer++;
+        for (long x = 0; x <= max_x; x++) {
+            answer += (long) Math.sqrt((double) d * d - x * x * k * k) / k + 1;
         }
 
         return answer;
     }
 
-    public int[] convertToBaseKArray(int n, int k) {
-        int[] result = new int[32];
-        int i = 31;
-
-        while (n > 0) {
-            result[i--] = n % k;
-            n /= k;
-        }
-
-        return result;
-    }
-
-    public long convertFromBaseK(int[] digits, int start, int end) {
-        long result = 0;
-        long multiplier = 1;
-
-        for (int i = end - 1; i >= start; i--) {
-            result += digits[i] * multiplier;
-            multiplier *= 10;
-        }
-
-        return result;
-    }
-
-    public boolean isPrime(long n) {
-        if (n <= 1) {
-            return false;
-        } else if (n == 2) {
-            return true;
-        }
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     @Test
     void 정답() {
-        int[] n = { 437674, 110011 };
-        int[] k = { 3, 10 };
-        int[] result = { 3, 2 };
+        int[] k = { 2, 1 };
+        int[] d = { 4, 5 };
+        int[] result = { 6, 26 };
 
         for (int i = 0; i < result.length; i++) {
-            Assertions.assertEquals(result[i], solution(n[i], k[i]));
+            Assertions.assertEquals(result[i], solution(k[i], d[i]));
         }
     }
 }
