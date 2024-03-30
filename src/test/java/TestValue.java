02c86fd5283b474f42a96908d65a26f865616f8f
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-03-30 16:22:18 +0900

    이웃한 칸

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -2,48 +2,41 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int[] bandage, int health, int[][] attacks) {
-        int castTime = bandage[0];
-        int healingPerSecond = bandage[1];
-        int additionalHealing = bandage[2];
-        int maxHealth = health;
-        int preAttackTime = attacks[0][0];
+    public int solution(String[][] board, int h, int w) {
+        int count = 0;
+        String color = board[h][w];
 
-        for (int[] attack : attacks) {
-            int attackTime = attack[0];
-            int damage = attack[1];
-            int timeBetween = attackTime - preAttackTime - 1;
+        int[][] directions = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
 
-            if (timeBetween > 0) {
-                health += (timeBetween * healingPerSecond);
-                health += ((timeBetween / castTime) * additionalHealing);
-                if (health > maxHealth) {
-                    health = maxHealth;
+        for (int[] dir : directions) {
+            int tempH = h + dir[0];
+            int tempW = w + dir[1];
+
+            if (isValidMove(tempH, tempW, board)) {
+                if (color.equals(board[tempH][tempW])) {
+                    count += 1;
                 }
             }
-
-            health -= damage;
-
-            if (health <= 0) {
-                return -1;
-            }
-
-            preAttackTime = attackTime;
         }
+        return count;
+    }
 
-        return health;
+    private boolean isValidMove(int h, int w, String[][] board) {
+        return h >= 0 && h < board.length && w >= 0 && w < board[0].length;
     }
 
     @Test
     void 정답() {
-        int[][] bandage = { { 5, 1, 5 }, { 3, 2, 7 }, { 4, 2, 7 }, { 1, 1, 1 } };
-        int[] health = { 30, 20, 20, 5 };
-        int[][][] attacks = { { { 2, 10 }, { 9, 15 }, { 10, 5 }, { 11, 5 } }, { { 1, 15 }, { 5, 16 }, { 8, 6 } },
-                { { 1, 15 }, { 5, 16 }, { 8, 6 } }, { { 1, 2 }, { 3, 2 } } };
-        int[] result = { 5, -1, -1, 3 };
+        String[][][] board = {
+                { { "blue", "red", "orange", "red" }, { "red", "red", "blue", "orange" },
+                        { "blue", "orange", "red", "red" }, { "orange", "orange", "red", "blue" } },
+                { { "yellow", "green", "blue" }, { "blue", "green", "yellow" }, { "yellow", "blue", "blue" } } };
+        int[] h = { 1, 0 };
+        int[] w = { 1, 1 };
+        int[] result = { 2, 1 };
 
         for (int i = 0; i < result.length; i++) {
-            Assertions.assertEquals(result[i], solution(bandage[i], health[i], attacks[i]));
+            Assertions.assertEquals(result[i], solution(board[i], h[i], w[i]));
         }
     }
 }
