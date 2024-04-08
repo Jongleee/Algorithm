82c80cd8bb7c52675499eda47806718ec7393601
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-04-08 22:39:25 +0900

    문자열을 정수로 바꾸기 평균 구하기

--- src/test/java/TestValue.java
+++ src/test/java/TestValue.java
@@ -1,109 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestValue {
-    public int solution(int k, int n, int[][] reqs) {
-        int answer = 0;
-
-        List<List<Time>> timeForEachType = new ArrayList<>();
-        for (int i = 0; i < k + 1; i++) {
-            timeForEachType.add(new ArrayList<>());
+    public double solution(int[] arr) {
+        int length = arr.length;
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
         }
-
-        for (int[] req : reqs) {
-            int startTime = req[0];
-            int duration = req[1];
-            int type = req[2];
-            timeForEachType.get(type).add(new Time(startTime, startTime + duration));
-        }
-
-        int[][] waitTimeForEachTime = new int[k + 1][n + 1];
-        for (int type = 1; type < k + 1; type++) {
-            if (timeForEachType.get(type).isEmpty()) continue;
-            for (int counselors = 1; counselors <= (n - k) + 1; counselors++) {
-                waitTimeForEachTime[type][counselors] = calculateWaitTime(timeForEachType.get(type), counselors);
-            }
-        }
-
-        int[] currentCounselors = new int[k + 1];
-        for (int type = 1; type < k + 1; type++) {
-            currentCounselors[type] = 1;
-        }
-
-        int remainCounselorNumber = n - k;
-        while (remainCounselorNumber-- > 0) {
-            int maxReduceTime = 0;
-            int correspondingType = 0;
-
-            for (int type = 1; type < k + 1; type++) {
-                int currentCounselorsByType = currentCounselors[type];
-                int reduceWaitingTime = waitTimeForEachTime[type][currentCounselorsByType] - waitTimeForEachTime[type][currentCounselorsByType + 1];
-
-                if (reduceWaitingTime >= maxReduceTime) {
-                    maxReduceTime = reduceWaitingTime;
-                    correspondingType = type;
-                }
-            }
-
-            if (maxReduceTime == 0) break;
-
-            currentCounselors[correspondingType]++;
-        }
-
-        for (int type = 1; type < k + 1; type++) {
-            if (timeForEachType.get(type).isEmpty()) continue;
-            int counselors = currentCounselors[type];
-            answer += waitTimeForEachTime[type][counselors];
-        }
-
-        return answer;
+        return (double) sum / length;
     }
 
-    private int calculateWaitTime(List<Time> typeTime, int counselorNumber) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int waitTime = 0;
-
-        for (Time t : typeTime) {
-            if (pq.isEmpty() || pq.size() < counselorNumber) {
-                pq.add(t.end);
-            } else {
-                int earlyConsultEndTime = pq.poll();
-                if (t.start < earlyConsultEndTime) {
-                    waitTime += (earlyConsultEndTime - t.start);
-                    pq.add(earlyConsultEndTime + (t.end - t.start));
-                } else {
-                    pq.add(t.end);
-                }
-            }
-        }
-        return waitTime;
-    }
-
-    private static class Time {
-        int start, end;
-
-        public Time(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-    
     @Test
     void 정답() {
-        int[] k = { 3, 2 };
-        int[] n = { 5, 3 };
-        int[][][] reqs = {
-                { { 10, 60, 1 }, { 15, 100, 3 }, { 20, 30, 1 }, { 30, 50, 3 }, { 50, 40, 1 }, { 60, 30, 2 },
-                        { 65, 30, 1 }, { 70, 100, 2 } },
-                { { 5, 55, 2 }, { 10, 90, 2 }, { 20, 40, 2 }, { 50, 45, 2 }, { 100, 50, 2 } } };
-        int[] result = { 25, 90 };
+        int[][] k = { { 1, 2, 3, 4 }, { 5, 5 } };
+        double[] result = { 2.5, 5 };
 
         for (int i = 0; i < result.length; i++) {
-            Assertions.assertEquals(result[i], solution(k[i], n[i], reqs[i]));
+            Assertions.assertEquals(result[i], solution(k[i]));
         }
     }
 }
