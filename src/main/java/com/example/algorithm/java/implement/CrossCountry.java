37294e66161d0bc2c2674e71a1ffb38b92f2a90c
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-04-29 23:38:43 +0900

    크로스 컨트리

--- src/main/java/com/example/algorithm/java/implement/CrossCountry.java
+++ src/main/java/com/example/algorithm/java/implement/CrossCountry.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.implement;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class CrossCountry {
     static int testCases;
     static int numOfPlayers;
     static int[] playerTeam = new int[1001];
