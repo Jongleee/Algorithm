c5544e4a23d275a0a5ac7171b84d9d1c1e44316d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-05-01 23:34:40 +0900

    카드2

--- src/main/java/com/example/algorithm/java/queue/Card2.java
+++ src/main/java/com/example/algorithm/java/queue/Card2.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.queue;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Card2 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
