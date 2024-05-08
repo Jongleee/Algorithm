304e9582674ee75728de455020c98e8f0a2727b4
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-05-08 23:59:17 +0900

    햄버거 분배

--- src/main/java/com/example/algorithm/java/Main.java
+++ src/main/java/com/example/algorithm/java/Main.java
@@ -1,72 +1,50 @@
 package com.example.algorithm.java;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static int k;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        String num = st.nextToken();
-        int len = num.length();
-        int[] nums = new int[len];
-        for(int i = 0; i < len; i++) {
-            nums[i] = num.charAt(i) - '0';
-        }
-        
-        int idx = 0;
-        int n = 0;
-        while(idx < len) {
-            n++;
-            int temp = n;
-            int length = (int)(Math.log10(n) + 1);
-            int[] arr = new int[length];
-            for(int i = length - 1; i >= 0; i--) {
-                arr[i] = temp % 10;
-                temp /= 10;
-            }
-            for(int i = 0; i < length; i++) {
-                if(nums[idx] == arr[i])
-                    idx++;
-                if(idx >= len)
-                    break;
+        StringTokenizer st = new StringTokenizer(br.readLine().trim(), " ");
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        char[] line = br.readLine().toCharArray();
+        int peopleNum = 0;
+        for (int i = 0; i < n; i++) {
+            if (line[i] == 'P' && isEatHamburger(i, line)) {
+                peopleNum++;
             }
         }
-        bw.write(n + "");
-        bw.close();
+        System.out.println(peopleNum);
+    }
+
+    private static boolean isEatHamburger(int index, char[] line) {
+        int start = Math.max(0, index - k);
+        int end = Math.min(n - 1, index + k);
+        for (int i = start; i <= end; i++) {
+            if (line[i] == 'H') {
+                line[i] = 'T';
+                return true;
+            }
+        }
+        return false;
     }
 }
 
 /*
-1234
+20 1
+HHPHPPHHPPHPPPHPHPHP
 
-4
-
-82340329923
-
-43
+8
 
 
-32098221
+20 2
+HHHHHPPPPPHPHPHPHHHP
 
-61
-
-
-1111111
-
-14
-
-00000000000000000000000000000000000000000000000000000000000000000000000
-
-400
-
-345029834023049820394802334909240982039842039483294792934790209
-
-279
+7
  */
