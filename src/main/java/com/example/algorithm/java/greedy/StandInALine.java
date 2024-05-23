79af08ffbcbfb56b713e2c7257cf0ed3b3cfdb2b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-05-23 15:57:56 +0900

    한 줄로 서기

--- src/main/java/com/example/algorithm/java/greedy/StandInALine.java
+++ src/main/java/com/example/algorithm/java/greedy/StandInALine.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class StandInALine {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
