f14b226c0173be5661b9cd380646599ba6b82823
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-06-18 23:41:56 +0900

    0 만들기

--- src/main/java/com/example/algorithm/java/bruteForce/MakeZero.java
+++ src/main/java/com/example/algorithm/java/bruteForce/MakeZero.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.bruteForce;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class MakeZero {
     static int n;
     static StringBuilder sb;
     static final char[] OPERATORS = {' ', '+', '-'};
