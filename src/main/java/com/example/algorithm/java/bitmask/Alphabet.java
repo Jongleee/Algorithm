ed817fc476b3711614cf90b77408272225fcb1fc
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-06-22 20:59:54 +0900

    알파벳

--- src/main/java/com/example/algorithm/java/bitmask/Alphabet.java
+++ src/main/java/com/example/algorithm/java/bitmask/Alphabet.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.bitmask;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Alphabet {
     static int r;
     static int c;
     static int answer;
