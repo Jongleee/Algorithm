434a094e238c074ab13f7d9cdd6fa707c9ab881d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-06-28 23:41:36 +0900

    여행 가자

--- src/main/java/com/example/algorithm/java/unionFind/LetsGoTrip.java
+++ src/main/java/com/example/algorithm/java/unionFind/LetsGoTrip.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.unionFind;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class LetsGoTrip {
     private static final char CONNECTED = '1';
     private static final String YES = "YES";
     private static final String NO = "NO";
