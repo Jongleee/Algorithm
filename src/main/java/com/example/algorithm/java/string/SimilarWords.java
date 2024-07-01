06c5e134d33d7ff86434958e2bdb50118117abca
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-07-01 23:53:13 +0900

    비슷한 단어

--- src/main/java/com/example/algorithm/java/string/SimilarWords.java
+++ src/main/java/com/example/algorithm/java/string/SimilarWords.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.string;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class SimilarWords {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
