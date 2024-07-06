13937c220fa642deaf9389d243f643f1f9e07f24
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-07-06 19:35:16 +0900

    탑 보기

--- src/main/java/com/example/algorithm/java/dynamicProgramming/TowerView.java
+++ src/main/java/com/example/algorithm/java/dynamicProgramming/TowerView.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.dynamicProgramming;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class TowerView {
     static int n;
     static int[] buildings;
     static int[][] dp;
