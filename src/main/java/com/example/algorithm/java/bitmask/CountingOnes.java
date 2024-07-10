cbe9ebe81b0a5caf76940a3f1078834bc2774984
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-07-10 23:01:02 +0900

    1의 개수 세기

--- src/main/java/com/example/algorithm/java/bitmask/CountingOnes.java
+++ src/main/java/com/example/algorithm/java/bitmask/CountingOnes.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.bitmask;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class CountingOnes {
     static long a;
     static long b;
     static long[] dp = new long[57];
