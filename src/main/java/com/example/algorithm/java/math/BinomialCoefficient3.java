1014e8b5bd736c33bda8bab278cad5040f6ae0d8
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-07-22 20:29:14 +0900

    이항 계수 3

--- src/main/java/com/example/algorithm/java/math/BinomialCoefficient3.java
+++ src/main/java/com/example/algorithm/java/math/BinomialCoefficient3.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.math;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BinomialCoefficient3 {
     private static final long MOD = 1000000007;
 
     public static void main(String[] args) throws IOException {
