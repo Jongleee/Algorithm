23ef8eaef3824a64e73ae1ccd7f8a7586c8f408b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-07-24 23:51:53 +0900

    행렬 제곱

--- src/main/java/com/example/algorithm/java/math/MatrixExponentiation.java
+++ src/main/java/com/example/algorithm/java/math/MatrixExponentiation.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.math;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class MatrixExponentiation {
     static int n;
     static long b;
     static int[][] matrix;
