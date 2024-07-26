1a58268a3091454498f27767a72963b8b0c14512
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-07-26 23:52:42 +0900

    피보나치 수 3

--- src/main/java/com/example/algorithm/java/math/FibonacciNumber3.java
+++ src/main/java/com/example/algorithm/java/math/FibonacciNumber3.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.math;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class FibonacciNumber3 {
     private static final long DIV = 1000000;
 
     public static void main(String[] args) throws IOException {
