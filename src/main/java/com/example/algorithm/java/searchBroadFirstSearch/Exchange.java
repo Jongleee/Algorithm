43fc5debf4a1d9c9fd3d0249f0a9a05636d4ef98
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-08-06 23:36:52 +0900

    교환

--- src/main/java/com/example/algorithm/java/searchBroadFirstSearch/Exchange.java
+++ src/main/java/com/example/algorithm/java/searchBroadFirstSearch/Exchange.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.searchBroadFirstSearch;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Exchange {
     static class Info {
         int[] num;
         int depth;
