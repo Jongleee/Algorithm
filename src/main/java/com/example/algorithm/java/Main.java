af9ebf388a129b959dc0f9c39341cef8e1893266
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-08-12 23:56:04 +0900

    플로이드

--- src/main/java/com/example/algorithm/java/Main.java
+++ src/main/java/com/example/algorithm/java/Main.java
@@ -1,105 +1,83 @@
 package com.example.algorithm.java;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int rows;
-    private static int cols;
-    private static char[][] map;
-    private static final int[] dx = {-1, 1, 0, 0};
-    private static final int[] dy = {0, 0, -1, 1};
-    private static final int TOP = 0;
-    private static final int BOTTOM = 1;
-    private static final int LEFT = 2;
-    private static final int RIGHT = 3;
+    private static final int MAX_DISTANCE = 60000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] dimensions = br.readLine().split(" ");
-        rows = Integer.parseInt(dimensions[0]);
-        cols = Integer.parseInt(dimensions[1]);
-        map = new char[rows][cols];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        for (int i = 0; i < rows; i++) {
-            map[i] = br.readLine().toCharArray();
+        int cityCount = Integer.parseInt(br.readLine());
+        int busCount = Integer.parseInt(br.readLine());
+
+        int[][] distances = new int[cityCount + 1][cityCount + 1];
+
+        for (int i = 1; i <= cityCount; i++) {
+            Arrays.fill(distances[i], MAX_DISTANCE);
+            distances[i][i] = 0;
         }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (map[i][j] == '.' && checkPipe(i, j)) {
-                    return;
+        for (int i = 0; i < busCount; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int travelCost = Integer.parseInt(st.nextToken());
+
+            distances[from][to] = Math.min(distances[from][to], travelCost);
+        }
+
+        for (int via = 1; via <= cityCount; via++) {
+            for (int from = 1; from <= cityCount; from++) {
+                for (int to = 1; to <= cityCount; to++) {
+                    distances[from][to] = Math.min(distances[from][to],
+                            distances[from][via] + distances[via][to]);
                 }
             }
         }
-    }
 
-    private static boolean checkPipe(int x, int y) {
-        boolean[] connected = new boolean[4];
-        connected[TOP] = isInRange(x + dx[TOP], y + dy[TOP]) && canComeFromBottom(map[x + dx[TOP]][y + dy[TOP]]);
-        connected[BOTTOM] = isInRange(x + dx[BOTTOM], y + dy[BOTTOM]) && canComeFromTop(map[x + dx[BOTTOM]][y + dy[BOTTOM]]);
-        connected[LEFT] = isInRange(x + dx[LEFT], y + dy[LEFT]) && canComeFromRight(map[x + dx[LEFT]][y + dy[LEFT]]);
-        connected[RIGHT] = isInRange(x + dx[RIGHT], y + dy[RIGHT]) && canComeFromLeft(map[x + dx[RIGHT]][y + dy[RIGHT]]);
+        for (int i = 1; i <= cityCount; i++) {
+            for (int j = 1; j <= cityCount; j++) {
+                bw.write((distances[i][j] < MAX_DISTANCE ? distances[i][j] : 0) + " ");
+            }
+            bw.write("\n");
+        }
 
-        if (connected[TOP] && connected[BOTTOM] && connected[LEFT] && connected[RIGHT])
-            System.out.println((x + 1) + " " + (y + 1) + " +");
-        else if (connected[TOP] && connected[BOTTOM])
-            System.out.println((x + 1) + " " + (y + 1) + " |");
-        else if (connected[TOP] && connected[LEFT])
-            System.out.println((x + 1) + " " + (y + 1) + " 3");
-        else if (connected[TOP] && connected[RIGHT])
-            System.out.println((x + 1) + " " + (y + 1) + " 2");
-        else if (connected[BOTTOM] && connected[LEFT])
-            System.out.println((x + 1) + " " + (y + 1) + " 4");
-        else if (connected[BOTTOM] && connected[RIGHT])
-            System.out.println((x + 1) + " " + (y + 1) + " 1");
-        else if (connected[LEFT] && connected[RIGHT])
-            System.out.println((x + 1) + " " + (y + 1) + " -");
-        else
-            return false;
-
-        return true;
-    }
-
-    private static boolean isInRange(int x, int y) {
-        return 0 <= x && x < rows && 0 <= y && y < cols;
-    }
-
-    private static boolean canComeFromTop(char c) {
-        return c == '|' || c == '+' || c == '2' || c == '3';
-    }
-
-    private static boolean canComeFromBottom(char c) {
-        return c == '|' || c == '+' || c == '1' || c == '4';
-    }
-
-    private static boolean canComeFromLeft(char c) {
-        return c == '-' || c == '+' || c == '3' || c == '4';
-    }
-
-    private static boolean canComeFromRight(char c) {
-        return c == '-' || c == '+' || c == '1' || c == '2';
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
 
-
 /*
-3 7
-.......
-.M-.-Z.
-.......
+5
+14
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+3 5 10
+3 1 8
+1 4 2
+5 1 7
+3 4 2
+5 2 4
 
-2 4 -
-
-
-6 10
-Z.1----4..
-|.|....|..
-|..14..M..
-2-+++4....
-..2323....
-..........
-
-3 3 |
+0 2 3 1 4
+12 0 15 2 5
+8 5 0 1 1
+10 7 13 0 3
+7 4 10 6 0
 */
