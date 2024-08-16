954f5ea612444b012f3dd68aa994192054c9ffaa
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-08-16 23:47:28 +0900

    운동

--- src/main/java/com/example/algorithm/java/floydWarshall/Exercise.java
+++ src/main/java/com/example/algorithm/java/floydWarshall/Exercise.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.floydWarshall;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Exercise {
     private static final int INF = 63000000;
     private static int n;
     private static int[][] distance;
