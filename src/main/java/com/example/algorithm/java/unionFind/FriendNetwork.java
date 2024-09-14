b722dae5183f7c421e70b7826e482775c7f90d2d
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-09-14 15:42:08 +0900

    친구 네트워크

--- src/main/java/com/example/algorithm/java/unionFind/FriendNetwork.java
+++ src/main/java/com/example/algorithm/java/unionFind/FriendNetwork.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.unionFind;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main {
+public class FriendNetwork {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
