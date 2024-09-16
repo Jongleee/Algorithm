6a5014a322127e1770fd8599af464fe2b59647f2
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-09-16 20:58:43 +0900

    공항

--- src/main/java/com/example/algorithm/java/unionFind/Airport.java
+++ src/main/java/com/example/algorithm/java/unionFind/Airport.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.unionFind;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Airport {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
