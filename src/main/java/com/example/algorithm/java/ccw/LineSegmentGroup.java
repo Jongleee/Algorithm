2d40ff96f95a6dd27f317d3cb2e0b4a9737411ce
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-10-04 22:24:09 +0900

    선분 그룹

--- src/main/java/com/example/algorithm/java/ccw/LineSegmentGroup.java
+++ src/main/java/com/example/algorithm/java/ccw/LineSegmentGroup.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.ccw;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class LineSegmentGroup {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
