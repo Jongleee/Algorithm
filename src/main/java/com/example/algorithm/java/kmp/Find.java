29fa9c8dfd7b4d8e36238a3236f8a80917d14af2
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-10-05 15:51:18 +0900

    찾기

--- src/main/java/com/example/algorithm/java/kmp/Find.java
+++ src/main/java/com/example/algorithm/java/kmp/Find.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.kmp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Find {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String text = br.readLine();
