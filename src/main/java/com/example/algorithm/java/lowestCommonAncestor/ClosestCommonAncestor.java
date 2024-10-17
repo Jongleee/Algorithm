16911ac30ad20408fbdd1dd4ed1f885219695d2c
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-10-17 16:29:25 +0900

    가장 가까운 공통 조상

--- src/main/java/com/example/algorithm/java/lowestCommonAncestor/ClosestCommonAncestor.java
+++ src/main/java/com/example/algorithm/java/lowestCommonAncestor/ClosestCommonAncestor.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.lowestCommonAncestor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class ClosestCommonAncestor {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCaseCount = Integer.parseInt(br.readLine());
