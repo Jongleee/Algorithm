9736dc05a27255a2d4c74446fcce0d122e00423b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-10-19 16:25:54 +0900

    트리와 쿼리 2

--- src/main/java/com/example/algorithm/java/binaryLifting/TreeAndQuery2.java
+++ src/main/java/com/example/algorithm/java/binaryLifting/TreeAndQuery2.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.binaryLifting;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class TreeAndQuery2 {
     static int n;
     static int m;
     static int[] depth;
