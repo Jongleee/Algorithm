28ca7ad50c2e59f8d7bc1d17a91b7d473201baae
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-10-22 23:08:17 +0900

    도미노

--- src/main/java/com/example/algorithm/java/searchDFS/Domino.java
+++ src/main/java/com/example/algorithm/java/searchDFS/Domino.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.searchDFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Domino {
     static int testcase;
     static int n;
     static int m;
