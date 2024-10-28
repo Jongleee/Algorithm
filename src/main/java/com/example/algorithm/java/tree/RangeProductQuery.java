50f3d54149b16f5023756ef8258c136858b9e3ff
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-10-28 23:03:55 +0900

    구간 곱 구하기

--- src/main/java/com/example/algorithm/java/tree/RangeProductQuery.java
+++ src/main/java/com/example/algorithm/java/tree/RangeProductQuery.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.tree;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class RangeProductQuery {
     private static final int MOD = 1_000_000_007;
 
     public static void main(String[] args) throws Exception {
