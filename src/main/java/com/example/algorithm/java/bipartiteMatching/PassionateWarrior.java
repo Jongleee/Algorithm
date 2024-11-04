7dd3e197f0e88e24547074455cf65bc013bc435e
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-11-04 23:47:11 +0900

    열혈강호

--- src/main/java/com/example/algorithm/java/bipartiteMatching/PassionateWarrior.java
+++ src/main/java/com/example/algorithm/java/bipartiteMatching/PassionateWarrior.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.bipartiteMatching;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class PassionateWarrior {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
