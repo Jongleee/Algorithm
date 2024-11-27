e5ee2b17f68c5272b6ce854725eeef8854484925
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-11-27 23:38:08 +0900

    뱀과 사다리 게임

--- src/main/java/com/example/algorithm/java/searchBroadFirstSearch/SnakeAndLadderGame.java
+++ src/main/java/com/example/algorithm/java/searchBroadFirstSearch/SnakeAndLadderGame.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.searchBroadFirstSearch;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class SnakeAndLadderGame {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] board = new int[101];
