92caf08477ab3bfddecb16f8f4c9dc128d7cf0bc
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-12-12 17:45:26 +0900

    Dangerous Dive

--- src/main/java/com/example/algorithm/java/implementation/DangerousDive.java
+++ src/main/java/com/example/algorithm/java/implementation/DangerousDive.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.implementation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class DangerousDive {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
