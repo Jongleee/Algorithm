94e0094004e7726c6896824d687d287f569fc138
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-12-13 23:28:03 +0900

    Cross Country Skiing

--- src/main/java/com/example/algorithm/java/binarySearch/CrossCountrySkiing.java
+++ src/main/java/com/example/algorithm/java/binarySearch/CrossCountrySkiing.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.binarySearch;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class CrossCountrySkiing {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
