404146889ffb32a91267bbdeada0b3cab2c47652
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-12-14 16:47:01 +0900

    Ski Course Rating

--- src/main/java/com/example/algorithm/java/unionFind/SkiCourseRating.java
+++ src/main/java/com/example/algorithm/java/unionFind/SkiCourseRating.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.unionFind;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class SkiCourseRating {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
