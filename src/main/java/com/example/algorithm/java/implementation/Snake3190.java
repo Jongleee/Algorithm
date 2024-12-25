d8ac51a01fbdde3f23f9e3f84ecd86b273385842
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-12-25 15:37:12 +0900

    뱀

--- src/main/java/com/example/algorithm/java/implementation/Snake3190.java
+++ src/main/java/com/example/algorithm/java/implementation/Snake3190.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.implementation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Snake3190 {
     private static final int WALL = -1;
     private static final int APPLE = 1;
     private static final int EMPTY = 0;
