35add6e6a398f86a014f87e28c5f8667fba48322
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2024-12-26 15:52:38 +0900

    시험 감독

--- src/main/java/com/example/algorithm/java/implementation/ExamSupervisor.java
+++ src/main/java/com/example/algorithm/java/implementation/ExamSupervisor.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.implementation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class ExamSupervisor {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
