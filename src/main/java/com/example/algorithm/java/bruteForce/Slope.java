dfe52a270032db536cb9aece554adba86e53564b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-01-04 16:44:54 +0900

    경사로

--- src/main/java/com/example/algorithm/java/bruteForce/Slope.java
+++ src/main/java/com/example/algorithm/java/bruteForce/Slope.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.bruteForce;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Slope {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
