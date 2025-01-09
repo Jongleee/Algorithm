54d005ffeb8cbde00e59b875b39011a3cc624ba5
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-01-09 16:48:12 +0900

    치킨 배달

--- src/main/java/com/example/algorithm/java/backtracking/ChickenDelivery.java
+++ src/main/java/com/example/algorithm/java/backtracking/ChickenDelivery.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.backtracking;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class ChickenDelivery {
+    
     private static class Dist implements Comparable<Dist> {
         int idx, distance;
 
