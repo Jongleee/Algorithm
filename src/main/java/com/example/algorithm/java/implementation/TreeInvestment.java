30bb27665765cf0c9d839557359490da026a0a2e
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-01-10 17:55:17 +0900

    나무 재테크

--- src/main/java/com/example/algorithm/java/implementation/TreeInvestment.java
+++ src/main/java/com/example/algorithm/java/implementation/TreeInvestment.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.implementation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class TreeInvestment {
     static class Tree {
         int age, count;
         Tree next, prev;
