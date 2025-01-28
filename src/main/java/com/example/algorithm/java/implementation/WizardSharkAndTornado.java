4380aa631d238cfa067e755274c9860aac7ab6ff
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-01-28 17:33:55 +0900

    마법사 상어와 토네이도

--- src/main/java/com/example/algorithm/java/implementation/WizardSharkAndTornado.java
+++ src/main/java/com/example/algorithm/java/implementation/WizardSharkAndTornado.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.implementation;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class WizardSharkAndTornado {
     static class TornadoSimulation {
         private final int[][] grid;
         private final int size;
