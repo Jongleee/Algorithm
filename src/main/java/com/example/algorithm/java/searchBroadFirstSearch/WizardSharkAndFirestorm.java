7543c8b99911e8e91a9905003eb45187acbcae88
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-01-29 16:36:22 +0900

    마법사 상어와 파이어스톰

--- src/main/java/com/example/algorithm/java/searchBroadFirstSearch/WizardSharkAndFirestorm.java
+++ src/main/java/com/example/algorithm/java/searchBroadFirstSearch/WizardSharkAndFirestorm.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.searchBroadFirstSearch;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class WizardSharkAndFirestorm {
     private static final int[] DR = { -1, 1, 0, 0 };
     private static final int[] DC = { 0, 0, -1, 1 };
 
