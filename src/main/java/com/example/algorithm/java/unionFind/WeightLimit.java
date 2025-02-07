9032ecb397cec0041c7c9b7da0b05aedb1572ca5
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-02-07 17:37:45 +0900

    중량제한

--- src/main/java/com/example/algorithm/java/unionFind/WeightLimit.java
+++ src/main/java/com/example/algorithm/java/unionFind/WeightLimit.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.unionFind;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class WeightLimit {
     private static class Edge implements Comparable<Edge> {
         int s, e, cost;
 
