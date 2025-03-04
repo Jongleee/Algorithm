89aa851882cf458356aefc50aecd0d0e7cacb8f9
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-03-04 17:13:10 +0900

    파일 합치기 2

--- src/main/java/com/example/algorithm/java/knuth/FileMerge2.java
+++ src/main/java/com/example/algorithm/java/knuth/FileMerge2.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.knuth;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class FileMerge2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
