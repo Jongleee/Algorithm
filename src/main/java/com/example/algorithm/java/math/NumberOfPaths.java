0c88c5328fe83acfbe8beea8c0b81b4e1548ae46
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-03-17 17:39:30 +0900

    길의 개수

--- src/main/java/com/example/algorithm/java/math/NumberOfPaths.java
+++ src/main/java/com/example/algorithm/java/math/NumberOfPaths.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.math;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class NumberOfPaths {
     private static final int MOD = 1000003;
 
     public static void main(String[] args) throws Exception {
