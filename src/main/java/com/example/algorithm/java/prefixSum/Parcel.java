726089f8cc41ddfb893a57765c647106e3a117e4
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-03-24 16:30:14 +0900

    Parcel

--- src/main/java/com/example/algorithm/java/prefixSum/Parcel.java
+++ src/main/java/com/example/algorithm/java/prefixSum/Parcel.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.prefixSum;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Parcel {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
