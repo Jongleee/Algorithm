7ff0a6810e6b7a59b376256bcd30d6b01b95ab9b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-03-26 20:31:14 +0900

    정점들의 거리

--- src/main/java/com/example/algorithm/java/lowestCommonAncestor/DistanceBetweenVertices.java
+++ src/main/java/com/example/algorithm/java/lowestCommonAncestor/DistanceBetweenVertices.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.lowestCommonAncestor;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class DistanceBetweenVertices {
     static class Node {
         int to, cost;
 
