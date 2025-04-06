2c06f49a1add8d50e7203f973dcfd94f32c0e4ba
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-04-06 16:10:10 +0900

    웜홀

--- src/main/java/com/example/algorithm/java/bellmanFord/Wormhole.java
+++ src/main/java/com/example/algorithm/java/bellmanFord/Wormhole.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.bellmanFord;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Wormhole {
     static class Edge {
         int from, to, cost;
 
