88589fbdd40de83ca94053eb5dbdcce796d4d97f
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-04-10 16:07:14 +0900

    LCS

--- src/main/java/com/example/algorithm/java/string/LongestCommonSubsequence.java
+++ src/main/java/com/example/algorithm/java/string/LongestCommonSubsequence.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.string;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class LongestCommonSubsequence {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
