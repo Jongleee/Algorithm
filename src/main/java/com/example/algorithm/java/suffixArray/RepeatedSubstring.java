7df8d070f4fdb5b4203cbaf3738a5793bb597d84
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-05-03 16:07:57 +0900

    반복 부분문자열

--- src/main/java/com/example/algorithm/java/suffixArray/RepeatedSubstring.java
+++ src/main/java/com/example/algorithm/java/suffixArray/RepeatedSubstring.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.suffixArray;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
-public class Main {
+public class RepeatedSubstring {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
