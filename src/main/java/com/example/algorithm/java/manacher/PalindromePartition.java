854159273298cf846dc87e47bbe1d6d30a8c1724
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-05-13 18:14:21 +0900

    팰린드롬 분할

--- src/main/java/com/example/algorithm/java/manacher/PalindromePartition.java
+++ src/main/java/com/example/algorithm/java/manacher/PalindromePartition.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.manacher;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class PalindromePartition {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
