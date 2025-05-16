92901c64096d7870b33e5c7b933367b3dd69d155
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-05-16 17:39:44 +0900

    Dance Dance Revolution

--- src/main/java/com/example/algorithm/java/dynamicProgramming/DanceDanceRevolution.java
+++ src/main/java/com/example/algorithm/java/dynamicProgramming/DanceDanceRevolution.java
@@ -1,10 +1,10 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.dynamicProgramming;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class DanceDanceRevolution {
     private static final int MAX = 5;
 
     public static void main(String[] args) throws IOException {
