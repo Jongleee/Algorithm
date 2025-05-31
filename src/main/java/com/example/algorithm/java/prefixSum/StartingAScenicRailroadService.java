3f25a1056276766cf3224bc8f9b0213e906e0c50
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-05-31 17:08:28 +0900

    Starting a Scenic Railroad Service

--- src/main/java/com/example/algorithm/java/prefixSum/StartingAScenicRailroadService.java
+++ src/main/java/com/example/algorithm/java/prefixSum/StartingAScenicRailroadService.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.prefixSum;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class StartingAScenicRailroadService {
     public static void main(String[] args) throws IOException {
         final int MAX = 100001;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
