964def13c36944779985dc4a80c5f3295b48193b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-06-11 19:16:13 +0900

    XOR 쿼리

--- src/main/java/com/example/algorithm/java/segmentTree/persistent/XorQuery.java
+++ src/main/java/com/example/algorithm/java/segmentTree/persistent/XorQuery.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.segmentTree.persistent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class XorQuery {
     private static final int MAX_X = 500000;
     private static final char ADD = '1';
     private static final char XOR = '2';
