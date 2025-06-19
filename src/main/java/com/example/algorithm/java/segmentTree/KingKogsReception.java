8ed7969d7e652884c169d4218cd130dd7edbb24b
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-06-19 16:32:31 +0900

    King Kog’s Reception

--- src/main/java/com/example/algorithm/java/segmentTree/KingKogsReception.java
+++ src/main/java/com/example/algorithm/java/segmentTree/KingKogsReception.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.segmentTree;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class KingKogsReception {
     private static final int MAX = 1 << 21;
     private static final long[] tree = new long[MAX];
     private static final long[] treeMax = new long[MAX];
