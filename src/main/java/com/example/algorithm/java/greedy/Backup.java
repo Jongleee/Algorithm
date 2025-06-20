cee6805e8d874050868f1e9e2d8bd8458de5b154
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-06-20 18:11:48 +0900

    백업

--- src/main/java/com/example/algorithm/java/greedy/Backup.java
+++ src/main/java/com/example/algorithm/java/greedy/Backup.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Backup {
     private static class Edge implements Comparable<Edge> {
         int length;
         boolean disabled;
