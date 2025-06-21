a56165f7fef6a9bae923be7842f8608fcefb57c1
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-06-21 16:54:11 +0900

    유성

--- src/main/java/com/example/algorithm/java/fenwickTree/Meteor.java
+++ src/main/java/com/example/algorithm/java/fenwickTree/Meteor.java
@@ -1,11 +1,11 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.fenwickTree;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Meteor {
     static final String NIE = "NIE\n";
     static final char NEW_LINE = '\n';
 
