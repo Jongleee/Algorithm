a95a934056e962414d23d3a41467c084374d69d1
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-07-06 16:29:07 +0900

    공장들

--- src/main/java/com/example/algorithm/java/tree/centroid/Factories.java
+++ src/main/java/com/example/algorithm/java/tree/centroid/Factories.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.tree.centroid;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Factories {
     static class Node {
         int to;
         long weight;
