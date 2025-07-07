88643fa574457643966b0e4d5f5e676ad00d2d75
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2025-07-07 17:29:30 +0900

    씽크스몰

--- src/main/java/com/example/algorithm/java/ntt/ThinkSmall.java
+++ src/main/java/com/example/algorithm/java/ntt/ThinkSmall.java
@@ -1,4 +1,4 @@
-package com.example.algorithm.java;
+package com.example.algorithm.java.ntt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class ThinkSmall {
     public static void main(String[] args) throws Exception {
         final int SIZE = 1 << 21;
         final long P1 = 998244353L;
